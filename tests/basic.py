efd36ced5d6acd3ab790cd9e964a392a0653756d
nabr <nabr@dtu.dk>
2021-02-23 12:55:38 +0100

    Added basic testing of demo scripts

--- tests/basic.py
+++ tests/basic.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
 #Add basic tests here.
+
+import os
+for filename in os.listdir("../demos"):
+    print('Running file: ' + filename)
+    exec(open('../demos/'+filename).read())
