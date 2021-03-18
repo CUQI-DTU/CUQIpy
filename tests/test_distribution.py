653f20d45f50f0bdbdd29d965888e84faec9a66b
Jakob S. Jorgensen <jakj@dtu.dk>
2021-03-18 14:27:33 +0100

    Add normal pdf test

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -1,4 +1,11 @@
 import cuqi
+import numpy as np
 
-def test_Normal():
-    assert cuqi.distribution.Normal(0,1).mean == 0
+from pytest import approx
+
+def test_Normal_mean_standard():
+    assert cuqi.distribution.Normal(0,1).mean == approx(0.0)
+
+def test_Normal_pdf_mean():
+    pX = cuqi.distribution.Normal(0.1,1)
+    assert pX.pdf(0.1) == approx(1.0/np.sqrt(2.0*np.pi))
