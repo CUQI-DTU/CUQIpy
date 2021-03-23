3186add6345ae30883b9ee2ae9e7aaaa47935018
nabr <nabr@dtu.dk>
2021-03-23 10:26:57 +0100

    Added parametrized tests of Normal distribution

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -2,6 +2,7 @@ import cuqi
 import numpy as np
 
 from pytest import approx
+import pytest
 
 def test_Normal_mean_standard():
     assert cuqi.distribution.Normal(0,1).mean == approx(0.0)
@@ -10,10 +11,18 @@ def test_Normal_pdf_mean():
     pX = cuqi.distribution.Normal(0.1,1)
     assert pX.pdf(0.1) == approx(1.0/np.sqrt(2.0*np.pi))
 
-def test_Normal_sample_regression():
+@pytest.mark.parametrize("mean,var,expected",
+                        [
+                            (2,3.5,[[8.17418321], [3.40055023]]),
+                            (3.141592653589793,2.6457513110645907,[[7.80883646],[4.20030911]]),
+                            (-1e-09, 1000000.0,[[1764052.34596766],[400157.20836722]]),
+                            (1.7724538509055159, 0, [[1.77245385],[1.77245385]])
+                        ]
+                        )
+def test_Normal_sample_regression(mean,var,expected):
     rng = np.random.RandomState(0) #Replaces legacy method: np.random.seed(0)
-    samples = cuqi.distribution.Normal(2,3.5).sample(2,rng=rng)
-    target = np.array([[8.17418321], [3.40055023]])
+    samples = cuqi.distribution.Normal(mean,var).sample(2,rng=rng)
+    target = np.array(expected)
     assert np.allclose( samples, target)
 
 def test_Gaussian_mean():
