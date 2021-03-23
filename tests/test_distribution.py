660f9a8afce8c5012541b48bbf35c7eee00f5bb5
nabr <nabr@dtu.dk>
2021-03-23 11:14:15 +0100

    Made paramatrize call prettier

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -11,14 +11,12 @@ def test_Normal_pdf_mean():
     pX = cuqi.distribution.Normal(0.1,1)
     assert pX.pdf(0.1) == approx(1.0/np.sqrt(2.0*np.pi))
 
-@pytest.mark.parametrize("mean,var,expected",
-                        [
+@pytest.mark.parametrize("mean,var,expected",[
                             (2,3.5,[[8.17418321], [3.40055023]]),
                             (3.141592653589793,2.6457513110645907,[[7.80883646],[4.20030911]]),
                             (-1e-09, 1000000.0,[[1764052.34596766],[400157.20836722]]),
                             (1.7724538509055159, 0, [[1.77245385],[1.77245385]])
-                        ]
-                        )
+                        ])
 def test_Normal_sample_regression(mean,var,expected):
     rng = np.random.RandomState(0) #Replaces legacy method: np.random.seed(0)
     samples = cuqi.distribution.Normal(mean,var).sample(2,rng=rng)
