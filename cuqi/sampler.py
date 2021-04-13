7e00e972203782f56c7d4a004bfe9b9ba12a852e
furca <furca@dtu.dk>
2021-04-13 20:49:33 +0200

    just starting

--- cuqi/sampler.py
+++ cuqi/sampler.py
@@ -5,6 +5,98 @@ import numpy as np
 # import matplotlib.pyplot as plt
 eps = np.finfo(float).eps
 
+
+#===================================================================
+#===================================================================
+#===================================================================
+class CWMH(object):
+    
+    def __init__(self, pi_target, proposal, scale, init_x):
+        self.target = pi_target
+        self.proposal = proposal
+        self.scale = scale
+        self.x0 = init_x
+        self.n = len(init_x)
+
+    def sample(self, N, Nb):        
+        # def CGLS_reg_samples(x_old, A, W1sq_D1, W2sq_D2, b_meas, lambd, delta, x_maxit, x_tol):  
+        # params for cgls
+        m = len(b_meas)
+        nbar = len(x_old)
+
+        # apply cgls
+        G_fun = lambda x, flag: proj_forward_reg_mat(x, flag, m, A, W1sq_D1, W2sq_D2, lambd, delta)
+        g = np.hstack([np.sqrt(lambd)*b_meas, np.zeros(nbar), np.zeros(nbar)]) + np.random.randn(m+2*nbar)
+        # g = np.hstack([np.sqrt(lambd)*b_meas + np.random.randn(m), np.random.randn(nbar), np.random.randn(nbar)])
+        #
+        x_next, it = cgls(G_fun, g, x_old, x_maxit, x_tol)
+        
+        return x_next, it#, misfit
+
+#===================================================================
+def cgls(A, b, x0, maxit, tol):
+    # http://web.stanford.edu/group/SOL/software/cgls/
+    
+    # initial state
+    x = x0.copy()
+    r = b - A(x, 1)
+    s = A(r, 2) #- shift*x
+    
+    # initialization
+    p = s.copy()
+    norms0 = LA.norm(s)
+    normx = LA.norm(x)
+    gamma, xmax = norms0**2, normx
+    
+    # main loop
+    k, flag, indefinite = 0, 0, 0
+    while (k < maxit) and (flag == 0):
+        k += 1  
+        # xold = np.copy(x)
+        #
+        q = A(p, 1)
+        delta_cgls = LA.norm(q)**2 #+ shift*LA.norm(p)**2
+        #
+        if (delta_cgls < 0):
+            indefinite = 1
+        elif (delta_cgls == 0):
+            delta_cgls = eps
+        alpha_cgls = gamma / delta_cgls
+        #
+        x += alpha_cgls*p    
+        x  = np.maximum(x, 0)
+        r -= alpha_cgls*q
+        s  = A(r, 2) #- shift*x
+        #
+        gamma1 = gamma.copy()
+        norms = LA.norm(s)
+        gamma = norms**2
+        p = s + (gamma/gamma1)*p
+        
+        # convergence
+        normx = LA.norm(x)
+        # relerr = LA.norm(x - xold) / normx
+        # if relerr <= tol:
+        #     flag = 1
+        xmax = max(xmax, normx)
+        flag = (norms <= norms0*tol) or (normx*tol >= 1)
+        # flag = 1: CGLS converged to the desired tolerance TOL within MAXIT
+        # resNE = norms / norms0
+    #
+    shrink = normx/xmax
+    if k == maxit:          
+        flag = 2   # CGLS iterated MAXIT times but did not converge
+    if indefinite:          
+        flag = 3   # Matrix (A'*A + delta*L) seems to be singular or indefinite
+        sys.exit('\n Negative curvature detected !')  
+    if shrink <= np.sqrt(tol):
+        flag = 4   # Instability likely: (A'*A + delta*L) indefinite and NORM(X) decreased
+        sys.exit('\n Instability likely !')  
+    
+    return x, k
+
+
+
 #===================================================================
 #===================================================================
 #===================================================================
