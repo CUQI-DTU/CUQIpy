d3b394053f3b4260ce0b1c750392f5b774ae6b25
nabr <nabr@dtu.dk>
2021-04-14 14:08:06 +0200

    Added support for matrix as input argument to LinearModel

--- cuqi/model.py
+++ cuqi/model.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.sparse import csc_matrix
 
 class Model(object):
     # Generic Model class.
@@ -10,39 +11,61 @@ class Model(object):
             raise TypeError("Forward needs to be callable function of some kind")
             
         #Store forward func
-        self.forward_func = forward
+        self._forward_func = forward
         
         #Store dimension
         self.dim = dim
                
     def forward(self, x):
-        return self.forward_func(x)
+        return self._forward_func(x)
     
 class LinearModel(Model):
     # Linear forward model with forward and adjoint (transpose).
-    # Only accepts callable functions for now.
     
-    def __init__(self,forward,adjoint,dim=[]):
+    def __init__(self,forward,adjoint=None,dim=None):
         
+        #Assume forward is matrix if not callable (TODO: add more checks)
+        if not callable(forward): 
+            forward_func = lambda x: forward@x
+            adjoint_func = lambda y: forward.T@y
+            matrix = forward
+            dim = forward.shape
+        else:
+            forward_func = forward
+            adjoint_func = adjoint
+            matrix = None
+            dim = dim
+
         #Initialize Model class
-        super().__init__(forward,dim)
+        super().__init__(forward_func,dim)
         
         #Check if input is callable
-        if callable(adjoint) is not True:
+        if callable(adjoint_func) is not True:
             raise TypeError("Adjoint needs to be callable function of some kind")
             
         #Add adjoint
-        self.adjoint = adjoint
-        
+        self._adjoint_func = adjoint_func
+
+        #Store matrix privately
+        self._matrix = matrix
+
+    def adjoint(self):
+        return self._adjoint_func
+
     def get_matrix(self):
-        mat = np.zeros((self.dim[0],self.dim[1]))
-        e = np.zeros(self.dim[1])
-        
-        for i in range(self.dim[1]):
-            e[i] = 1
-            mat[:,i] = self.forward(e)
-            e[i] = 0
+        if self._matrix is not None: #Matrix exists so return it
+            return self._matrix
+        else:
+            mat = csc_matrix((self.dim[0],self.dim[1])) #Sparse matrix
+            e = np.zeros(self.dim[1])
             
-        return mat
-        
+            for i in range(self.dim[1]):
+                e[i] = 1
+                mat[:,i] = self.forward(e)
+                e[i] = 0
+
+            #Store matrix for future use
+            self._matrix = mat
+
+            return self._matrix
         
