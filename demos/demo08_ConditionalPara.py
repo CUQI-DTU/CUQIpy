d6f3d1af10e06cc3b37f47f8ed96d54bf45ed729
nabr <nabr@dtu.dk>
2021-05-27 12:43:29 +0200

    Add Gibbs sampler from 5.1 in Johns book

--- demos/demo08_ConditionalPara.py
+++ demos/demo08_ConditionalPara.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append("..") 
 import numpy as np
 import cuqi
+import matplotlib.pyplot as plt
 
 # %%
 x = cuqi.distribution.Normal2(0,1)
@@ -15,3 +16,52 @@ y(std=10).sample(1)
 # %%
 y.sample(1)
 
+# %%
+# Example from Johns book. Algorithm 5.1
+n_samp = 1000
+alpha = 1
+beta = 1e-4
+
+# Load deconvolution test problem
+tp = cuqi.testproblem.Deconvolution()
+m,n = tp.model.dim
+
+# Matricies
+A = tp.model.get_matrix()
+b = tp.data
+L = np.eye(n)
+
+#x = cuqi.distribution.Gaussian(mean=None,std=None)
+l = cuqi.distribution.Gamma(shape=m/2+alpha,rate=1)
+d = cuqi.distribution.Gamma(shape=n/2+alpha,rate=1)
+
+# Preallocate sample vectors
+ls = np.zeros(n_samp+1)
+ds = np.zeros(n_samp+1)
+xs = np.zeros((n,n_samp+1))
+
+# Initial parameters
+ls[0] = 0.05; ds[0]=0.2
+
+# Initial x
+mean_x = lambda l,d: np.linalg.inv(l*A.T@A+d*L)@(l*A.T@b)
+cov_x  = lambda l,d: np.linalg.inv(l*A.T@A+d*L)
+xs[:,0] = mean_x(ls[0],ds[0])
+
+for k in range(n_samp):
+
+    #Sample hyperparameters
+    ls[k+1] = l(rate=.5*np.linalg.norm(A@xs[:,k]-b)**2+beta).sample(1)
+    ds[k+1] = d(rate=.5*xs[:,k].T@(L@xs[:,k])+beta).sample(1)
+
+    # Sample x
+    S = np.linalg.cholesky(cov_x(ls[k+1],ds[k+1]))
+    xs[:,k+1] = mean_x(ls[k+1],ds[k+1]) + S@np.random.randn(n)
+    
+# %%
+plt.subplot(121); plt.plot(ls); plt.title("lambda chain")
+plt.subplot(122); plt.plot(ds); plt.title("delta chain")
+# %%
+burn_in = 200
+plt.plot(tp.exactSolution)
+plt.plot(np.mean(xs[:,burn_in:],axis=1))
