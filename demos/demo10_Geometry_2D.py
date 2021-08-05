41b1ceeb100e5bf59460f5211a9d7f1f212a3283
amaal <amaal@dtu.edu>
2021-08-05 08:50:01 +0000

    Add demo file for Continuous2D class

--- demos/demo10_Geometry_2D.py
+++ demos/demo10_Geometry_2D.py
@@ -4,27 +4,16 @@ sys.path.append("..")
 import cuqi
 import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # %%
 geom = cuqi.geometry.Continuous2D((100,40))
-z = geom.grid[0]**2 + geom.grid[1]**2
+
 # %%
 plt.figure(1)
-geom.plot(np.ravel(z))
+geom.plot(np.ravel(geom.grid[0]))
 
 plt.figure(2)
-geom.plot_contour(z)
+geom.plot_contour(geom.grid[0])
 
 plt.figure(3)
-im = geom.plot_contourf(z)
-plt.colorbar(im[0])
-
-plt.figure(4)
-ims = geom.plot(np.stack((np.ravel(z),np.ravel(-z)), axis=-1))
-
-f1 = plt.figure(5)
-ims = geom.plot_contour(np.stack((z,z,z), axis=-1))
-for i, im in enumerate(ims):
-    cax = make_axes_locatable(f1.axes[i]).append_axes('right', size='5%', pad=0.05)
-    f1.colorbar(im, cax = cax)
+geom.plot_contourf(geom.grid[0])
