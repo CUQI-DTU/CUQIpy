04aa1b3ad9eb65f4524901fff775c04ca2b49012
amaal <amaal@dtu.edu>
2021-08-11 08:49:55 +0000

    tests for geometry attributes in Model class

--- tests/test_model.py
+++ tests/test_model.py
@@ -16,3 +16,20 @@ def test_LinearModel_getMatrix(seed):
     mat2 = model2.get_matrix() #Sparse matrix (generated from functions)
 
     assert np.allclose(mat1,mat2.A)
+
+def test_initialize_model_dim():
+    model1 = cuqi.model.Model(lambda x:x, dim = (4,4))
+    assert(len(model1.domainGeom.grid) == 4 and len(model1.rangeGeom.grid) == 4 )
+
+def test_initialize_model_geom():
+    model1 = cuqi.model.Model(lambda x:x)
+    model1.rangeGeom = cuqi.geometry.Continuous1D(dim=[5])
+    model1.domainGeom = cuqi.geometry.Continuous1D(dim=[3])
+    dim_old = model1.dim 
+    model1.rangeGeom = cuqi.geometry.Continuous1D(dim=[4])
+    assert(dim_old == (5,3) and model1.dim == (4,3)) 
+
+def test_initialize_model_matr():
+    model1 = cuqi.model.LinearModel(np.eye(5))
+    assert( model1.dim == (5,5) and len(model1.domainGeom.grid) == 5 and
+            len(model1.rangeGeom.grid) == 5)
