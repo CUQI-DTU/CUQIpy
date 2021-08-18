0629f3cfe661a04451cf385ec6bacb1b8fc812ed
amaal <amaal@dtu.edu>
2021-08-18 12:30:25 +0000

    Change value of the function to be plotted and use colorbar

--- demos/demo10_Geometry_2D.py
+++ demos/demo10_Geometry_2D.py
@@ -7,13 +7,14 @@ import numpy as np
 
 # %%
 geom = cuqi.geometry.Continuous2D((100,40))
-
+z = geom.grid[0]**2 + geom.grid[1]**2
 # %%
 plt.figure(1)
-geom.plot(np.ravel(geom.grid[0]))
+geom.plot(np.ravel(z))
 
 plt.figure(2)
-geom.plot_contour(geom.grid[0])
+geom.plot_contour(z)
 
 plt.figure(3)
-geom.plot_contourf(geom.grid[0])
+im = geom.plot_contourf(z)
+plt.colorbar(im)
