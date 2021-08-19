cca3a385be74f7869abc13449bb274687540b7c1
amaal <amaal@dtu.edu>
2021-08-19 09:40:21 +0000

    handle subplots

--- cuqi/geometry.py
+++ cuqi/geometry.py
@@ -20,9 +20,9 @@ class Geometry(ABC):
         pass
 
     @abstractmethod
-    def _default_configuration(self):
+    def _pre_plot(self,values):
         """
-        Default configuration of plots to be called inside any 'plot_' method
+        A method that implements any default configuration or set up that needs to occur before ploting. This method is to be called inside any 'plot_' method.
         """
         pass
 
@@ -39,12 +39,13 @@ class Continuous1D(Geometry):
             raise NotImplementedError("Cannot init 1D geometry with spatial dimension > 1")
 
     def plot(self,values,*args,**kwargs):
-        self._default_configuration()
+        self._pre_plot(values)
         return plt.plot(self.grid,values,*args,**kwargs)
 
-    def _default_configuration(self):
+    def _pre_plot(self, values):
         if self.labels is not None:
             plt.xlabel(self.labels[0])
+        return values
 
 
 class Continuous2D(Geometry):
@@ -68,22 +69,50 @@ class Continuous2D(Geometry):
         kwargs : keyword arguments
             keyword arguments which the function :meth:`matplotlib.pyplot.pcolor` normally takes.
         """
-        self._default_configuration()
-        return plt.pcolor(self.grid[0],self.grid[1],values.reshape(self.grid[0].shape),
-                          **kwargs)
+        values = self._pre_plot(values)
+        ims = []
+        for i, axis in enumerate(plt.gcf().axes):
+            ims.append(axis.pcolor(self.grid[0],self.grid[1],values[...,i].reshape(self.grid[0].shape),
+                          **kwargs))
+        return ims
     
     def plot_contour(self,values,**kwargs):
-        self._default_configuration()
-        return plt.contour(self.grid[0],self.grid[1],values.reshape(self.grid[0].shape),
-                           **kwargs)
+        values = self._pre_plot(values)
+        ims = []
+        for i, axis in enumerate(plt.gcf().axes):
+            ims.append(axis.contour(self.grid[0],self.grid[1],values[...,i].reshape(self.grid[0].shape),
+                          **kwargs))
+        return ims
 
     def plot_contourf(self,values,**kwargs):
-        self._default_configuration()
-        return plt.contourf(self.grid[0],self.grid[1],values.reshape(self.grid[0].shape),
-                            **kwargs)
+        values = self._pre_plot(values)
+        ims = []
+        for i, axis in enumerate(plt.gcf().axes):
+            ims.append(axis.contourf(self.grid[0],self.grid[1],values[...,i].reshape(self.grid[0].shape),
+                          **kwargs))
+        return ims
     
-    def _default_configuration(self):
-        if self.labels is not None:
-            plt.xlabel(self.labels[0])
-            plt.ylabel(self.labels[1])
-        plt.gca().axis('equal')
+    def _pre_plot(self,values):
+        if len(values.shape) == 3 or\
+             (len(values.shape) == 2 and values.shape[0]== len(self.grid[0].flat)): #TODO: change len(self.domain_geometry.grid) to self.domain_geometry.ndofs 
+            self._create_subplots(values.shape[-1])
+        else:
+            self._create_subplots(1)
+            values = values[..., np.newaxis]
+
+        for i, axis in enumerate(plt.gcf().axes):
+            if self.labels is not None:
+                axis.set_xlabel(self.labels[0])
+                axis.set_ylabel(self.labels[1])
+            axis.set_aspect('equal')
+      
+        return values
+
+    def _create_subplots(self,N):
+        Nx = math.ceil(np.sqrt(N))
+        Ny = Nx
+        fig = plt.gcf()
+        fig.set_size_inches(fig.bbox_inches.corners()[3][0]*Nx, fig.bbox_inches.corners()[3][1]*Ny)
+        for i in range(N):
+            subplot_id = Ny*100+ Nx*10 + (i+1)
+            fig.add_subplot(subplot_id)
