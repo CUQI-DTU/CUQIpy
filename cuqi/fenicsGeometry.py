cbf2871a753d23825ede0648ef876f50548996d1
amaal <amaal@dtu.edu>
2021-08-24 09:39:13 +0000

    update fenicsGeometry module and its demo

--- cuqi/fenicsGeometry.py
+++ cuqi/fenicsGeometry.py
@@ -1,21 +1,28 @@
 from cuqi.geometry import Geometry
 import numpy as np
 import dolfin as dl
-from abc import ABC, abstractmethod
 import matplotlib.pyplot as plt
+import math
 
 
-class FenicsContinuous(Geometry,ABC):
+class FenicsContinuous(Geometry):
 
-    @abstractmethod
-    def __init__(self, Vh):
+    def __init__(self, Vh, labels = ['x', 'y']):
+        self._physical_dim = Vh.mesh().geometry().dim()
+        if self.physical_dim >2:
+            raise NotImplementedError("'FenicsContinuous' object does not support 3D meshes yet. 'mesh' needs to be a 1D or 2D mesh.")
+        self.labels = labels
         self.Vh = Vh
+
+    @property
+    def physical_dim(self):
+        return self._physical_dim   
 
     @property
     def mesh(self):
         return self.Vh.mesh()
 
-    def plot(self,values,**kwargs):
+    def plot(self,values,subplots=True,**kwargs):
         """
         Overrides :meth:`cuqi.geometry.Geometry.plot`. See :meth:`cuqi.geometry.Geometry.plot` for description  and definition of the parameter `values`.
         
@@ -24,39 +31,34 @@ class FenicsContinuous(Geometry,ABC):
         kwargs : keyword arguments
             keyword arguments which the function :meth:`dolfin.plot` normally takes.
         """
+        values = self._process_values(values)
+        subplot_ids = self._create_subplot_list(values)
+
+        ims = []
         func = dl.Function(self.Vh)
-        func.vector().zero()
-        func.vector().set_local(values)
-        p = dl.plot(func, **kwargs)
-        self._label_coordinates(p)
-        return p
+        for rows,cols,id in subplot_ids:
+            func.vector().zero()
+            func.vector().set_local(values[...,id-1])
+            if subplots:
+                plt.subplot(rows,cols,id); 
+            ims.append(dl.plot(func, **kwargs))
+
+        self._plot_config(subplots) 
+        return ims
+
+    def _process_values(self, values):
+        if len(values.shape) == 1:
+            values = values[..., np.newaxis]
+        
+        return values
     
-    def _label_coordinates(self, p):
-        pass
-
-
-class FenicsContinuous1D(FenicsContinuous):
-
-    def __init__(self,Vh):
-        if Vh.mesh().geometry().dim()!=1:
-            raise ValueError("'mesh' used in creating 'FenicsContinuous1D' object must be a one-dimensional mesh.")
-        super().__init__(Vh)
+    def _plot_config(self, subplot):
+        if self.labels is not None:
+            if subplot == False:
+                plt.gca().set_xlabel(self.labels[0])
+                if self.physical_dim == 2: plt.gca().set_ylabel(self.labels[1]) 
+            else:
+                for i, axis in enumerate(plt.gcf().axes):
+                    axis.set_xlabel(self.labels[0])
+                    if self.physical_dim == 2: axis.set_ylabel(self.labels[1])
     
-    def _label_coordinates(self, p):
-        plt.gca().set_xlabel('x')
-
-
-class FenicsContinuous2D(FenicsContinuous):
-    def __init__(self,Vh):
-        if Vh.mesh().geometry().dim()!=2:
-            raise ValueError("'mesh' used in creating 'FenicsContinuous2D' object must be a two-dimensional mesh.")
-        super().__init__(Vh)
-     
-    def _label_coordinates(self, p):
-        p.ax.set_xlabel('x')
-        p.ax.set_ylabel('y')
-
-
-class FenicsContinuous3D(FenicsContinuous):
-    def __init__(self,Vh):
-        raise NotImplementedError("'FenicsContinuous' object for 3D meshes is not implemented yet. 'mesh' needs to be a 1D or 2D mesh.")
--- demos/demo11_FenicsGeometry.py
+++ demos/demo11_FenicsGeometry.py
@@ -4,12 +4,13 @@ sys.path.append("..")
 import cuqi
 import dolfin as dl
 import matplotlib.pyplot as plt
+import numpy as np
 
 #%% 1D case test
 # Define FenicsContinuous2D object 
 mesh = dl.UnitIntervalMesh(30)
 Vh = dl.FunctionSpace(mesh, 'CG', 1)
-geom = cuqi.fenicsGeometry.FenicsContinuous1D(Vh)
+geom = cuqi.fenicsGeometry.FenicsContinuous(Vh)
 
 # Define function
 f1= dl.Function(Vh)
@@ -19,16 +20,23 @@ f1.interpolate(dl.Expression("pow(sin(x[0]),3)", degree =2))
 plt.figure(1)
 geom.plot(f1.vector().get_local())
 
+plt.figure(2)
+geom.plot(np.stack((f1.vector().get_local(), f1.vector().get_local()), axis = -1), subplots = False)
+
 #%% 2D case test
 # Define FenicsContinuous2D object 
 mesh = dl.UnitSquareMesh(10,10)
 Vh = dl.FunctionSpace(mesh, 'CG', 1)
-geom = cuqi.fenicsGeometry.FenicsContinuous2D(Vh)
+geom = cuqi.fenicsGeometry.FenicsContinuous(Vh)
 
 # Define function
 f1= dl.Function(Vh)
 f1.interpolate(dl.Expression("pow(sin(x[0]),2)*cos(x[1]*2)", degree =2))
 
 # plot
-plt.figure(2)
+plt.figure(3)
 geom.plot(f1.vector().get_local())
+
+plt.figure(4)
+geom.plot(np.stack((f1.vector().get_local(), f1.vector().get_local()), axis = -1))
+
