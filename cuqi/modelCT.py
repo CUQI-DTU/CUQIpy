9f8cf861cfc209bc92f44bc37f85895b5b7ba51c
nabr <nabr@dtu.dk>
2021-09-03 12:19:04 +0200

    Initial ASTRA CT demo+class

---
+++ cuqi/modelCT.py
@@ -0,0 +1,30 @@
+import cuqi
+import warnings
+import numpy as np
+
+try:
+    import astra
+except Exception as error:
+    warnings.warn(error.msg)
+
+class CT(cuqi.model.LinearModel):
+    """Base CT class"""
+    
+    def __init__(self, proj_type="parallel", im_size=(128,128), det_count=128, angles=np.linspace(0,np.pi,128)):
+        """TODO documentation of calls"""
+
+        proj_geom = astra.create_proj_geom("parallel",1.0,det_count,angles)
+        vol_geom = astra.create_vol_geom(im_size[0],im_size[1])
+        proj_id = astra.create_projector("linear",proj_geom,vol_geom)
+
+        #TODO: refine geometries to reflect CT problem
+        # e.g. vol_geom["option"]["WindowMinX"] etc.
+        range_geometry  = cuqi.geometry.Continuous2D((det_count,np.size(angles)))
+        domain_geometry = cuqi.geometry.Continuous2D(im_size)
+
+        forward = lambda x: astra.create_sino(np.reshape(x,im_size), proj_id)[1].ravel()
+        adjoint = lambda y: astra.create_backprojection(np.reshape(y,(det_count,np.size(angles))),proj_id)[1].ravel()
+
+        super().__init__(forward,adjoint,range_geometry,domain_geometry)
+
+
---
+++ demos/demo11_CT.py
@@ -0,0 +1,40 @@
+# %%
+sys.path.append("..") 
+import cuqi
+import astra
+import numpy as np
+import scipy.io as io
+import matplotlib.pyplot as plt
+import scipy.io as io
+from PIL import Image
+
+from cuqi.modelCT import CT
+# %%
+N = 32; n=N**2
+p = 32
+q = np.linspace(0,np.pi,30)
+m = p*np.size(q)
+
+x = io.loadmat("data/phantom512.mat")["X"]
+x= np.array(Image.fromarray(x).resize((N,N)))
+x = x.ravel()
+model = CT(im_size=(N,N),det_count=p,angles=q)
+
+#%%
+prior = cuqi.distribution.Gaussian(np.zeros(n),1)
+noise = cuqi.distribution.Gaussian(np.zeros(m),0.1)
+
+b_exact = model@x
+e = noise.sample().flatten()
+b=b_exact+e
+
+# %%
+IP=cuqi.problem.Type1(b,model,noise,prior)
+results=IP.sample(5000)
+#%%
+x_mean = np.mean(results.samples,axis=-1)
+model.domain_geometry.plot(x_mean)
+#%%
+x_mean = np.std(results.samples,axis=-1)
+model.domain_geometry.plot(x_mean)
+# %%
