b6327598b5764a50748833f038999e2221181452
nabr <nabr@dtu.dk>
2021-09-09 14:48:43 +0200

    Add Shifted geometry CT model

--- demos/demo11_CT.py
+++ demos/demo11_CT.py
@@ -5,18 +5,24 @@ import numpy as np
 import scipy.io as io
 import matplotlib.pyplot as plt
 from PIL import Image
+from scipy import sparse
 
-from cuqi.modelCT import CT
+from cuqi.modelCT import CT, CTshifted
 # %%
-N = 32; n=N**2
-p = 32
-q = np.linspace(0,np.pi,30)
-m = p*np.size(q)
+#model = CT()
+model = CTshifted()
+
+N = model.dims[1][0]
+n = N**2
+m = model.dims[0][0]*model.dims[0][1]
 
 x = io.loadmat("data/phantom512.mat")["X"]
 x= np.array(Image.fromarray(x).resize((N,N)))
 x = x.ravel()
-model = CT(im_size=(N,N),det_count=p,angles=q)
+
+# %%
+b = model@x
+plt.imshow(np.reshape(model.adjoint(b),(N,N))); plt.title("Back projection")
 
 #%%
 prior = cuqi.distribution.Gaussian(np.zeros(n),1)
