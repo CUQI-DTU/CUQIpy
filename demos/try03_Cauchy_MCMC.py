bdfc74e682175e2e3c123bd734e3be6666738934
amaal <amaal@dtu.edu>
2021-09-21 13:42:37 +0000

    fix typo

--- demos/try03_Cauchy_MCMC.py
+++ demos/try03_Cauchy_MCMC.py
@@ -62,7 +62,7 @@ print('Elapsed time:', time.time() - ti)
 med_xpos = np.median(x_s, axis=1) # sp.stats.mode
 sigma_xpos = x_s.std(axis=1)
 lo95, up95 = np.percentile(x_s, [2.5, 97.5], axis=1)
-relerr = round(np.linalg.norm(med_xpos - test.f_true)/norm_f*100, 2)
+relerr = round(np.linalg.norm(med_xpos - test.exactSolution)/norm_f*100, 2)
 print('\nRelerror median:', relerr, '\n')
 
 # =============================================================================
