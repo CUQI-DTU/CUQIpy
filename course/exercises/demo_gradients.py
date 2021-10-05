ad619190e6e4d1621fbb804f3848bfb9fc6810e9
nabr <nabr@dtu.dk>
2021-10-05 20:34:15 +0200

    Add more comments to demo file

--- course/exercises/demo_gradients.py
+++ course/exercises/demo_gradients.py
@@ -14,6 +14,7 @@ import cuqi
 %autoreload 2
 
 # %%
+# Load cuqi deblur model and data
 TP = cuqi.testproblem.Deblur()
 model = TP.model #Deblur model
 data = TP.data #Data from deblur problem
@@ -22,24 +23,29 @@ n = model.domain_geometry.dim
 m = model.range_geometry.dim
 
 # %%
+# Define Gaussian likelihood and prior
 likelihood = cuqi.distribution.GaussianGen(model, sigma2)
 prior      = cuqi.distribution.GaussianGen(0, 0.1*np.ones(n))
 
 # %%
+# Define potential of posterior (returns logpdf and gradient w.r.t x)
 def potential(x):
     logpdf = -prior.logpdf(x)-likelihood(x=x).logpdf(data) 
     grad   = -prior.gradient(x)-likelihood.gradient(data,x=x)
     return logpdf,grad
 
 # %%
+# Solve posterior problem using BFGS
 solver = cuqi.solver.L_BFGS_B(potential,np.random.randn(n))
 x_MAP = solver.solve()
 
 # %%
+# Plot result
 model.domain_geometry.plot(x_MAP)
 model.domain_geometry.plot(TP.exactSolution)
 
 # %%
+# Compare with MAP computed using BayesianProblem (within the testproblem)
 prior2 = cuqi.distribution.Gaussian(np.zeros(n), np.sqrt(0.1), np.eye(n))
 TP.prior = prior2
 x_MAP2 = TP.MAP()
