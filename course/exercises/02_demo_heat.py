42879df267ba64f689aa6776975f8501dfdc947b
babak <babak@gray.local>
2021-10-29 10:45:50 +0200

    sine KL added to heat

--- course/exercises/02_demo_heat.py
+++ course/exercises/02_demo_heat.py
@@ -5,7 +5,7 @@ sys.path.append("../..")
 import cuqi
 
 # domain definition
-N = 201            # spatial discretization 
+N = 50            # spatial discretization 
 L = 1
 T = 0.2
 
@@ -14,15 +14,15 @@ mean = 0.0
 var = 10
 lc = 0.2
 p = 2
-C_YY = lambda x1, x2: var*np.exp( -(1/p) * (abs(x1-x2)/lc)**p )
-KL_map = lambda x: np.exp(x)
+C_YY = lambda x1, x2: var*np.exp( -(1/p) * (abs( x1 - x2 )/lc)**p )
+KL_map = lambda x: x
 
 # define model 
 model = cuqi.model.Heat_1D(N=N, L=L, T=T, field_type=None, KL_map=KL_map)
 x = model.domain_geometry.grid
 
 # forward propagation
-Ns = 20
+Ns = 100
 
 # 1. random field: point-wise 
 XX, YY = np.meshgrid(x, x, indexing='ij')
@@ -48,11 +48,8 @@ ax1 = plt.subplot(122)
 ax1.plot(x, u)
 plt.tight_layout()
 
-
-# 2. random field: KL expansion 
-d_KL = 50
-model = cuqi.model.Heat_1D(N=N, L=L, T=T, field_type='CustomKL', \
-                    cov_fun=C_YY, mean=mean, std=np.sqrt(var), d_KL=d_KL, KL_map=KL_map)
+# 2. random field: KL expansion sine basis 
+model = cuqi.model.Heat_1D(N=N, L=L, T=T, field_type='KL', KL_map=KL_map)
 x = model.domain_geometry.grid
 #
 theta = np.random.normal(0, 1, size=(d_KL, Ns)) # KL coefficients
@@ -75,8 +72,34 @@ ax1 = plt.subplot(122)
 ax1.plot(x, u)
 plt.tight_layout()
 
+# 3. random field: KL expansion custom
+d_KL = 50
+model = cuqi.model.Heat_1D(N=N, L=L, T=T, field_type='CustomKL', \
+                    cov_fun=C_YY, mean=mean, std=np.sqrt(var), d_KL=d_KL, KL_map=KL_map)
+x = model.domain_geometry.grid
+#
+theta = np.random.normal(0, 1, size=(d_KL, Ns)) # KL coefficients
+u = np.empty((N, Ns))  # store pressure field realizations
+kappa = np.empty((N, Ns))
+for i in range(Ns):
+    kappa[:, i] = model.domain_geometry.apply_map(theta[:, i])
+    u[:, i] = model.forward(theta[:, i])
+    
+mu_kappa = np.mean(kappa, axis=1)
+sigma_kappa = np.std(kappa, ddof=0, axis=1)
+mu_u = np.mean(u, axis=1)
+sigma_u = np.std(u, ddof=0, axis=1)
 
-# 3. random field: step function
+plt.figure(3)
+ax1 = plt.subplot(121)
+ax1.plot(x, kappa)
+ax1.plot(x, mu_kappa, 'k-', linewidth=2)
+ax1 = plt.subplot(122)
+ax1.plot(x, u)
+plt.tight_layout()
+
+
+# 4. random field: step function
 model = cuqi.model.Heat_1D(N=N, L=L, T=T, field_type='Step', KL_map=KL_map)
 x = model.domain_geometry.grid
 #
@@ -92,7 +115,7 @@ sigma_kappa = np.std(kappa, ddof=0, axis=1)
 mu_u = np.mean(u, axis=1)
 sigma_u = np.std(u, ddof=0, axis=1)
 
-plt.figure(3)
+plt.figure(4)
 ax1 = plt.subplot(121)
 ax1.plot(x, kappa)
 ax1.plot(x, mu_kappa, 'k-', linewidth=2)
