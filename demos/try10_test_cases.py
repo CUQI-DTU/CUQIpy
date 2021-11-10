4c535f1f9e719a53f1ec251a9ea381e2cd4fcc69
amaal <amaal@dtu.edu>
2021-11-10 12:20:19 +0000

    idea for test cases: demo

---
+++ demos/try10_test_cases.py
@@ -0,0 +1,33 @@
+
+#%%
+sys.path.append("..") 
+import numpy as np
+from scipy.sparse import diags
+import matplotlib.pyplot as plt
+import cuqi
+
+# %% Set up Deconvolution test problem
+# Parameters for Deconvolution problem
+dim = 128
+kernel = ["Gauss","Sinc","vonMises"]
+phantom = ["Gauss","Sinc","vonMises","Square","Hat","Bumps","DerivGauss"]
+noise_type = ["Gaussian","ScaledGaussian"]
+noise_std = 0.05
+
+#%% Deconvolution Test problem
+model, data, noise = cuqi.testproblem.Deconvolution.get_building_blocks(
+    dim = dim,
+    kernel=kernel[0],
+    phantom=phantom[3],
+    noise_type=noise_type[0],
+    noise_std = noise_std
+)
+
+#%% Deblur Test problem 
+model, data, noise = cuqi.testproblem.Deblur.get_building_blocks()
+
+#%%  Poisson_1D Test problem
+KL_map = lambda x: np.exp(x)
+f = lambda xs: 10*np.exp( -( (xs - 0.5)**2 ) / 0.02) 
+
+model, data, noise = cuqi.testproblem.Poisson_1D.get_building_blocks(dim=501, endpoint=1, source=f, field_type=None, KL_map=KL_map)
