bd6163f6e5dff861758ce0be30866439a77a6d07
nabr <nabr@dtu.dk>
2021-11-11 17:01:05 +0100

    Show-case fun2par of MappedGeometry

--- demos/try10_mappedGeometry.py
+++ demos/try10_mappedGeometry.py
@@ -15,10 +15,13 @@ geom = StepExpansion(np.linspace(0,1,10))
 # %%
 geom.plot(vals)
 # %% mapped step geometry
-mapped_geom = MappedGeometry(geom,lambda x: np.exp(x))
+mapped_geom = MappedGeometry(geom,lambda x: np.exp(x),lambda x: np.log(x))
 mapped_geom
 #%%
 mapped_geom.plot(vals)
 
 # %%
 geom.fun2par(geom.par2fun(vals))
+
+# %%
+mapped_geom.fun2par(mapped_geom.par2fun(vals))
