c0c2cdc4143bad8b09970f27ede182be4b98e3d9
nabr <nabr@dtu.dk>
2021-11-16 12:54:03 +0100

    Add demo for JoinedGaussian

---
+++ demos/demo20_JoinedGaussian.py
@@ -0,0 +1,22 @@
+# %%
+import sys
+sys.path.append("..") 
+import cuqi
+import numpy as np
+
+# %%
+# Define two Gaussians by the mean and sqrtprec
+n = 128
+
+X1 = cuqi.distribution.GaussianCov(np.ones(n),np.eye(n))
+X2 = cuqi.distribution.GMRF(3*np.ones(n),1,n,1,'zero')
+
+# %%
+# Define joined Gaussian
+
+X = cuqi.distribution.JoinedGaussianSqrtPrec([X1.mean,X2.mean],[X1.sqrtprec,X2.sqrtprec])
+
+# %%
+# Use this Gaussian as prior in test problem
+TP = cuqi.testproblem.Deconvolution(prior=X)
+TP.UQ()
