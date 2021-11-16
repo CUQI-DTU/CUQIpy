6ae5cc5c7aa436ea0de10f882ceea5b906438c90
nabr <nabr@dtu.dk>
2021-11-16 15:53:35 +0100

    Add demo that compared single priors with joined priors

--- demos/demo20_JoinedGaussian.py
+++ demos/demo20_JoinedGaussian.py
@@ -6,22 +6,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # %%
-# Define two Gaussians by the mean and sqrtprec
+# Define two Gaussians on the entire domain
 n = 128
 
-X1 = cuqi.distribution.GaussianCov(np.ones(n),np.eye(n))
-X2 = cuqi.distribution.GMRF(3*np.ones(n),1,n,1,'zero')
+z = np.zeros(n//4); o = np.ones(n//4) #Building blocks for defining means and covariances
+
+X1 = cuqi.distribution.GaussianPrec(np.hstack((z,o,o,z)),np.diag(np.hstack((100*o,2*o,2*o,100*o))))
+X2 = cuqi.distribution.GMRF(np.hstack((o,o,o,o)),25,n,1,'zero')
+# %%
+plt.figure(); X1.sample(5).plot(); plt.title("X1 (Gaussian with high probability zeros in ends)")
+plt.figure(); X2.sample(5).plot(); plt.title("X2 (GMRF)")
 
 # %%
-# Define joined Gaussian
-
-X = cuqi.distribution.JoinedGaussianSqrtPrec([X1.mean,X2.mean],[X1.sqrtprec,X2.sqrtprec])
+# First solve using only X1 "zero tails" prior
+results_X1 = cuqi.testproblem.Deconvolution(prior=X1).sample_posterior(5000)
 
 # %%
-# Use this Gaussian as prior in test problem
+# Second solve using only GMRF prior
+results_X2 = cuqi.testproblem.Deconvolution(prior=X2).sample_posterior(5000)
+
+# %%
+# Third, define joined Gaussian that incorporates high probability of zero in both ends.
+means = [X1.mean,X2.mean]
+sqrtprecs = [X1.sqrtprec,X2.sqrtprec.toarray()] #Either all sparse or all non-sparse
+X = cuqi.distribution.JoinedGaussianSqrtPrec(means,sqrtprecs)
+
+# and solve using joined prior
 TP = cuqi.testproblem.Deconvolution(prior=X)
-TP.UQ()
+results_X = TP.sample_posterior(5000)
 
+# %%
+# Compare results
+plt.figure(); results_X1.plot_ci(95,exact=TP.exactSolution); plt.title("Posterior with X1 (zero tail Gaussian)"); plt.ylim([-0.5,1.5])
+plt.figure(); results_X2.plot_ci(95,exact=TP.exactSolution); plt.title("Posterior with X2 (GMRF)"); plt.ylim([-0.5,1.5])
+plt.figure(); results_X.plot_ci(95,exact=TP.exactSolution); plt.title("Posterior with X (joined)"); plt.ylim([-0.5,1.5])
 # %%
 # Maximum a posteriori estimation (logpdf is not implemented, so throws an error.)
 #x_MAP = TP.MAP()
