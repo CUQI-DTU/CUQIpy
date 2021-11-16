fa7553d7f3e87fa7ad9ac981cb76fb1f55904100
amaal <amaal@dtu.edu>
2021-11-16 14:07:11 +0000

    simplify script

--- demos/try10_test_cases.py
+++ demos/try10_test_cases.py
@@ -6,28 +6,15 @@ from scipy.sparse import diags
 import matplotlib.pyplot as plt
 import cuqi
 
-# %% Set up Deconvolution test problem
-# Parameters for Deconvolution problem
-dim = 128
-kernel = ["Gauss","Sinc","vonMises"]
-phantom = ["Gauss","Sinc","vonMises","Square","Hat","Bumps","DerivGauss"]
-noise_type = ["Gaussian","ScaledGaussian"]
-noise_std = 0.05
 
 #%% Deconvolution Test problem
-model, data, problem_info = cuqi.testproblem.Deconvolution.get_components(
-    dim = dim,
-    kernel=kernel[0],
-    phantom=phantom[3],
-    noise_type=noise_type[0],
-    noise_std = noise_std
-)
+model_deconv, data_deconv, problem_info_deconv = cuqi.testproblem.Deconvolution.get_components()
 
 #%% Deblur Test problem 
-model, data, problem_info = cuqi.testproblem.Deblur.get_components()
+model_deblur, data_deblur, problem_info_deblur = cuqi.testproblem.Deblur.get_components()
 
 #%%  Poisson_1D Test problem
 KL_map = lambda x: np.exp(x)
 f = lambda xs: 10*np.exp( -( (xs - 0.5)**2 ) / 0.02) 
 
-model, data, problem_info = cuqi.testproblem.Poisson_1D.get_components(dim=501, endpoint=1, source=f, field_type=None, KL_map=KL_map)
+model_poisson, data_poisson, problem_info_poisson = cuqi.testproblem.Poisson_1D.get_components(dim=501, endpoint=1, source=f, field_type=None, KL_map=KL_map)
