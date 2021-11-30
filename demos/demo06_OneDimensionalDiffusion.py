9258ce405109300d82f321d94b50784dbf4b1c82
amaal <amaal@dtu.edu>
2021-11-30 11:40:54 +0000

    Update demo to reduce problem dimension and reflect update in the library

--- demos/demo06_OneDimensionalDiffusion.py
+++ demos/demo06_OneDimensionalDiffusion.py
@@ -1,4 +1,5 @@
 
+#%%
 import sys
 import time
 import numpy as np
@@ -6,32 +7,35 @@ import matplotlib.pyplot as plt
 sys.path.append("../")
 import cuqi
 
+np.random.seed(0)
 #%% Set up and solve cuqi problem that uses FEniCSPDEModel 
 
 #%% Problem of form b = A(x) + e
 # Create operator A
-A = cuqi.PDEmodel.FEniCSDiffusion1D(measurement_type = 'potential')
+model = cuqi.fenics.pde.FEniCSDiffusion1D(measurement_type = 'potential', shape = (20,))
 
 #%% Create & plot prior 
-n = A.dim
+n = model.dim
 pr_mean = np.zeros(n)
 prior = cuqi.distribution.GMRF(pr_mean,25,n,1,'zero') 
-ps = cuqi.samples.Samples(prior.sample(100))
+ps = prior.sample(100)
 plt.figure()
 ps.plot_ci(95,exact=np.zeros(n))
 
 #%% Create noise (e) and data (b) 
 true_m = prior.sample(1)
-true_u = A.forward(true_m)
-noise_std = 0.01 * np.max(true_u)
-e = cuqi.distribution.Gaussian(np.zeros(n), noise_std, np.eye(n))
-b = true_u + e.sample().T 
+true_u = model.forward(true_m)
+noise_std = float(0.01 * np.max(true_u))
+likelihood = cuqi.distribution.Gaussian(mean = model, std = noise_std)
+data = likelihood(x=true_m).sample() 
 
 #%% Create cuqi problem (sets up likelihood and posterior) 
-IP = cuqi.problem.Type1(b,A,e,prior)
+IP = cuqi.problem.BayesianProblem( likelihood, prior, data) 
 
 #%% Sample & plot posterior
-results = IP.sample(Ns=10000) 
+results = IP.sample_posterior(5000) 
+
+#%% plot
 plt.figure()
-results.plot_ci(95,exact=true_m)
+results.plot_ci(95,exact=true_m, plot_par = True)
 plt.show()
