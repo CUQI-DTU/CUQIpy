81654b97eb8efa8705f00bc724a19be5e52ec145
Jakob S. Jorgensen <jakj@dtu.dk>
2021-12-03 14:19:52 +0100

    Add gradient in demo of UserDefined

--- demos/demo21_UserDefinedDistribution.py
+++ demos/demo21_UserDefinedDistribution.py
@@ -31,8 +31,9 @@ plt.plot(grid, NU.logpdf(grid), '--')
 #%% Compare samples
 N.sample()
 
-#%%
-NU.sample()
+#%%  Cannot sample yet, as sample method not defined. This would fail:
+
+#NU.sample()
 
 #%%
 NU.sample_func = lambda : mu1 + std1*np.random.randn(dim1,1)
@@ -45,6 +46,7 @@ NS = 10000
 s_N  = N.sample(NS)
 s_NU = NU.sample(NS)
 
+plt.figure()
 s_N.hist_chain(0, bins=100)
 s_NU.hist_chain(0, bins=100)
 
@@ -64,11 +66,22 @@ logpdf_func2 = lambda xx: -0.5*(dim2*np.log(2*np.pi) + np.sum(np.log(cov2)) + \
         np.sum(np.square( (xx-mu2) @ diags(np.sqrt(1/cov2))), axis=-1))
 GU = cuqi.distribution.UserDefinedDistribution(logpdf_func=logpdf_func2)
 
-#%%
+#%% Define a try point at which to compare logpdf and gradient.
 try_point = np.array([1.5, 2.5, 3.5, 4.5, 5.5])
 
+#%%
 G.logpdf(try_point)
 
 #%%
 GU.logpdf(try_point)
+
+# %%  Now add a gradient function as well and compare evaluated at the try point
+GU.gradient_func = lambda xx: -diags(1/cov2) @ (xx - mu2)
+
+# %%
+G.gradient(try_point)
+
+# %%
+GU.gradient(try_point)
+
 # %%
