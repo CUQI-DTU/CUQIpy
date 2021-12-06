d7135c1aa39fb19863c5a29976d55b5610d361eb
amaal <amaal@dtu.edu>
2021-12-06 12:47:27 +0000

    we use set FEniCSDiffusion as testproblem instead of model

--- cuqi/fenics/testproblem.py
+++ cuqi/fenics/testproblem.py
@@ -1,119 +1,65 @@
-class FEniCSDiffusion(PDEModel):
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+sys.path.append("../../")
+import dolfin as dl
+import warnings
+import cuqi
+from cuqi.problem import BayesianProblem
+import dolfin as dl
+import ufl
 
-    def __init__(self, mesh, Vh, bc=None, bc0=None, f= None,\
-        measurement_type = 'potential', parameter_type="conductivity_field"):
 
+class FEniCSDiffusion1D(BayesianProblem):
 
-        if parameter_type == "fixed_radius_inclusion": #TODO: Add function to create form based on parameter_type 
-            R = 0.1
-            Gamma = 1
-            tol = 1E-6  
-            self.kappa = lambda m: dl.Expression('pow(x[0]-m0,2)+pow(x[1]-m1,2) <= pow(R,2) + tol ? 1+Gamma : 1', \
-                                  degree=1, tol=tol, m0=m.vector()[0], m1=m.vector()[1], R=R, Gamma = Gamma)#TODO: make the form differentiable with respect to m
-            domain_geometry = cuqi.geometry.Discrete(['x','y'])
-            def form(u,m,p):
-                return self.kappa(m)*ufl.inner(ufl.grad(u), ufl.grad(p))*ufl.dx - f*p*ufl.dx 
+    def __init__(self, dim = 100, L = 1, exactSolution = None, SNR = 100):
 
-        elif parameter_type == "conductivity_field":
-            domain_geometry = cuqi.fenics.geometry.FenicsContinuous(Vh[1])
-            def form(u,m,p):
-                return ufl.exp(m)*ufl.inner(ufl.grad(u), ufl.grad(p))*ufl.dx - f*p*ufl.dx 
+        def u_boundary(x, on_boundary):
+            return on_boundary
+        
+        def form(u,m,p):
+            return ufl.exp(m)*ufl.inner(ufl.grad(u), ufl.grad(p))*ufl.dx 
+        
+        #%% Create PDE model
+        mesh = dl.IntervalMesh(dim, 0,L)
+        solution_function_space = dl.FunctionSpace(mesh, 'Lagrange', 1)
+        parameter_function_space = dl.FunctionSpace(mesh, 'Lagrange', 1)
+        dirichlet_bc_expression = dl.Expression("x[0]", degree=1)
+        dirichlet_bc = dl.DirichletBC(solution_function_space, dirichlet_bc_expression, u_boundary)
+        PDE = cuqi.fenics.pde.SteadyStateLinearFEniCSPDE( form, mesh, solution_function_space, parameter_function_space, dirichlet_bc,observation_operator=None)
 
-                #self, form, mesh, Vh, bc=None, bc0=None, obs_op=None
-        obs_op = self._create_obs_op(measurement_type)
-        super().__init__(form, mesh, Vh, bc=bc, bc0=bc0, f=f, obs_op = obs_op, 
-                         range_geometry=None , domain_geometry = domain_geometry)
-    
+        domain_geometry = cuqi.fenics.geometry.FenicsContinuous(parameter_function_space)
+        range_geometry = cuqi.fenics.geometry.FenicsContinuous(solution_function_space)
+        
+        # Prepare model
+        model = cuqi.model.PDEModel(PDE,range_geometry,domain_geometry)
 
-    def _create_obs_op(cls, measurement_type):
+        pr_mean = np.zeros(domain_geometry.dim)
+        prior = cuqi.distribution.GMRF(pr_mean,25,domain_geometry.dim,1,'zero') 
+        # Set up exact solution
+        if exactSolution is None:
 
-        if measurement_type == 'potential':
-            obs_op = lambda m, u: u 
-        elif measurement_type == 'gradu_squared':
-            obs_op = lambda m, u: dl.inner(dl.grad(u),dl.grad(u))
-        elif measurement_type == 'power_density':
-            obs_op = lambda m, u: dl.exp(m)*dl.inner(dl.grad(u),dl.grad(u))
-        elif measurement_type == 'sigma_u':
-            obs_op = lambda m, u: dl.exp(m)*u
-        elif measurement_type == 'sigma_norm_gradu':
-            obs_op = lambda m, u: dl.exp(m)*dl.sqrt(dl.inner(dl.grad(u),dl.grad(u)))
-        else:
-            raise NotImplementedError
-        return obs_op
+            exactSolution = prior.sample()
+            #np.sin(np.linspace(0,L,parameter_function_space.dim()))+ 2 
 
-    
-class FEniCSDiffusion1D(FEniCSDiffusion):
+        # Generate exact data
+        b_exact = model.forward(exactSolution,is_par=False)
 
-    def __init__(self, mesh=None, Vh=None, bc=None, bc0=None, f=None,\
-                 measurement_type = 'potential', parameter_type="conductivity_field", shape= (50,)):
-        if shape is not None:
-            if len(shape) !=1: 
-                raise ValueError('shape need to be a one dimensional tuple (e.g. (20,))') 
-            N = np.prod(shape)
-        else: 
-            N = 30
+        # Add noise to data
+        sigma = np.linalg.norm(b_exact)/SNR
+        sigma2 = sigma*sigma # variance of the observation Gaussian noise
+        data = b_exact + np.random.normal( 0, sigma, b_exact.shape )
 
-        if mesh is None: 
-            mesh = dl.UnitIntervalMesh(N)
-        elif (isinstance(mesh, tuple) and len(mesh) ==1) or isinstance(mesh, (int,np.integer)):
-            if isinstance(mesh, tuple): mesh = mesh[0] 
-            mesh = dl.UnitIntervalMesh(mesh)
+        likelihood = cuqi.distribution.GaussianCov(model, sigma2*np.eye(range_geometry.dim))
 
-        if Vh is None:
-            Vh_STATE = dl.FunctionSpace(mesh, 'Lagrange', 1)
-            Vh_PARAMETER = dl.FunctionSpace(mesh, 'Lagrange', 1)
-            Vh_ADJOINT = dl.FunctionSpace(mesh, 'Lagrange', 1)
-            Vh = [Vh_STATE, Vh_PARAMETER, Vh_ADJOINT]
+        
+        #cuqi.distribution.GaussianCov(np.zeros(domain_geometry.dim), 1)
 
-        if bc is None:
-            assert(bc0 == None), "If bc0 is specified, bc must be specified as well."
-            def u_boundary(x, on_boundary):
-                return on_boundary
+        # Initialize Deconvolution as BayesianProblem problem
+        super().__init__(likelihood,prior,data)
 
-            u_bdr = dl.Expression("x[0]", degree=1)
-            bc = dl.DirichletBC(Vh_STATE, u_bdr, u_boundary)
-
-            u_bdr0 = dl.Constant(0.0)
-            bc0 = dl.DirichletBC(Vh_ADJOINT, u_bdr0, u_boundary)
-
-        if f is None:
-            f = dl.Constant(0.0)
-
-        super().__init__(mesh, Vh, bc=bc, bc0=bc0, f=f, measurement_type = measurement_type,\
-                         parameter_type=parameter_type)
-
-class FEniCSDiffusion2D(FEniCSDiffusion):
-
-    def __init__(self, mesh=None, Vh=None, bc=None, bc0=None, f=None,\
-                 measurement_type = 'potential', parameter_type="conductivity_field"):
-        if mesh is None:
-            mesh = dl.UnitSquareMesh(40, 40)
-        elif isinstance(mesh, tuple) and len(mesh) ==2: 
-            mesh = dl.UnitSquareMesh(mesh[0], mesh[1])
-
-        if Vh is None: #TODO: creating spaces should be part of the super class (diffusion)
-            Vh_STATE = dl.FunctionSpace(mesh, 'Lagrange', 1)
-            if parameter_type=="conductivity_field":
-                Vh_PARAMETER = dl.FunctionSpace(mesh, 'Lagrange', 1)
-            elif parameter_type == "fixed_radius_inclusion":    
-                Vh_PARAMETER = dl.VectorFunctionSpace(mesh, "R", degree=0, dim=2)
-            Vh_ADJOINT = dl.FunctionSpace(mesh, 'Lagrange', 1)
-            Vh = [Vh_STATE, Vh_PARAMETER, Vh_ADJOINT]
-
-        if bc is None:
-            assert(bc0 == None), "If bc0 is specified, bc must be specified as well."
-            def u_boundary(x, on_boundary):
-                return on_boundary
-
-            u_bdr = dl.Expression("x[0]", degree=1)
-            bc = dl.DirichletBC(Vh_STATE, u_bdr, u_boundary)
-
-            u_bdr0 = dl.Constant(0.0)
-            bc0 = dl.DirichletBC(Vh_ADJOINT, u_bdr0, u_boundary)
-
-        if f is None:
-            f = dl.Constant(0.0)
-
-        super().__init__(mesh, Vh, bc=bc, bc0=bc0, f=f, measurement_type = measurement_type,\
-                         parameter_type=parameter_type)
+        # Store exact values
+        self.exactSolution = exactSolution
+        self.exactData = b_exact
+        self.infoString = f"Noise type: Additive i.i.d. noise with mean zero and signal to noise ratio: {SNR}"
 
