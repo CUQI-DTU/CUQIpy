notation fixes on new kl

cuqi/geometry.py
@@ -456,23 +456,21 @@ class KLExpansion_Full(Continuous1D):
     # init function defining paramters for the KL expansion
     def __init__(self, grid, params, axis_labels=['x'],**kwargs):
 
-        el = params['cor_len']
-        self.tau2 = 1./el/el
-        nu = params["nu"]
-        self.gamma = nu+1.
-        self.var = params["std"]**2
-        self.N = len(grid)
-        
         super().__init__(grid, axis_labels,**kwargs)
 
-        #self.modes = np.concatenate( [range(0, int(self.N / 2) ) , range(-int(self.N / 2) ,0) ] )
-        self.modes = np.arange(0,self.N)
+        cl = params['cor_len']
+        tau2 = 1./cl/cl
+        nu = params["nu"]
+        gamma = nu+1.
+        self.var = params["std"]**2
 
-        self.coefs =  np.float_power( self.tau2,self.gamma ) * np.float_power(self.tau2+self.modes**2,-self.gamma)
+        modes = np.arange(0,self.dim)
+
+        self.coefs =  np.float_power( tau2,gamma ) * np.float_power(tau2+modes**2,-gamma)
 
     # computes the real function out of expansion coefs
     def par2fun(self,p):
-        freq = np.zeros(self.N)
+        freq = np.zeros(self.dim)
         m = len(p)
         freq[:m] = p
         temp = freq*self.coefs

cuqi/testproblem.py
@@ -433,7 +433,7 @@ class Poisson_1D(BayesianProblem):
 
     """
     def __init__(self, dim=128, dim_obs=None, endpoint=1, source=lambda xs: 10*np.exp( -( (xs - 0.5)**2 ) / 0.02), field_type=None, field_params=None, KL_map=None, KL_imap=None, SNR=200):
-        
+
         if dim_obs is None:
             dim_obs = dim-1
 