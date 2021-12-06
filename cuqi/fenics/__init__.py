76a60705adca25342a70c95d8dbe57360472ea61
amaal <amaal@dtu.edu>
2021-12-06 12:45:01 +0000

    update the way we check for fenics import error

--- cuqi/fenics/__init__.py
+++ cuqi/fenics/__init__.py
@@ -1,4 +1,13 @@
-from . import pde
-from . import geometry
+try: 
+    from . import pde
+    from . import geometry
+    from . import testproblem
+    from . import model
+    __all__ = [pde, geometry, testproblem, model]
+    print("cuqi.fenics module is loaded.")
 
-__all__ = [pde, geometry]
+except ModuleNotFoundError as error:
+    if error.name == "dolfin" or error.name == "ufl": 
+        pass
+    else:
+        raise error
