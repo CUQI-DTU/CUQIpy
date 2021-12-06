03eed2183d6c4f3a6c58e55a1634ac88b284e435
amaal <amaal@dtu.edu>
2021-12-06 11:59:34 +0000

    changes to the scrip to use new interface

--- demos/try12_FEniCSPDE.py
+++ demos/try12_FEniCSPDE.py
@@ -1,37 +1,27 @@
 
 #%%
 import sys
-import time
 import numpy as np
 import matplotlib.pyplot as plt
 sys.path.append("../")
 import cuqi
-import dolfin as dl
+ 
+dim = 30
+#%%
+model, data, problemInfo = cuqi.fenics.testproblem.FEniCSDiffusion1D.get_components(dim = dim)
+# %%
+prior = cuqi.distribution.GMRF(np.zeros(model.domain_dim),25,model.domain_dim,1,'zero') 
 
-try: 
-    import dolfin as dl
-    import ufl
-except Exception as error:
-    warnings.warn(error.msg)
+SNR = 100
+sigma = np.linalg.norm(problemInfo.exactData)/SNR 
+likelihood = cuqi.distribution.GaussianCov(model, sigma**2*np.eye(model.range_dim))
+posterior = cuqi.distribution.Posterior(likelihood, prior, data)
+# %%
+sampler = cuqi.sampler.pCN(posterior)
+samples = sampler.sample_adapt(1000)
+# %%
+samples.plot_ci(95, plot_par = True, exact = problemInfo.exactSolution)
+plt.xticks(np.arange(posterior.dim)[::5]);
 
-
-#%% Define methods
-def u_boundary(x, on_boundary):
-    return on_boundary
-
-def form(u,m,p):
-    return ufl.exp(m)*ufl.inner(ufl.grad(u), ufl.grad(p))*ufl.dx 
-
-#%% Create PDE model
-dim = 100
-mesh = dl.UnitIntervalMesh(dim)
-solution_function_space = dl.FunctionSpace(mesh, 'Lagrange', 1)
-parameter_function_space = dl.FunctionSpace(mesh, 'Lagrange', 1)
-u_bdr = dl.Expression("x[0]", degree=1)
-dirichlet_bc = dl.DirichletBC(solution_function_space, u_bdr, u_boundary)
-pde = cuqi.fenics.pde.SteadyStateLinearFEniCSPDE( form, mesh, solution_function_space, parameter_function_space, dirichlet_bc,observation_operator=None)
-
-#%% Test the PDE model
-pde.assemble(np.sin(np.arange(dim)))
-sol = pde.solve()
-dl.plot(sol)
+# %%
+samples.plot([100,300])
