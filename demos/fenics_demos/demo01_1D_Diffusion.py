5c664986c8e6cf2adc19cb4bcad95dc9cee8230a
amaal <amaal@dtu.edu>
2021-12-07 11:44:37 +0000

    try observation operator

--- demos/fenics_demos/demo01_1D_Diffusion.py
+++ demos/fenics_demos/demo01_1D_Diffusion.py
@@ -5,10 +5,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 sys.path.append("../../")
 import cuqi
+from math import floor
  
+np.random.seed(0)
+
 dim = 30
+N= dim + 1
+
+myExactSolution = np.zeros(N)
+myExactSolution[:floor(N/3)] = .2
+myExactSolution[floor(N/3):floor(2*N/3)] = 1
+myExactSolution[floor(2*N/3):] = .5
 #%%
-model, data, problemInfo = cuqi.fenics.testproblem.FEniCSDiffusion1D.get_components(dim = dim)
+model, data, problemInfo = cuqi.fenics.testproblem.FEniCSDiffusion1D.get_components(dim = dim, exactSolution = myExactSolution, observation_operator='gradu_squared' )
 # %%
 prior = cuqi.distribution.GMRF(np.zeros(model.domain_dim),25,model.domain_dim,1,'zero') 
 
@@ -18,10 +27,10 @@ likelihood = cuqi.distribution.GaussianCov(model, sigma**2*np.eye(model.range_di
 posterior = cuqi.distribution.Posterior(likelihood, prior, data)
 # %%
 sampler = cuqi.sampler.pCN(posterior)
-samples = sampler.sample_adapt(1000)
+samples = sampler.sample_adapt(5000)
 # %%
 samples.plot_ci(95, plot_par = True, exact = problemInfo.exactSolution)
-plt.xticks(np.arange(posterior.dim)[::5]);
+#plt.xticks(np.arange(posterior.dim)[::5]);
 
 # %%
 samples.plot([100,300])
