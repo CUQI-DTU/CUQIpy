6fcda692e1a712e96b1bc559c62117b7baf71a19
nabr <nabr@dtu.dk>
2021-12-07 12:05:11 +0100

    Add (perhaps) more robust import strategy for 3rd party engines

--- cuqi/__init__.py
+++ cuqi/__init__.py
@@ -1,4 +1,3 @@
-from . import data
 from . import diagnostics
 from . import distribution
 from . import geometry
@@ -12,5 +11,30 @@ from . import solver
 from . import testproblem
 from . import utilities
 
-# Import engines
-from . import fenics 
+# ==== Import engines ====
+# Silently fails on import cuqi,
+# but gives error on import cuqi.engine
+
+engines = []
+
+# FEniCS
+try:
+    from . import fenics
+    engines.append("fenics")
+except ModuleNotFoundError:
+    pass
+
+# ASTRA-TOOLBOX
+try:
+    from . import astra
+    engines.append("astra")
+except ModuleNotFoundError:
+    pass
+
+#Print extra modules
+
+if len(engines)>0:
+    print("3rd party engines available in cuqi:",end=" ")
+    print(*engines)
+
+del engines
--- cuqi/fenics/__init__.py
+++ cuqi/fenics/__init__.py
@@ -1,13 +1,4 @@
-try: 
-    from . import pde
-    from . import geometry
-    from . import testproblem
-    from . import model
-    __all__ = [pde, geometry, testproblem, model]
-    print("cuqi.fenics module is loaded.")
-
-except ModuleNotFoundError as error:
-    if error.name == "dolfin" or error.name == "ufl": 
-        pass
-    else:
-        raise error
+from . import pde
+from . import geometry
+from . import testproblem
+__all__ = [pde, geometry, testproblem]
