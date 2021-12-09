081aa2a2c37ea2c80955b8abd38a8a21705732e8
amalghamdi <amal.m.alghamdi@gmail.com>
2021-12-09 10:11:23 +0100

    add comments, add multiple forms, and use of mapped geometry

--- cuqi/fenics/testproblem.py
+++ cuqi/fenics/testproblem.py
@@ -64,53 +64,60 @@ class FEniCSDiffusion1D(BayesianProblem):
 
     """
     
-    def __init__(self, dim = 100, L = 1, exactSolution = None, SNR = 100, observation_operator = None):
+    def __init__(self, dim = 100, endpoint = 1, exactSolution = None, SNR = 100, observation_operator = None, form = 'default_form', map = None):
 
+        # Create FEniCSPDE        
         def u_boundary(x, on_boundary):
             return on_boundary
+        if form == 'default_form':
+            def form(m,u,p):
+                return m*ufl.inner(ufl.grad(u), ufl.grad(p))*ufl.dx 
+        elif form == 'exp_form':
+            def form(m,u,p):
+                return ufl.exp(m)*ufl.inner(ufl.grad(u), ufl.grad(p))*ufl.dx 
         
-        def form(m,u,p):
-            return ufl.exp(m)*ufl.inner(ufl.grad(u), ufl.grad(p))*ufl.dx 
-        
-        #%% Create PDE model
-        mesh = dl.IntervalMesh(dim, 0,L)
+        mesh = dl.IntervalMesh(dim, 0,endpoint)
+
         solution_function_space = dl.FunctionSpace(mesh, 'Lagrange', 1)
         parameter_function_space = dl.FunctionSpace(mesh, 'Lagrange', 1)
+
         dirichlet_bc_expression = dl.Expression("x[0]", degree=1)
         dirichlet_bc = dl.DirichletBC(solution_function_space, dirichlet_bc_expression, u_boundary)
-        PDE = cuqi.fenics.pde.SteadyStateLinearFEniCSPDE( form, mesh, solution_function_space, parameter_function_space, dirichlet_bc,observation_operator=observation_operator)
 
-        domain_geometry = cuqi.fenics.geometry.FenicsContinuous(parameter_function_space)
-        range_geometry = cuqi.fenics.geometry.FenicsContinuous(solution_function_space)
+        PDE = cuqi.fenics.pde.SteadyStateLinearFEniCSPDE( form, mesh, solution_function_space, parameter_function_space, dirichlet_bc,observation_operator=observation_operator)
         
-        # Prepare model
+        # Create PDE model
+        domain_geometry = cuqi.fenics.geometry.FEniCSContinuous(parameter_function_space)
+        if map is not None:
+            domain_geometry =  cuqi.fenics.geometry.FEniCSMappedGeometry(geometry=domain_geometry, map = map)
+
+        range_geometry = cuqi.fenics.geometry.FEniCSContinuous(solution_function_space)
+        
         model = cuqi.model.PDEModel(PDE,range_geometry,domain_geometry)
 
+        # Create prior
         pr_mean = np.zeros(domain_geometry.dim)
         prior = cuqi.distribution.GMRF(pr_mean,25,domain_geometry.dim,1,'zero') 
+        
         # Set up exact solution
         if exactSolution is None:
-
             exactSolution = prior.sample()
-            #np.sin(np.linspace(0,L,parameter_function_space.dim()))+ 2 
 
         # Generate exact data
-        b_exact = model.forward(exactSolution,is_par=False)
+        b_exact = model.forward(domain_geometry.par2fun(exactSolution),is_par=False)
 
         # Add noise to data
         sigma = np.linalg.norm(b_exact)/SNR
         sigma2 = sigma*sigma # variance of the observation Gaussian noise
         data = b_exact + np.random.normal( 0, sigma, b_exact.shape )
-
+        
+        # Create likelihood
         likelihood = cuqi.distribution.GaussianCov(model, sigma2*np.eye(range_geometry.dim))
 
-        
-        #cuqi.distribution.GaussianCov(np.zeros(domain_geometry.dim), 1)
-
-        # Initialize Deconvolution as BayesianProblem problem
+        # Initialize FEniCSDiffusion1D as BayesianProblem problem
         super().__init__(likelihood,prior,data)
 
-        # Store exact values
+        # Store exact values and information
         self.exactSolution = exactSolution
         self.exactData = b_exact
         self.infoString = f"Noise type: Additive i.i.d. noise with mean zero and signal to noise ratio: {SNR}"
