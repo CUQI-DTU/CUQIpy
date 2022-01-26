1b2505329d62c4541eba7c1a5c43955e7ea85079
nabr <nabr@dtu.dk>
2022-01-26 10:08:47 +0100

    Add arviz to requirements + basic test

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -27,3 +27,18 @@ def test_samples_plot(geom,is_par,plot_par):
     if dim > 2:
         s.plot([0,2],is_par=is_par,plot_par=plot_par)
         s.plot_chain([0,2])
+
+def test_samples_plot_autocorrelation():
+    # Make basic distribution and sample
+    dist = cuqi.distribution.DistributionGallery("CalSom91")
+    sampler = cuqi.sampler.MetropolisHastings(dist)
+    samples = sampler.sample_adapt(10000)
+
+    # Switch to discrete geometry (easiest for "variable" names)
+    samples.geometry = cuqi.geometry.Discrete(["alpha","beta"])
+
+    # Plot with defaults
+    samples.plot_autocorrelation()
+
+    # Plot for single parameter
+    samples.plot_autocorrelation([0])
