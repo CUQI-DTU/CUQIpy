1f7831e25b1293b4ce1449bab129188591c9cbf6
amalghamdi <amal.m.alghamdi@gmail.com>
2022-01-31 09:44:28 +0100

    add solver signature test

--- tests/test_pde.py
+++ tests/test_pde.py
@@ -51,6 +51,51 @@ def test_solver_choice():
 
     assert(np.allclose(observed_sol_1,observed_sol_2) and np.allclose(observed_sol_1,observed_sol_3))
 
+@pytest.mark.parametrize("solve_kwargs, expected_returned",
+                         [
+                          ({"num_returns":2, "dim":50}, (2,)),
+                          ({"num_returns":3}, (2,1)) 
+                         ])
+def test_solver_signature(solve_kwargs, expected_returned):
+    # Poisson equation
+    dim = 50 #Number of nodes
+    L = 10 # Length of the domain
+    dx = L/(dim-1) # grid spacing 
+    grid_sol = np.linspace(dx, L, dim-1, endpoint=False)
+    grid_obs = grid_sol
+    source = lambda a, x0: a*np.exp( -50 * ( (grid_sol - x0)/L)**2 ) #source term
+    kappa = np.ones(dim)
+    kappa[np.where(np.arange(dim)>dim/2)] = 2 #kappa is the diffusivity 
+
+
+    # Solver1 (default)
+    FOFD_operator = cuqi.operator.FirstOrderFiniteDifference(dim-1,bc_type='zero',dx=dx).get_matrix().todense()
+    diff_operator = FOFD_operator.T @np.diag(kappa) @ FOFD_operator
+    poisson_form = lambda x: (diff_operator, source(x[0],x[1]))
+    CUQI_pde = cuqi.pde.SteadyStateLinearPDE(poisson_form, grid_sol=grid_sol, grid_obs=grid_obs)
+    x_exact = np.array([10,3]) # [10,3] are the source term parameters [a, x0]
+    CUQI_pde.assemble(x_exact)
+    sol, info = CUQI_pde.solve()
+    assert isinstance(sol, np.ndarray)
+
+    def mySolve(*args, num_returns=1, dim=50):
+        val1 = scipy.linalg.solve(*args)
+        val2 = 2
+        val3 = 1
+        if num_returns == 1:
+            return val1
+        elif num_returns == 2:
+            return val1, val2
+        elif num_returns == 3:
+            return val1, val2, val3
+
+    CUQI_pde = cuqi.pde.SteadyStateLinearPDE(poisson_form, grid_sol=grid_sol, grid_obs=grid_obs, linalg_solve=mySolve, linalg_solve_kwargs=solve_kwargs)
+    x_exact = np.array([10,3]) # [10,3] are the source term parameters [a, x0]
+    CUQI_pde.assemble(x_exact)
+    sol, returned = CUQI_pde.solve()
+
+    assert np.all(expected_returned == returned)
+
 
 
 
