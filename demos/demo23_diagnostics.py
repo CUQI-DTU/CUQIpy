38202a1de635c2b9d6cf997b2b856f836be1e51e
nabr <nabr@dtu.dk>
2022-02-01 09:44:34 +0100

    Update demo with LMRF distribution

--- demos/demo23_diagnostics.py
+++ demos/demo23_diagnostics.py
@@ -2,7 +2,7 @@
 import sys
 sys.path.append("..")
 import cuqi
-# %%
+# %% 2-dimensional distribution with circular shape
 dist = cuqi.distribution.DistributionGallery("CalSom91")
 sampler = cuqi.sampler.MetropolisHastings(dist)
 samples = sampler.sample_adapt(50000)
@@ -13,24 +13,25 @@ samples.geometry = cuqi.geometry.Discrete(["alpha","beta"])
 ax_t = samples.plot_trace()
 # %% autocorrelation
 ax_ac = samples.plot_autocorrelation()
-# %% plot marginals
-ax_p = samples.plot_pair()
+# %% plot pair-wise scatter plot
+ax_p = samples.plot_pair(marginals=True)
 # %%
 samples_bt = samples.burnthin(20000,20)
 samples_bt.plot_autocorrelation()
 # %%
 samples_bt.plot_trace()
 # %% plot marginals
-samples_bt.plot_pair()
-# %% Try all plotting tools with 4 variables
+samples_bt.plot_pair(marginals=True)
+# %% Try the plotting tools with 4 variable distribution
 import numpy as np
-dist4 = cuqi.distribution.Gaussian(np.array([1,2,3,4]),1)
+#dist4 = cuqi.distribution.Gaussian(np.array([1,2,3,4]),1)
+dist4 = cuqi.distribution.LMRF(np.array([1,2,3,4]), 1, 4, 1, 'zero')
 sampler4 = cuqi.sampler.MetropolisHastings(dist4)
-samples4 = sampler4.sample_adapt(20000)
+samples4 = sampler4.sample_adapt(50000)
 samples4.geometry = cuqi.geometry.Discrete(["a","b","c","d"])
 # %%
 samples4.plot_trace()
-samples4_bt = samples4.burnthin(10000)
+samples4_bt = samples4.burnthin(20000,5)
 # %%
 samples4_bt.plot_trace()
 # %%
@@ -44,4 +45,4 @@ samples4_bt.plot_pair(kind='hexbin', textsize=25)
 # %%
 ax_4 = samples4_bt.plot_autocorrelation()
 # %%
-ax_22 = samples4_bt.plot_autocorrelation(label_axis=False, grid=(2,2))
+ax_22 = samples4_bt.plot_autocorrelation(grid=(2,2))
