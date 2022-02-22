7b8a3406ef2f6f69507c1e11beb8050e0f502114
amalghamdi <amal.m.alghamdi@gmail.com>
2022-02-22 10:18:11 +0100

    add demo for ULA

---
+++ demos/demo27_ULA.py
@@ -0,0 +1,43 @@
+# %% Initialize and import CUQI
+import sys
+sys.path.append("..") 
+import numpy as np
+import cuqi
+import matplotlib.pyplot as plt
+
+# %% Create CUQI test problem
+test = cuqi.testproblem.Deblur()
+n = test.model.domain_dim
+h = test.meshsize
+
+# Extract data
+data = test.data
+
+# Extract Likelihood
+likelihood  = test.likelihood
+
+# Define Prior
+loc = np.zeros(n)
+delta = 1
+scale = delta*h
+prior = cuqi.distribution.Cauchy_diff(loc, scale, 'neumann')
+
+# %% Create the posterior and the sampler
+posterior = cuqi.distribution.Posterior(likelihood,prior,data)
+MCMC = cuqi.sampler.ULA(posterior)
+
+# %% Sample
+samples  = MCMC.sample(1000,500)
+
+# %% Compute mean
+x_mean = np.mean(samples.samples,axis=1)
+
+# %% Compare mean and exact solution by plotting
+plt.plot(x_mean)
+plt.plot(test.exactSolution)
+
+# %% Plot confidence interval
+samples.plot_ci(95,exact=test.exactSolution)
+
+# %% Plot autocorrelation
+samples.plot_autocorrelation(max_lag = 300)
