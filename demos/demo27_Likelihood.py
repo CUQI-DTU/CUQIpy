d2997e993b3907a639e77a521add935406ef8975
nabr <nabr@dtu.dk>
2022-02-25 12:27:32 +0100

    Add comments from talk about likelihood

--- demos/demo27_Likelihood.py
+++ demos/demo27_Likelihood.py
@@ -12,6 +12,8 @@ data = TP.data
 n = TP.model.domain_dim
 
 # %%
+model = TP.model #Model data etc.
+data_dist = cuqi.distribution.GaussianCov(model, lambda sigma: sigma, conds_dists=[s]) # this is also the likelihood dist.
 likelihood = cuqi.likelihood.Likelihood(data_dist, data)
 likelihood
 
@@ -30,12 +32,23 @@ likelihoodU.gradient(x=gt);
 # %% Five line example (No change yet)
 model = TP.model #Model data etc.
 prior = cuqi.distribution.Gaussian(mean=np.zeros(n), std=0.2)
-data_dist = cuqi.distribution.Gaussian(model, 0.05) # this is also the likelihood dist.
+data_dist = cuqi.distribution.Gaussian(model, 0.05)
 IP = cuqi.problem.BayesianProblem(data_dist, prior, data)
 IP.UQ(exact=TP.exactSolution)
 
+# %% Five line example (Make to_likelihood method)
+model = TP.model #Model data etc.
+prior = cuqi.distribution.Gaussian(mean=np.zeros(n), std=0.2)
+#likelihood = cuqi.distribution.Gaussian(model, 0.05).to_likelihood(data) # Avoid naming the conditional distribution :D
+IP = cuqi.problem.BayesianProblem(likelihood, prior)
+IP.UQ(exact=TP.exactSolution)
+
 # %% Posterior (remains with distribution..)
-posterior = cuqi.distribution.Posterior(data_dist, prior, data)
+
+# posterior = likelihood x prior / evidence
+
+#posterior = cuqi.distribution.Posterior(data_dist, prior, data) # !!Kill your darlings!!
+#posterior = cuqi.distribution.Posterior(likelihood, prior)
 
 # %% Minimize.. #ToDo. Fix distribution allowing args
 #x0 = np.zeros(n)
