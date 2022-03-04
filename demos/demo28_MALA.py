5ef422bb543721bfcca74827e737d9b41bfcbf67
amalghamdi <amal.m.alghamdi@gmail.com>
2022-03-04 12:00:20 +0100

    add demo MALA sampler

---
+++ demos/demo28_MALA.py
@@ -0,0 +1,40 @@
+# %% Initialize and import CUQI
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+sys.path.append("..")
+import cuqi
+
+# %% Create CUQI test problem
+test = cuqi.testproblem.Deblur()
+n = test.model.domain_dim
+
+# Extract data
+data = test.data
+
+# Extract Likelihood
+likelihood = test.likelihood
+
+# Define Prior
+loc = np.zeros(n)
+prior = cuqi.distribution.Cauchy_diff(loc, .2, 'neumann')
+
+# %% Create the posterior and the sampler
+posterior = cuqi.distribution.Posterior(likelihood, prior, data)
+MCMC = cuqi.sampler.MALA(posterior, scale=.007, x0=np.ones(n))
+
+# %% Sample
+samples = MCMC.sample(3000, 500)
+
+# %% Compute mean
+x_mean = np.mean(samples.samples, axis=1)
+
+# %% Compare mean and exact solution by plotting
+plt.plot(x_mean)
+plt.plot(test.exactSolution)
+
+# %% Plot confidence interval
+samples.plot_ci(95, exact=test.exactSolution)
+
+# %% Plot autocorrelation
+samples.plot_autocorrelation(max_lag=3000)
