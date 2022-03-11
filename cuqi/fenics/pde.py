e4bb8163853651886e65b69d89f52c817f714bce
amaal <amaal@dtu.edu>
2022-03-11 13:00:45 +0000

    quick fix (hack) for returned solver info

--- cuqi/fenics/pde.py
+++ cuqi/fenics/pde.py
@@ -53,7 +53,7 @@ class SteadyStateLinearFEniCSPDE(FEniCSPDE):
     def solve(self):
         PDE_solution_fun = dl.Function(self.solution_function_space)
         dl.solve(self.diff_op ==  self.rhs, PDE_solution_fun, self.dirichlet_bc)
-        return PDE_solution_fun 
+        return PDE_solution_fun, None 
 
     def observe(self,PDE_solution_fun):
         if self.observation_operator == None: 
