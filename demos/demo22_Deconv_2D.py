450215005ce9d712edb4f691c37a8e6e26ba22f3
nabr <nabr@dtu.dk>
2022-03-17 01:25:49 +0100

    Update demo with defaults

--- demos/demo22_Deconv_2D.py
+++ demos/demo22_Deconv_2D.py
@@ -1,19 +1,13 @@
 #%%
 import sys
-
 sys.path.append("..")
 import numpy as np
 import matplotlib.pyplot as plt
 import cuqi
 
-#import skimage
-#from skimage import color
-#phantom = color.rgb2gray(skimage.data.astronaut())
-phantom = "satellite"
-
-#%% %Cuqi deblur test problem
-model, data, probInfo = cuqi.testproblem.Deconv_2D.get_components(dim=128, phantom=phantom)
-likelihood = cuqi.distribution.GaussianCov(model, 0.05**2).to_likelihood(data)
+#%% %Cuqi Deconv 2D test problem
+model, data, probInfo = cuqi.testproblem.Deconv_2D.get_components()
+likelihood = cuqi.distribution.GaussianCov(model, 0.0036**2).to_likelihood(data)
 prior = cuqi.distribution.GaussianCov(np.zeros(model.domain_dim), 1, geometry=model.domain_geometry)
 BP = cuqi.problem.BayesianProblem(likelihood, prior)
 
