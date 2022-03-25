2662caa5a8cef03b3a55c590bbf1658a82f20531
nabr <nabr@dtu.dk>
2022-03-25 18:02:53 +0100

    Update try script with new functionality

--- demos/try12_Deconvolution_BayesianProblem.py
+++ demos/try12_Deconvolution_BayesianProblem.py
@@ -1,11 +1,7 @@
 # %% Here we test the automatic sampler selection for Deconvolution (1D and 2D).
 # TODO (Questions):
-# 1) In Laplace_diff we use CWMH. Here the scale is set to some specific value. Can we make a better choice?
-# 2) In Cauchy_diff+Laplace_Diff we do not support 2D yet.
-# 3) In GMRF, LMRF 2D is somewhat supported, but currently breaks in the code.
-# 4) Is Laplace_diff and LMRF the same thing?
-# 5) Add doc to Cauchy_diff and Laplace_diff.
-# 6) Add doc to GMRF, LMRF.
+# 1) Add doc to Cauchy_diff and Laplace_diff.
+# 2) Add doc to GMRF, LMRF.
 
 import sys
 sys.path.append("..")
@@ -20,8 +16,9 @@ from cuqi.sampler import NUTS, CWMH
 %load_ext autoreload
 %autoreload 2
 # %% Deconvolution 1D problem
-TP = cuqi.testproblem.Deconvolution(phantom="square")
-#TP = cuqi.testproblem.Deconvolution2D()
+dim = 128
+#TP = cuqi.testproblem.Deconvolution(dim=dim, phantom="square")
+TP = cuqi.testproblem.Deconvolution2D(dim=dim, phantom="cat")
 n = TP.model.domain_dim
 N = TP.model.domain_geometry.shape[0]
 ndim = len(TP.model.domain_geometry.shape)
@@ -30,20 +27,20 @@ ndim = len(TP.model.domain_geometry.shape)
 if ndim == 1: par = 0.02 #1d
 if ndim == 2: par = 1 #2d
 if ndim == 1: Ns = 1000
-if ndim == 2: Ns = 100
+if ndim == 2: Ns = 500
 
 # %% Prior choices (Main ones of interest: Gaussian, GMR, Cauchy_diff, Laplace_diff:
 # Working choices
 #TP.prior = Gaussian(mean=np.zeros(n), std=par, geometry=TP.model.domain_geometry) #Not for 2D.
 #TP.prior = GaussianCov(mean=np.zeros(n), cov=par**2, geometry=TP.model.domain_geometry)
+TP.prior = GMRF(np.zeros(n), 1/par**2, N, ndim, "zero", geometry=TP.model.domain_geometry) # Odd behavior (swingy?)
 
-# Seems ok? - (Needs fix in 2D)
-#TP.prior = Cauchy_diff(location=np.zeros(n), scale=par, bc_type="zero", geometry=TP.model.domain_geometry) #NUTS is not adapting parameters fully. (Not using sample(n,nb) atm.)
-TP.prior = Laplace_diff(location=np.zeros(n), scale=par, bc_type="zero", geometry=TP.model.domain_geometry) #Does not veer away from initial guess very much in prior samples
-#TP.prior = GMRF(np.zeros(n), 1/par**2, N, ndim, "zero", geometry=TP.model.domain_geometry) # Odd behavior (swingy?)
+# Seems ok in 1D - (Not really seem feasible in 2D it seems) - Cauchy might work.
+#TP.prior = Cauchy_diff(location=np.zeros(n), scale=0.1, bc_type="zero", physical_dim=ndim, geometry=TP.model.domain_geometry) #NUTS is not adapting parameters fully. (Not using sample(n,nb) atm.)
+#TP.prior = Laplace_diff(location=np.zeros(n), scale=0.01, bc_type="zero", physical_dim=ndim, geometry=TP.model.domain_geometry) #Does not veer away from initial guess very much in prior samples
 #TP.prior = LMRF(np.zeros(n), 1/par**2, N, ndim, "zero", geometry=TP.model.domain_geometry) # Seems OK. Same as Laplace diff actually?
 
-# Bad choices (ignore)
+# Bad choices (ignore) both 1D and 2D
 #TP.prior = Beta(2*np.ones(n), 5*np.ones(n)) #Might need tuning
 #TP.prior = Laplace(np.zeros(n), 10) #Might need tuning
 #TP.prior = InverseGamma(3*np.ones(n), np.zeros(n), 1*np.ones(n)) #Bad choice in general.. #Might need tuning
@@ -66,14 +63,15 @@ samples.plot_autocorrelation()
 
 # %% Prior samples
 try:
+    print("Trying direct..")
     samples_prior = TP.prior.sample(3)
-    print("Direct")
+    
 except:
     try:
-        samples_prior = NUTS(TP.prior).sample(1000)
-        print("NUTS")
+        print("Trying NUTS..")
+        samples_prior = NUTS(TP.prior).sample(Ns)
     except:
-        samples_prior = CWMH(TP.prior).sample_adapt(5000)
-        print("CWMH")
+        print("Trying CWMH..")
+        samples_prior = CWMH(TP.prior).sample_adapt(Ns)
 samples_prior.plot()
 # %%
