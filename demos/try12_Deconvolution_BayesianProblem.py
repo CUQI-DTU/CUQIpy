050dc4c3d00cb7d64f03c45ab859fd86c0af64db
nabr <nabr@dtu.dk>
2022-03-28 23:48:38 +0200

    Update demo script

--- demos/try12_Deconvolution_BayesianProblem.py
+++ demos/try12_Deconvolution_BayesianProblem.py
@@ -18,8 +18,8 @@ from cuqi.sampler import NUTS, CWMH
 %autoreload 2
 # %% Deconvolution 1D problem
 dim = 128
-#TP = cuqi.testproblem.Deconvolution(dim=dim, phantom="square")
-TP = cuqi.testproblem.Deconvolution2D(dim=dim, phantom="cat")
+TP = cuqi.testproblem.Deconvolution(dim=dim, phantom="square")
+#TP = cuqi.testproblem.Deconvolution2D(dim=dim, phantom="satellite")
 n = TP.model.domain_dim
 N = TP.model.domain_geometry.shape[0]
 ndim = len(TP.model.domain_geometry.shape)
@@ -34,10 +34,10 @@ if ndim == 2: Ns = 500
 # Working choices
 #TP.prior = Gaussian(mean=np.zeros(n), std=par, geometry=TP.model.domain_geometry) #Not for 2D.
 #TP.prior = GaussianCov(mean=np.zeros(n), cov=par**2, geometry=TP.model.domain_geometry)
-TP.prior = GMRF(np.zeros(n), 1/par**2, N, ndim, "zero", geometry=TP.model.domain_geometry) # Odd behavior (swingy?)
+#TP.prior = GMRF(np.zeros(n), 1/par**2, N, ndim, "zero", geometry=TP.model.domain_geometry) # Odd behavior (swingy?)
 
 # Seems ok in 1D - (Not really seem feasible in 2D it seems) - Cauchy might work.
-#TP.prior = Cauchy_diff(location=np.zeros(n), scale=0.1, bc_type="zero", physical_dim=ndim, geometry=TP.model.domain_geometry) #NUTS is not adapting parameters fully. (Not using sample(n,nb) atm.)
+TP.prior = Cauchy_diff(location=np.zeros(n), scale=0.01, bc_type="zero", physical_dim=ndim, geometry=TP.model.domain_geometry) #NUTS is not adapting parameters fully. (Not using sample(n,nb) atm.)
 #TP.prior = Laplace_diff(location=np.zeros(n), scale=0.01, bc_type="zero", physical_dim=ndim, geometry=TP.model.domain_geometry) #Does not veer away from initial guess very much in prior samples
 #TP.prior = LMRF(np.zeros(n), 1/par**2, N, ndim, "zero", geometry=TP.model.domain_geometry) # Seems OK. Same as Laplace diff actually?
 
@@ -64,6 +64,19 @@ samples.plot_autocorrelation()
 
 # %% scatter
 samples.plot_pair()
+
+# %% MAP ESTIMATE
+x_map, info = TP.MAP()
+
+if ndim == 1:
+    plt.plot(x_map, label="MAP")
+    plt.plot(TP.exactSolution, label="exact")
+    plt.legend()
+elif ndim == 2:
+    plt.figure()
+    cuqi.samples.CUQIarray(x_map, geometry=TP.model.domain_geometry).plot()
+    plt.figure()
+    plt.subplot(122); TP.exactSolution.plot()
 
 # %% Prior samples
 try:
