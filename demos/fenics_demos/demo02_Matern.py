eb33e83ada15f4519c860f0ef45c9fa654dcacef
amaal <amaal@dtu.edu>
2022-03-28 09:27:47 +0000

    remove commented out code and fix a bug

--- demos/fenics_demos/demo02_Matern.py
+++ demos/fenics_demos/demo02_Matern.py
@@ -10,16 +10,6 @@ import dolfin as dl
 mesh = dl.UnitSquareMesh(20,20)
 V = dl.FunctionSpace(mesh, 'CG', 1)
 geometry = cuqi.fenics.geometry.FEniCSContinuous(V)
-matern = cuqi.fenics.field.Matern(geometry, l = .2, nu = 2, num_terms=128)
+matern = cuqi.fenics.geometry.Matern(geometry, l = .2, nu = 2, num_terms=128)
 
 dl.plot(matern.par2fun(np.random.rand(128)))
-
-
-# Matern 2
-
-#geometry = cuqi.geometry.Continuous2D(grid=(10,10))
-#matern2 = cuqi.fenics.field.Matern(geometry, l = .2, nu = 2, num_terms=128)
-
-#dl.plot(matern2.par2fun(np.random.rand(128)))
-
-# %%
