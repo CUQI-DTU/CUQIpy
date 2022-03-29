1ac1ea3ae11d2c08569a88695995f21a52bd9418
amaal <amaal@dtu.edu>
2022-03-29 12:55:20 +0000

    update matern initializer

--- demos/fenics_demos/demo02_Matern.py
+++ demos/fenics_demos/demo02_Matern.py
@@ -10,6 +10,6 @@ import dolfin as dl
 mesh = dl.UnitSquareMesh(20,20)
 V = dl.FunctionSpace(mesh, 'CG', 1)
 geometry = cuqi.fenics.geometry.FEniCSContinuous(V)
-matern = cuqi.fenics.geometry.Matern(geometry, l = .2, nu = 2, num_terms=128)
+matern = cuqi.fenics.geometry.Matern(geometry, l = .2, num_terms=128)
 
 dl.plot(matern.par2fun(np.random.rand(128)))
