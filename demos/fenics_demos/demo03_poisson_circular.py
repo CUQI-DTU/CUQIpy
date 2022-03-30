5a4a8d71f17d30e88372c0a37923754c5c206e9d
amaal <amaal@dtu.edu>
2022-03-30 09:30:15 +0000

    update demo

--- demos/fenics_demos/demo03_poisson_circular.py
+++ demos/fenics_demos/demo03_poisson_circular.py
@@ -1,35 +1,36 @@
 #%% Imports
 from turtle import pos, update
-from dolfin import * 
+import dolfin as dl
 import sys
 import numpy as np
 sys.path.append("../../")
 import cuqi
-from mshr import *
+import mshr
 import matplotlib.pyplot as plt
 
-#%% Define observation map 
-obs_func = None
+#%% 1. First, we create a poisson PDE using FEniCS. We specify the domain,
+# the PDE form, the boundary conditions and the source term (the following
+# 6 steps).
 
-#%% Define domain and mesh
-domain = Circle(Point(0,0),1)
-mesh = generate_mesh(domain, 20)
+#%% 1.1 Define domain and mesh
+domain = mshr.Circle(dl.Point(0,0),1)
+mesh = mshr.generate_mesh(domain, 20)
 
-# Define function spaces 
-V = FiniteElement("CG", mesh.ufl_cell(), 1)
-R = FiniteElement("R", mesh.ufl_cell(), 0)
-parameter_space = FunctionSpace(mesh, "CG", 1)
-solution_space = FunctionSpace(mesh, V*R)
-V_space = FunctionSpace(mesh, V)
+#%% 1.2 Define function spaces 
+V = dl.FiniteElement("CG", mesh.ufl_cell(), 1)
+R = dl.FiniteElement("R", mesh.ufl_cell(), 0)
+parameter_space = dl.FunctionSpace(mesh, "CG", 1)
+solution_space = dl.FunctionSpace(mesh, V*R)
+V_space = dl.FunctionSpace(mesh, V)
 
-#%% Define sourceterm
-class source(UserExpression):
+#%% 1.3 Define sourceterm
+class source(dl.UserExpression):
     def eval(self,values,x):
         values[0] = 10*np.exp(-(np.power(x[0]-0.5, 2) + np.power(x[1], 2)))
 FEM_el = parameter_space.ufl_element()
 source_term = source(element=FEM_el)
 
-#%% Define Poisson problem
+#%% 1.4 Define Poisson problem form
 def form(m,u,p):
     u_0 = u[0]
     c_0 = u[1]
@@ -37,113 +38,114 @@ def form(m,u,p):
     v_0 = p[0]
     d_0 = p[1]
 
-    return m*inner( grad(u_0), grad(v_0) )*dx + c_0*v_0*ds + u_0*d_0*ds - source_term*v_0*dx
+    return m*dl.inner( dl.grad(u_0), dl.grad(v_0) )*dl.dx + c_0*v_0*dl.ds + u_0*d_0*dl.ds - source_term*v_0*dl.dx
 
-#%% Define (dummy) Dirichlet BCs
+#%% 1.5 Define (dummy) Dirichlet BCs
 def u_boundary(x, on_boundary):
     return False
-bc_func = Expression("1", degree=1)
-dirichlet_bc = DirichletBC(solution_space.sub(0), bc_func, u_boundary)
+bc_func = dl.Expression("1", degree=1)
+dirichlet_bc = dl.DirichletBC(solution_space.sub(0), bc_func, u_boundary)
 
-#%% Create CUQI PDE
-PDE = cuqi.fenics.pde.SteadyStateLinearFEniCSPDE( form, mesh, solution_space, parameter_space,dirichlet_bc, observation_operator=obs_func)
+#%% 1.6 Define observation map (applied to the solution to generate the 
+# observables)
+obs_func = None
 
-#%% Create the domain geometry 
+#%% 2.Second, we define the Bayesian Inverse probelem using CUQI. We 
+# create the geometries for the model domain and range, the forward
+# model, the prior, the likelihood and the posterior (the following
+# 10 steps).
+
+
+#%% 2.1 Create the domain geometry 
+
+# The space on which the Bayesian parameters are defined
 fenics_continuous_geo = cuqi.fenics.geometry.FEniCSContinuous(parameter_space)
-matern_geo = cuqi.fenics.geometry.Matern(fenics_continuous_geo, l = .2, num_terms=128)
 
+# The matern filed (maps i.i.d normal random vector of dimention `num_terms`
+# to maren field realization on `fenics_continuous_geo`)
+matern_geo = cuqi.fenics.geometry.Matern(fenics_continuous_geo, length_scale = .2, num_terms=128)
+
+# We create a map `heavy_map` to map the matern field realization to two levels
+# c_minus and c_plus 
 c_minus = 1
 c_plus = 10
-
 def heavy_map(func):
     dofs = func.vector().get_local()
     updated_dofs = c_minus*0.5*(1 + np.sign(dofs)) + c_plus*0.5*(1 - np.sign(dofs))
     func.vector().set_local(updated_dofs)
     return func
 
+# finally, we create the domain geometry which applies the
+# map `heavy_map` on matern realizations.
 domain_geometry = cuqi.fenics.geometry.FEniCSMappedGeometry(matern_geo, map = heavy_map)
 
-#%% Create the range geomtry 
+#%% 2.2 Create the range geomtry 
 range_geometry = cuqi.fenics.geometry.FEniCSContinuous(solution_space) 
 
-#%% Create CUQI model
+#%% 2.3 Create CUQI PDE (which encapsulates the FEniCS formulation
+# of the PDE)
+PDE = cuqi.fenics.pde.SteadyStateLinearFEniCSPDE( form, mesh, solution_space, parameter_space,dirichlet_bc, observation_operator=obs_func)
+
+#%% 2.4 Create CUQI model
 model = cuqi.model.PDEModel(PDE,range_geometry,domain_geometry)
 
-#%% Create prior
+#%% 2.5 Create a prior
 pr_mean = np.zeros(domain_geometry.dim)
 prior = cuqi.distribution.GaussianCov(pr_mean, cov=np.eye(domain_geometry.dim), geometry= domain_geometry)
 
-#%% Define the exact solution
+#%% 2.6 Define the exact solution
 exactSolution = prior.sample()
 
-#%% Generate exact data
+#%% 2.7 Generate exact data
 b_exact = model.forward(domain_geometry.par2fun(exactSolution),is_par=False)
 
-#%% Add noise to data
+#%% 2.8 Add noise to data
 SNR = 100
 sigma = np.linalg.norm(b_exact)/SNR
 sigma2 = sigma*sigma # variance of the observation Gaussian noise
 data = b_exact + np.random.normal( 0, sigma, b_exact.shape )
 
-#%% Create likelihood
+#%% 2.9 Create the likelihood
 likelihood = cuqi.distribution.GaussianCov(model, sigma2*np.ones(range_geometry.dim)).to_likelihood(data)
 
-#%% Create posterior
+#%% 2.10 Create posterior
 posterior = cuqi.distribution.Posterior(likelihood, prior)
 
-#%% Create MH Sampler
-MHSampler = cuqi.sampler.MetropolisHastings(
-    posterior,
-    proposal=None,
-    scale=None,
-    x0=None,
-    dim=None,
-)
 
-#%% Sample using the MH Sampler
-samplesMH = MHSampler.sample_adapt(1000)
+#%% 3 Third, we define a pCN sampler, sample, and inspect the results 
 
-
-#%% Plot the exact solution
+#%% 3.1 Plot the exact solution
 plt.figure()
-im = plot(domain_geometry.par2fun(exactSolution), title="exact solution")
+im = dl.plot(domain_geometry.par2fun(exactSolution), title="exact solution")
 plt.colorbar(im)
 
-#%% Plot prior samples
+#%% 3.2 Plot prior samples
 prior_samples = prior.sample(5)
 ims = prior_samples.plot(title="prior")
 plt.colorbar(ims[-1])
 
-#%% Plot posterior MH samples
-ims = samplesMH.plot([0, 100, 300, 600, 800, 900],title="posterior")
-plt.colorbar(ims[-1])
 
-#%% Plot trace and autocorrelation (MH)
-samplesMH.plot_trace()
-samplesMH.plot_autocorrelation(max_lag=300)
-
-
-#%% Create pCN Sampler 
+#%% 3.3 Create pCN Sampler 
 pCNSampler = cuqi.sampler.pCN(
     posterior,
     scale=None,
     x0=None,
 )
 
-#%% Sample using the pCN sampler
+#%% 3.4 Sample using the pCN sampler
 samplespCN = pCNSampler.sample_adapt(1000)
 
-#%% Plot posterior pCN samples 
+#%% 3.5 Plot posterior pCN samples 
 ims = samplespCN.plot([0, 100, 300, 600, 800, 900],title="posterior")
 plt.colorbar(ims[-1])
 
-# %% Plot trace and autocorrelation (pCN)
+# %% 3.6 Plot trace and autocorrelation (pCN)
 samplespCN.plot_trace()
 samplespCN.plot_autocorrelation(max_lag=300)
 
-#%% Plot credible interval (MH)
+#%% 3.7 Plot credible interval (MH)
 plt.figure()
-samplesMH.plot_ci(plot_par = True, exact=exactSolution)
+samplespCN.plot_ci(plot_par = True, exact=exactSolution)
 plt.xticks(range(128)[::20], range(128)[::20])
-plt.title("Credible interval MH")
+plt.title("Credible interval")
 # %%
