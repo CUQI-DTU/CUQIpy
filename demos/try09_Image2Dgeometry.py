c3e086f7f3e4f55dbd51599d3adc176523fe5693
Silja Christensen <swech@dtu.dk>
2022-04-05 15:38:52 +0200

    Use convolution to try image2D geometry.

--- demos/try09_Image2Dgeometry.py
+++ demos/try09_Image2Dgeometry.py
@@ -4,33 +4,41 @@ import sys
 sys.path.append("..") 
 import cuqi
 import numpy as np
+from scipy import ndimage
+import matplotlib.pyplot as plt
 
 #%%
-# Forward matrix, picks out the first 5 rows of a 10x... matrix
-A = np.zeros((5,10))
-for i in range(0,5):
-    A[i,i] = 1
+# Forward problem. Convolution with kernel k
+k = np.array([[1,1,1],[1,1,0],[1,0,0]])
+forward_func = lambda x: ndimage.convolve(x, k, mode='constant', cval=0.0)
+adjoint_func = lambda x: x
 
 # Use Image geometries
-range_geometry = cuqi.geometry.Image2D((5,10), order = "C")
-domain_geometry = cuqi.geometry.Image2D((10,10), order = "C")
+range_geometry = cuqi.geometry.Image2D((4,4), order = "C")
+domain_geometry = cuqi.geometry.Image2D((4,4), order = "C")
 
 # Linear cuqi model
-model = cuqi.model.LinearModel(A,range_geometry=range_geometry,domain_geometry=domain_geometry)
+model = cuqi.model.LinearModel(forward_func,adjoint=adjoint_func,range_geometry=range_geometry,domain_geometry=domain_geometry)
 
 # %% Setup exact parameters x
-x_exact_im = np.ones((10,10))
+x_exact_im = np.array([[1, 2, 0, 0],
+              [5, 3, 0, 4],
+              [0, 0, 0, 7],
+              [9, 3, 0, 0]])
 x_exact = cuqi.samples.CUQIarray(x_exact_im, is_par=False, geometry=domain_geometry) # CUQIarray to represent both vector and image
 x_exact.plot()
+plt.colorbar()
 
 # %% Compute exact data
 b_exact = model.forward(x_exact)
 b_exact.plot()
+plt.colorbar()
 
 # %% Setup data distribution
-data_distb = cuqi.distribution.GaussianCov(mean = model, cov = 0.1, geometry=range_geometry) #Geometry is not automatically inferred yet
+data_distb = cuqi.distribution.GaussianCov(mean = model, cov = 1, geometry=range_geometry) #Geometry is not automatically inferred yet
 
 # %% Sample from data distribution to obtain noisy data
 data = data_distb(x = x_exact).sample()
 data.plot()
+plt.colorbar()
 # %%
