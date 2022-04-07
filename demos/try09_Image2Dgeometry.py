86dbfbb2075b20960d1580803e0b279372d440a5
Silja Christensen <swech@dtu.dk>
2022-04-07 08:22:29 +0200

    Use cat image in try09_Image2Dgeometry

--- demos/try09_Image2Dgeometry.py
+++ demos/try09_Image2Dgeometry.py
@@ -7,26 +7,24 @@ import numpy as np
 from scipy import ndimage
 import matplotlib.pyplot as plt
 
-#%%
+#%% Setup exact image with Image2D geometries
+
+N = 128
+x_exact_im = cuqi.data.cat(N)
+range_geometry = cuqi.geometry.Image2D((N,N), order = "C")
+domain_geometry = cuqi.geometry.Image2D((N,N), order = "C")
+x_exact = cuqi.samples.CUQIarray(x_exact_im, is_par=False, geometry=domain_geometry) # CUQIarray to represent both vector and image
+x_exact.plot()
+plt.colorbar()
+
+#%% Setup forward problem in CUQI model
+
 # Forward problem. Convolution with kernel k
 k = np.array([[1,1,1],[1,1,0],[1,0,0]])
 forward_func = lambda x: ndimage.convolve(x, k, mode='constant', cval=0.0)
 
-# Use Image geometries
-range_geometry = cuqi.geometry.Image2D((4,4), order = "C")
-domain_geometry = cuqi.geometry.Image2D((4,4), order = "C")
-
 # Linear cuqi model
 model = cuqi.model.Model(forward_func,range_geometry=range_geometry,domain_geometry=domain_geometry)
-
-# %% Setup exact parameters x
-x_exact_im = np.array([[1, 2, 0, 0],
-              [5, 3, 0, 4],
-              [0, 0, 0, 7],
-              [9, 3, 0, 0]])
-x_exact = cuqi.samples.CUQIarray(x_exact_im, is_par=False, geometry=domain_geometry) # CUQIarray to represent both vector and image
-x_exact.plot()
-plt.colorbar()
 
 # %% Compute exact data
 b_exact = model.forward(x_exact)
@@ -34,7 +32,7 @@ b_exact.plot()
 plt.colorbar()
 
 # %% Setup data distribution
-data_distb = cuqi.distribution.GaussianCov(mean = model, cov = 1, geometry=range_geometry) #Geometry is not automatically inferred yet
+data_distb = cuqi.distribution.GaussianCov(mean = model, cov = 50, geometry=range_geometry) #Geometry is not automatically inferred yet
 
 # %% Sample from data distribution to obtain noisy data
 data = data_distb(x = x_exact).sample()
