a14f6f32e0433653697868b2ecc691eb073207fa
nabr <nabr@dtu.dk>
2022-04-30 23:40:56 +0200

    Update demo to use astra

--- demos/astra_demos/demo01_CT.py
+++ demos/astra_demos/demo01_CT.py
@@ -7,8 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Specifically load the CT library (not loaded by default in cuqi)
-from cuqi.cil.model import CT2D_parallel, CT2D_fanbeam
-from cuqi.cil.testproblem import ParBeamCT_2D
+from cuqi.astra.model import CT2D_parallel, CT2D_fanbeam
+from cuqi.astra.testproblem import ParBeamCT_2D
 
 %load_ext autoreload
 %autoreload 2
