Fix gradient of Gaussian likelihood

cuqi/distribution.py
@@ -616,7 +616,7 @@ class GaussianCov(Distribution): # TODO: super general with precisions
             dev = val - model.forward(*args, **kwargs)
             if isinstance(dev, numbers.Number):
                 dev = np.array([dev])
-            return (self.prec @ dev) @ model.gradient(*args, **kwargs).reshape((1, model.domain_dim))
+            return model.gradient(self.prec @ dev, *args, **kwargs)
         else:
             warnings.warn('Gradient not implemented for {}'.format(type(self.mean)))
 

cuqi/model.py
@@ -21,6 +21,9 @@ class Model(object):
     domain_geometry : integer or cuqi.geometry.Geometry
         If integer is given a _DefaultGeometry is created with dimension of the integer.
 
+    gradient : callable function
+        Jacobian of the forward operator at a given point and direction.
+
     Attributes
     -----------
     range_geometry : cuqi.geometry.Geometry
@@ -123,10 +126,24 @@ class Model(object):
     def __call__(self,x):
         return self.forward(x)
 
-    def gradient(self, x):
+    def gradient(self, direction, wrt):
+        """ Gradient of the forward operator.
+
+        For non-linear models the gradient is computed using the
+        forward operator and the Jacobian of the forward operator.
+
+        Parameters
+        ----------
+        direction : ndarray
+            The direction to compute the gradient. The Jacobian is applied to this direction.
+
+        wrt : ndarray
+            The point to compute the Jacobian at. This is only used for non-linear models.
+        
+        """
         if self._gradient_func is None:
             raise NotImplementedError("Gradient is not implemented for this model.")
-        return self._gradient_func(x)
+        return self._gradient_func(direction, wrt)
 
     # approximate the Jacobian matrix of callable function func
     def approx_jacobian(self, x, epsilon=np.sqrt(np.finfo(np.float).eps)):
@@ -271,13 +288,25 @@ class LinearModel(Model):
 
             return self._matrix
 
-    def gradient(self,x):
-        """Evaluate the gradient of the forward map with respect to the model input."""
+    def gradient(self, direction, wrt=None):
+        """ Gradient of the model.
+
+        In the case of a linear model, the gradient is computed using the adjoint.
+
+        Parameters
+        ----------
+        direction : ndarray
+            The direction to compute the gradient. The Jacobian is applied to this direction.
+
+        wrt : ndarray
+            The point to compute Jacobian at. This is only used for non-linear models.
+        
+        """
         #Avoid complicated geometries that change the gradient.
         if not type(self.domain_geometry) in [_DefaultGeometry, Continuous1D, Continuous2D, Discrete, Image2D]:
             raise NotImplementedError("Gradient not implemented for model {} with domain geometry {}".format(self,self.domain_geometry))
 
-        return self.adjoint(x)
+        return self.adjoint(direction)
     
     def __matmul__(self, x):
         return self.forward(x)

cuqi/testproblem.py
@@ -1214,8 +1214,9 @@ class WangCubic(BayesianProblem):
         # forward model and gradient
         def forward(x):
             return 10*x[1] - 10*x[0]**3 + 5*x[0]**2 + 6*x[0]
-        def gradient(x):
-            return np.hstack([-30*x[0]**2 + 10*x[0] + 6, 10])
+        def gradient(direction, x):
+            # Jacobian.T @ direction
+            return np.vstack([-30*x[0]**2 + 10*x[0] + 6, 10]) @ direction
         model = cuqi.model.Model(forward, range_geometry=1, domain_geometry=2, gradient=gradient)
 
         # define prior

demos/demo33_testWang.py
@@ -3,7 +3,11 @@
 # Felipe Uribe @ DTU
 # =================================================================
 # Version 2022
-# =================================================================
+# %%=================================================================
+
+%load_ext autoreload
+%autoreload 2
+
 import sys
 sys.path.append("..") 
 import numpy as np
@@ -13,14 +17,16 @@ import scipy.stats as sps
 import cuqi
 import matplotlib.pyplot as plt
 
+
 # =================================================================
 tp = cuqi.testproblem.WangCubic()
 forward = tp.model.forward
+gradient = tp.model.gradient
 y_data = tp.data
 
 # Bayes
 pi_pr = tp.prior.pdf
-logpi_like = tp.likelihood.log
+loglike = tp.likelihood.log
 pi_pos = tp.posterior.pdf
 
 # =================================================================
@@ -47,26 +53,33 @@ fig.gca().set_aspect("equal")
 plt.tight_layout()
 plt.show()
 
-# ADD TEST: likelihood gradient checks
+# %%ADD TEST: likelihood gradient checks
+
+m = 1 # number of data points
+y_data = 1 # measured data
+sigma_obs = 1
+prec_obs = 1
+x0 = np.random.randn(2)
+
+# %%
+tp.likelihood.gradient(x0)
+
+# %%
+def logpi_like(x, grad=False):
+    forward_eval = forward(x)
+    misfit = y_data - forward_eval
+    if grad:
+        direction = np.array([prec_obs * misfit])
+        grad_eval = gradient(direction=direction, wrt=x)
+        return -0.5*(prec_obs * misfit**2 + np.log(2*np.pi) + np.log(sigma_obs)), grad_eval
+    else:
+        return -0.5*(prec_obs * misfit**2 + np.log(2*np.pi) + np.log(sigma_obs))#sps.norm.logpdf(y_data, forward_eval, sigma_obs)
+
+
+eval1, gradeval1 = logpi_like(x0, True)
 #
-# m = 1 # number of data points
-# y_data = 1 # measured data
-# sigma_obs = 1
-# prec_obs = 1
-# def logpi_like(x, grad=False):
-#     forward_eval = forward(x)
-#     misfit = y_data - forward_eval
-#     if grad:
-#         grad_eval = gradient(x)
-#         return -0.5*(prec_obs * misfit**2 + np.log(2*np.pi) + np.log(sigma_obs)), prec_obs * misfit * grad_eval
-#     else:
-#         return -0.5*(prec_obs * misfit**2 + np.log(2*np.pi) + np.log(sigma_obs))#sps.norm.logpdf(y_data, forward_eval, sigma_obs)
+eval2 = loglike(x0)
+gradeval2 = tp.likelihood.gradient(x0)
 #
-# x0 = np.random.randn(2)
-# eval1, gradeval1 = logpi_like(x0, True)
-# #
-# eval2 = loglike(x0)
-# gradeval2 = tp.likelihood.gradient(x0)
-# #
-# loglike = lambda x: logpi_like(x)
-# gradevalu3 = sp.optimize.approx_fprime(x0, loglike, 1e-5)
+loglike = lambda x: logpi_like(x)
+gradevalu3 = sp.optimize.approx_fprime(x0, loglike, 1e-5)