495f6e192ea155c2daf44dca5f9b05ca1ecf3d90
nabr <nabr@dtu.dk>
2022-05-21 23:15:37 +0200

    Do not reset name when conditioning

--- cuqi/distribution.py
+++ cuqi/distribution.py
@@ -194,7 +194,6 @@ class Distribution(ABC):
 
         # EVALUATE CONDITIONAL DISTRIBUTION
         new_dist = copy(self) #New cuqi distribution conditioned on the kwargs
-        new_dist.name = None  #Reset name to None
 
         # Go through every mutable variable and assign value from kwargs if present
         for var_key in mutable_vars:
