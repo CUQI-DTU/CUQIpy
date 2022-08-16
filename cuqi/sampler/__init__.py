ef27f26c42c5058c2108a2a9ea2932db45912c7f
nabr <nabr@dtu.dk>
2022-08-16 09:16:21 +0200

    Make submodules private

--- cuqi/sampler/__init__.py
+++ cuqi/sampler/__init__.py
@@ -1,18 +1,7 @@
-from .cwmh import CWMH
-from .hmc import NUTS
-from .langevin_algorithm import ULA, MALA
-from .laplace_approximation import UnadjustedLaplaceApproximation
-from .mh import MetropolisHastings
-from .pcn import pCN
-from .rto import Linear_RTO
-
-__all__ = [
-    'CWMH',
-    'NUTS',
-    'ULA',
-    'MALA',
-    'UnadjustedLaplaceApproximation',
-    'MetropolisHastings',
-    'pCN',
-    'Linear_RTO'
-]
+from ._cwmh import CWMH
+from ._hmc import NUTS
+from ._langevin_algorithm import ULA, MALA
+from ._laplace_approximation import UnadjustedLaplaceApproximation
+from ._mh import MetropolisHastings
+from ._pcn import pCN
+from ._rto import Linear_RTO
