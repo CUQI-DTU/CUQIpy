7527b30f7a2516554df01347744a3386df0a4339
nabr <nabr@dtu.dk>
2022-08-17 15:15:29 +0200

    Make geometry module a folder

---
+++ cuqi/geometry/__init__.py
@@ -0,0 +1,16 @@
+from ._geometry import (
+    _get_identity_geometries,
+    Geometry,
+    _WrappedGeometry,
+    Continuous,
+    Continuous1D,
+    Continuous2D,
+    Image2D,
+    Discrete,
+    MappedGeometry,
+    _DefaultGeometry,
+    KLExpansion,
+    KLExpansion_Full,
+    CustomKL,
+    StepExpansion
+)
