0c5b2d5e1be91eb53f5c4c8e4411a24b0b8ad328
nabr <nabr@dtu.dk>
2022-08-23 23:41:49 +0200

    Add tests for joint distribution

---
+++ tests/test_joint_distribution.py
@@ -0,0 +1,95 @@
+import cuqi
+import numpy as np
+import pytest
+
+def test_joint_dist_dim_geometry():
+    """ Test the dimension and geometry properties of a joint distribution """
+
+    model, data, _ = cuqi.testproblem.Poisson_1D.get_components() # Model is m times n, m != n.
+
+    # Bayesian model
+    d = cuqi.distribution.Gamma(1, 1e-4)
+    x = cuqi.distribution.GaussianCov(np.zeros(model.domain_dim), lambda d: d)
+    y = cuqi.distribution.GaussianCov(model, 1)
+
+    # Joint distribution
+    J = cuqi.distribution.JointDistribution([d, x, y])
+
+    # Test the dimensions
+    assert J.dim == [d.dim, x.dim, y.dim]
+
+    # Test the geometries
+    assert J.geometry == [d.geometry, x.geometry, y.geometry]
+
+    # Test dim and geometry for posterior
+    P = J(y=data)
+
+    assert P.dim == [d.dim, x.dim]
+
+    assert P.geometry == [d.geometry, x.geometry]
+
+
+@pytest.mark.parametrize("densities", [
+    [
+        cuqi.distribution.Gamma(1, 1e-4, name="x"),
+        cuqi.distribution.Normal(0, 1, name="y")
+    ],
+    [
+        cuqi.distribution.Gamma(1, 1e-4, name="x"),
+        cuqi.distribution.Normal(0, lambda x:x, name="y")
+    ],
+    [
+        cuqi.distribution.Gamma(1, 1e-4, name="d"),
+        cuqi.distribution.Gamma(1, 1e-2, name="l"),
+        cuqi.distribution.GaussianCov(np.zeros(8), lambda d: d, name="x"),
+        cuqi.distribution.GaussianCov(
+            mean=cuqi.testproblem.Deconvolution1D(dim=8).model,
+            cov=lambda l: l,
+            name="y"
+        )
+    ],
+    [
+        cuqi.distribution.Normal(0, 1e-2, name="z"),
+        cuqi.distribution.Gamma(1, lambda z: abs(z), name="d"),
+        cuqi.distribution.Gamma(lambda z: z, 1e-2, name="l"),
+        cuqi.distribution.GaussianCov(np.zeros(8), lambda d: d, name="x"),
+        cuqi.distribution.GaussianCov(
+            mean=cuqi.testproblem.Deconvolution1D(dim=8).model,
+            cov=lambda l: l,
+            name="y"
+        )
+    ],
+])
+def test_joint_dist_logd(densities):
+    """ Tests the log density function of the Joint distribution """
+
+    # Create a joint distribution
+    J = cuqi.distribution.JointDistribution(densities)
+
+    # Get the parameter names
+    names = J.get_parameter_names()
+
+    # Get dimensions
+    dims = J.dim
+
+    # Make dictionary of random parameters matching names and dimensions
+    params = {name: abs(np.random.randn(dim)) for name, dim in zip(names, dims)}
+
+    # Evaluate the log density function at random points
+    logd = J.logd(**params)
+
+    # Manually evaluate the log density function for each density
+    logd_manual = 0
+    for density in densities:
+        # Extract the parameters for this density
+        density_params = {name: params[name] for name in density.get_parameter_names()}
+        logd_manual += density.logd(**density_params)
+
+    # Compare the log density functions
+    assert logd == pytest.approx(logd_manual)
+
+
+
+
+
+
