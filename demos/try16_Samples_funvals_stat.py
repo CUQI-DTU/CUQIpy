64a8b0bac111a31d742175ec5d737c718b0e63b2
amal-ghamdi <amal.m.alghamdi@gmail.com>
2022-08-31 14:42:18 +0200

    Update demos/try16_Samples_funvals_stat.py
    update try script
    
    Co-authored-by: Jakob Sauer Jørgensen <jakobsj@users.noreply.github.com>

--- demos/try16_Samples_funvals_stat.py
+++ demos/try16_Samples_funvals_stat.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-samples = cuqi.samples.Samples(np.random.rand(10, 15), geometry = cuqi.geometry.MappedGeometry(cuqi.geometry.Continuous1D(10), map=lambda x:x**2))
+samples = cuqi.samples.Samples(3*np.random.rand(10, 15), geometry = cuqi.geometry.MappedGeometry(cuqi.geometry.Continuous1D(10), map=lambda x:x**2))
 
 # Compute statistics on parameter values
 samples.plot_ci(95, plot_par = True) #plot in parameter space
