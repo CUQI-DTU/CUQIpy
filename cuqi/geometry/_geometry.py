6ab205f940a76488b30a826d72336271cb854db8
amalghamdi <amal.m.alghamdi@gmail.com>
2022-08-31 14:54:43 +0200

    Update _get_identity_geometries documentation

--- cuqi/geometry/_geometry.py
+++ cuqi/geometry/_geometry.py
@@ -10,7 +10,7 @@ from functools import reduce
 import warnings
 
 def _get_identity_geometries():
-    """Return the geometries that have identity par2fun and fun2par methods (can still reshape).
+    """Return the geometries that have identity `par2fun` and `fun2par` methods (including those where `par2fun` and `fun2par` perform reshaping of the parameters or the function values array. e.g. the geometry `Image2D`.).
     These geometries do not alter the gradient computations.
     """
     return [_DefaultGeometry, Continuous1D, Continuous2D, Discrete, Image2D]
