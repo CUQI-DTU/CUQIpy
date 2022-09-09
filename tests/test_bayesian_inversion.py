5e50f6af71b6bf7929cd84db9005f2d7e5e79d1f
nabr <nabr@dtu.dk>
2022-09-09 14:19:55 +0200

    Remove import of sys in tests

--- tests/test_bayesian_inversion.py
+++ tests/test_bayesian_inversion.py
@@ -1,8 +1,7 @@
 import pytest
 import numpy as np
 import sys
-# import sys
-sys.path.append('../')
+
 from cuqi.testproblem import Deconvolution1D
 from cuqi.distribution import Gaussian, Gaussian, GMRF, Cauchy_diff, Laplace_diff, LMRF
 
