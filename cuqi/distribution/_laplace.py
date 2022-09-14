1e6b064284e33833336d9e84555ad65a776c983e
nabr <nabr@dtu.dk>
2022-09-14 23:42:31 +0200

    Add doc to Laplace distribution

--- cuqi/distribution/_laplace.py
+++ cuqi/distribution/_laplace.py
@@ -2,8 +2,28 @@ import numpy as np
 from cuqi.distribution import Distribution
 
 class Laplace(Distribution):
-    """
-    The variables of this Laplace distribution are iid.
+    """ Laplace distribution. 
+
+    Defines a Laplace distribution given a location and a precision. The Laplace distribution is defined as
+
+    .. math::
+
+        p(x) = \frac{1}{2b} \exp\left(-\frac{|x-\mu|}{b}\right),
+
+    where :math:`\mu` is the location and :math:`b` is the scale parameter.
+
+    The precision is defined as :math:`\lambda = \frac{1}{b}`.
+
+    The variables of this Laplace distribution are independent identically distributed (i.i.d.).
+
+    Parameters
+    ----------
+    location : scalar or ndarray
+        The location parameter of the distribution.
+
+    prec : scalar
+        The precision parameter of the distribution.
+
     """
 
     def __init__(self, location, prec, **kwargs):
