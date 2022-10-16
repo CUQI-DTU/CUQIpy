b0be1f7b386079821d4a0808d1ad292dbdf99df0
amal-ghamdi <amal.m.alghamdi@gmail.com>
2022-10-16 23:54:08 +0200

    Add documentation and fix noise estimation

--- demos/howtos/multiple_likelihoods.py
+++ demos/howtos/multiple_likelihoods.py
@@ -2,7 +2,7 @@
 How to set up multiple likelihoods 
 ==================================
 
-In this example we show how to set up multiple likelihoods for the same Bayesian parameter `x`, where each likelihood is associated with a different model set up.
+In this example we show how to set up multiple likelihoods for the same Bayesian parameter `x`, where each likelihood is associated with a different model set up. The model we use here is obtained from the test problem :class:`cuqi.testproblem.Poisson_1D`. See the class :class:`cuqi.testproblem.Poisson_1D` documentation for more details about the forward model.
 """
 # %% 
 # First we import the modules needed.
@@ -15,8 +15,9 @@ import matplotlib.pyplot as plt
 from math import ceil
 
 # %%
-# Provide an option for the cases we study in this demo. 
-# ------------------------------------------------------
+# Choose one of the two cases we study in this demo 
+# -------------------------------------------------
+# Choose `set_up = set_ups[0]` for the case where we have two 1D Poisson models that differ in the observation operator only. And choose `set_up = set_ups[1]` for the case where we have two 1D Poisson models that differ in the source term only.
 
 set_ups = ["multi_observation", "multi_source"]
 set_up = set_ups[0]
@@ -24,15 +25,16 @@ set_up = set_ups[0]
 assert set_up == "multi_observation" or set_up == "multi_source", "set_up must be either 'multi_observation' or 'multi_source'"
 
 # %%
-# Set up the models common parameters
-# -----------------------------------
+# Set up the parameters used in both models
+# -----------------------------------------
 
-dim = 50
-endpoint = 1
-field_type = "Step"
-SNR = 1000
-n_steps = 2
+dim = 50 # Number of the model grid points
+endpoint = 1 # The model domain is the interval [0, endpoint]
+field_type = "Step" # The conductivity (or diffusivity) field type
+SNR = 500 # Signal-to-noise ratio
+n_steps = 2 # Number of steps in the conductivity (or diffusivity) step field
 
+# Exact solution
 x_exact = np.empty(dim)
 x_exact[:ceil(dim/2)] = 1
 x_exact[ceil(dim/2):] = 3
@@ -44,13 +46,16 @@ x_exact[ceil(dim/2):] = 3
 
 observation_grid_map1 = None
 if set_up == "multi_observation":
+	# Observe on the first half of the domain
 	observation_grid_map1 = lambda x: x[np.where(x<.5)] 
 
+# The source term signal
 source1 = lambda xs: 20*np.sin(xs)+20.1
 
+# Obtain the forward model from the test problem
 model1, data1, problemInfo1 = cuqi.testproblem.Poisson_1D.get_components(dim=dim, endpoint=endpoint, field_type = field_type,field_params = {'n_steps':n_steps}, observation_grid_map = observation_grid_map1, exactSolution =x_exact , SNR =SNR)
 
-# plot data, exact data and exact solution
+# Plot data, exact data and exact solution
 plt.figure()
 data1.plot(label='data')
 problemInfo1.exactData.plot(label='exact data')
@@ -63,16 +68,19 @@ plt.legend()
 
 observation_grid_map2 = None
 if set_up == "multi_observation":
+        # Observe on the second half of the domain
 	observation_grid_map2 = lambda x: x[np.where(x>=.5)]
 
+# The source term signal
 if set_up == "multi_source":
 	source2 = lambda xs: 20*np.sin(2*xs)+20.1
 else:
 	source2 = source1
 
+# Obtain the forward model from the test problem
 model2, data2, problemInfo2 = cuqi.testproblem.Poisson_1D.get_components(dim=dim, endpoint=endpoint, field_type=field_type,field_params = {'n_steps': n_steps}, observation_grid_map = observation_grid_map2, exactSolution =x_exact , SNR =SNR)
 
-# plot data, exact data and exact solution
+# Plot data, exact data and exact solution
 plt.figure()
 data2.plot(label='data2')
 problemInfo2.exactData.plot(label='exact data2')
@@ -82,6 +90,7 @@ plt.legend()
 # %%
 # Create the prior
 # ----------------
+# Create the prior for the Bayesian parameter `x`, which is the conductivity (or diffusivity) of the medium. We use a Gaussian prior.
 
 x = cuqi.distribution.GaussianCov(np.zeros(model1.domain_dim), 3*np.ones(model1.domain_dim), geometry= model1.domain_geometry)
 
@@ -90,8 +99,8 @@ x = cuqi.distribution.GaussianCov(np.zeros(model1.domain_dim), 3*np.ones(model1.
 # ----------------------
 
 # Estimate the data noise standard deviation
-sigma_noise1 = np.linalg.norm(problemInfo1.exactData- data1)
-sigma_noise2 = np.linalg.norm(problemInfo2.exactData- data2)
+sigma_noise1 = np.linalg.norm(problemInfo1.exactData)/SNR
+sigma_noise2 = np.linalg.norm(problemInfo2.exactData)/SNR
 
 # Create the data distributions
 y1 = cuqi.distribution.Gaussian(mean=model1, std=sigma_noise1, corrmat=np.eye(model1.range_dim), geometry=model1.range_geometry)
@@ -107,11 +116,14 @@ z_joint = cuqi.distribution.JointDistribution(x,y1,y2)(y1=data1, y2=data2)._as_s
 # Sample from the posterior (multiple likelihoods)
 # ------------------------------------------------
 
+# Sample from the posterior
 sampler = cuqi.sampler.MetropolisHastings(z_joint)
 samples = sampler.sample_adapt(5000)
 
+# Set the samples geometry
 samples.geometry=x.geometry # this is will be not needed in the future as samples geometry will be automatically determined.
 
+# Plot the credible interval and compute the ESS
 samples.burnthin(1000).plot_ci(95, exact = problemInfo1.exactSolution)
 samples.compute_ess()
 
@@ -125,9 +137,11 @@ z1 = cuqi.distribution.JointDistribution(x,y1)(y1=data1)
 # Sample from the posterior (single likelihoods, first model)
 # -----------------------------------------------------------
 
+# Sample from the posterior
 sampler = cuqi.sampler.MetropolisHastings(z1)
 samples = sampler.sample_adapt(5000)
 
+# Plot the credible interval and compute the ESS
 samples.burnthin(1000).plot_ci(95, exact = problemInfo1.exactSolution)
 samples.compute_ess()
 
@@ -141,11 +155,10 @@ z2 = cuqi.distribution.JointDistribution(x,y2)(y2=data2)
 # Sample from the posterior (single likelihoods, second model)
 # ------------------------------------------------------------
 
+# Sample from the posterior
 sampler = cuqi.sampler.MetropolisHastings(z2)
 samples = sampler.sample_adapt(5000)
 
+# Plot the credible interval and compute the ESS
 samples.burnthin(1000).plot_ci(95, exact = problemInfo1.exactSolution)
 samples.compute_ess()
-
-
-# %%
