453581f466400a23e42e1cc5f5e61fdcb455f277
amal-ghamdi <amal.m.alghamdi@gmail.com>
2022-12-05 20:16:05 +0100

    Add documentation

--- demos/demo35_FD_gradient.py
+++ demos/demo35_FD_gradient.py
@@ -1,24 +1,26 @@
-# %%
+# %% Import required packages
 import cuqi
 import matplotlib.pyplot as plt
 
-#%%
+#%% Create test problem
 TP = cuqi.testproblem.Heat_1D(dim=60, max_time=0.01, field_type='Step')
 
+#%% Get the posterior
 posterior = TP.posterior
 
+#%% Sample from the posterior using Metropolis-Hastings
 MH_sampler = cuqi.sampler.MetropolisHastings(posterior)
 MH_samples = MH_sampler.sample_adapt(1000)
 MH_samples.plot_ci(95,exact=TP.exactSolution)
 
-#%%
+#%% Sample from the posterior using MALA
 plt.figure()
 posterior.use_FD = True
 MALA_sampler = cuqi.sampler.MALA(posterior, 0.0001)
 MALA_samples = MALA_sampler.sample_adapt(1000)
 MALA_samples.plot_ci(95,exact=TP.exactSolution)
 
-#%%
+#%% Plot the ESS of the two chains
 plt.figure()
 plt.plot(MH_samples.compute_ess(), label='MH ESS', marker='o')
 plt.plot(MALA_samples.compute_ess(), label='MALA ESS', marker='o')
