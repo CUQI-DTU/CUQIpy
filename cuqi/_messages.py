7cb19736a6e433e3975c037bd60519532737dfa1
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-01-13 11:55:30 +0100

    Update module describtion

--- cuqi/_messages.py
+++ cuqi/_messages.py
@@ -1,4 +1,4 @@
-""" This module defines variables that are used throughout CUQIpy. The values of these variables are not meant to be changed by the user."""
+""" This module defines messages (errors, warnings, etc) that are used throughout CUQIpy. The values of the following variables are not meant to be changed by the user."""
 
 _disable_warning_msg = lambda module_name: "To disable "+\
 "warnings for a given module or library, "+\
