9a9d6a0aa9ec8525370f0ca7e5ac1628c7115ec3
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-01-20 11:18:26 +0100

    update PEP

--- cuqi/_version.py
+++ cuqi/_version.py
@@ -43,7 +43,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep517"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "cuqi-"
     cfg.versionfile_source = "cuqi/_version.py"
