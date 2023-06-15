acbf8af2ca0178890accac4ba3358c18b686bb01
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-06-15 12:26:34 +0200

    update demo

--- demos/dev/funvec_representation.py
+++ demos/dev/funvec_representation.py
@@ -1,8 +1,8 @@
 """
-funvec variable representation supported by CUQIpy Geometry classes
-====================================================================
+Support vector representation of function values in CUQIpy geometries and samples
+====================================================================================
 
-    In this demo, we illustrate the utility of an introduced representation of variables in CUQIpy (besides the already introduced parameter and the function value representation). This representation, called funvec, is a vector represnetation of the function values. The function values generally can be an n-dimensional array or any other type of object. When needed and when possible, funvec is used to represent the function values as a vector (1D) array. funvec, is meant to be used internally by other CUQIpy classes and is not expected to be of interest to the typical user. In particular, funvec is used to represent the function values of samples in a vector format. This is useful for computing statistics on the function values of samples. 
+In this demo, we illustrate the utility of an introduced representation of variables in CUQIpy (besides the already introduced parameter and the function value representation). This representation, is a vector representation of the function values. The function values generally can be an n-dimensional array or any other type of object. When needed and when possible, this introduced representation is used to represent the function values as a vector (1D) array. It is meant to be used internally by other CUQIpy classes and is not expected to be of interest to the typical user. In particular, it is used to represent the function values of samples in a vector format. This is useful for computing statistics on the function values of samples. 
 """
 # %%
 # Import the necessary modules
@@ -26,14 +26,23 @@ geom_2D = cuqi.geometry.MappedGeometry(
     cuqi.geometry.Image2D((3, 3)),
     map=lambda x: x**2)
 
+# %%
+# Query the geometries `fun_is_array` property
+print(geom_1D.fun_is_array)
+print(geom_2D.fun_is_array)
+
 #%%
-# Query the `shape` of the  function value for both geometries
+# This property indicates whether the function value representation is an 
+# array (not necessarily a 1D array) or not. 
+
+#%%
+# Query the `shape` of the function value representation for both geometries
 print(geom_1D.fun_shape)
 print(geom_2D.fun_shape)
 
 # %%
 # Create random samples corresponding to each geometry
-# ----------------------------
+# ----------------------------------------------------
 
 #%%
 # Create samples of random parameter values for each geometry
@@ -41,51 +50,71 @@ samples_1D = cuqi.samples.Samples(np.random.rand(10, 5), geometry=geom_1D)
 samples_2D = cuqi.samples.Samples(np.random.rand(9, 5), geometry=geom_2D)
 
 # %%
-# Query the geometries `has_funvec` property
-print(geom_1D.has_funvec)
-print(geom_2D.has_funvec)
-
-#%%
-# Note that only the 2D geometry has a funvec representation. The 1D
-# geometry does not need this representation because the function values
-# are already represented as a vector (1D array).
-
-# %%
 # Obtain the corresponding samples of function values (`funvals`)  
 # ---------------------------------------------------------------
 
-samples_1D_funvals = samples_1D.funvals
-samples_2D_funvals = samples_2D.funvals
+samples_1D.funvals
+samples_2D.funvals
 
 # %%
-# query the `shape` of the `funvals` for both geometries
-print(samples_1D_funvals.shape)
-print(samples_2D_funvals.shape)
+# Note that the `funvals` property of the `Samples` class returns a `Samples` object
 
 # %%
-# Note that the `funvals` `shape` for the 2D geometry is (9, 5) and not (3, 3, 5).
-# To understand why this is the case, we query the `funvals` 
-# samples'`_funvals_directly_supported` property.
-print(samples_1D_funvals._funvals_directly_supported)
-print(samples_2D_funvals._funvals_directly_supported)
+# query the `shape` of the `.funvals` property for both samples objects
+print(samples_1D.funvals.shape)
+print(samples_2D.funvals.shape)
+
+# %% 
+# Note that for the 2D geometry, the `shape` of the `funvals` is (3, 3, 5). This is because the function value representation is a 2D array.
 
 # %%
-# Note that the 1D samples `funvals` are directly supported because par2fun
-# results in a 1D array of function values. However, `funvals` for 2D samples is
-# not directly supported because par2fun results in a 2D array representing the 
-# function values.
-#
-# However, `Samples` class can use `fun2funvec` methods of the geometry to convert to 
-# function value to a vector representation of the these values. In the `Image2D`
-# geometry, this is done by flattening the 2D array of the function value 
-# representation, thus we see that the shape of the funvals is (9, 5) and not
-# (3, 3, 5).
-# 
-# The flag `is_funvec` is used to indicate whether the samples `funvals` are in 
-# funvec representation or not (i.e. obtained using `fun2funvec` methods or not). 
+# query the `shape` of the `.funvals.vector` property for both samples objects
+print(samples_1D.funvals.vector.shape)
+print(samples_2D.funvals.vector.shape)
 
-print(samples_1D_funvals._is_funvec)
-print(samples_2D_funvals._is_funvec)
+# %%
+# Now for both geometries, the converted Samples object is a two
+# dimensional arrays. In this case, executing the `funvals` property leads to 
+# calling the `par2fun` method of the geometry to convert the parameter values
+# to function values. Then executing the `vector` property leads to calling the
+# `fun2vec` method of the geometry to convert the function values to a vector
+# representation of these values.
+
+# %%
+# query the `shape` of the `.vector` property for both geometries
+print(samples_1D.vector.shape)
+print(samples_2D.vector.shape)
+
+# %% 
+# For samples_1D, we query the is par and is vec properties for all 
+# representations of the samples
+
+print(samples_1D.is_par)
+print(samples_1D.is_vec)
+
+print(samples_1D.vector.is_par)
+print(samples_1D.vector.is_vec)
+
+print(samples_1D.funvals.is_par)
+print(samples_1D.funvals.is_vec)
+
+print(samples_1D.funvals.vector.is_par)
+print(samples_1D.funvals.vector.is_vec)
+
+# %% 
+# We do the same for samples_2D
+
+print(samples_2D.is_par)
+print(samples_2D.is_vec)
+
+print(samples_2D.vector.is_par)
+print(samples_2D.vector.is_vec)
+
+print(samples_2D.funvals.is_par)
+print(samples_2D.funvals.is_vec)
+
+print(samples_2D.funvals.vector.is_par)
+print(samples_2D.funvals.vector.is_vec)
 
 # %%
 # Computing statistics on parameter and on function values for samples (the 1D geometry case)
@@ -95,16 +124,16 @@ print(samples_2D_funvals._is_funvec)
 # Compute mean on parameter values and on function values
 plt.figure()
 samples_1D.plot_mean(color='r')
-samples_1D_funvals.plot_mean(color='b', linestyle='--')
+samples_1D.funvals.plot_mean(color='b', linestyle='--')
 
 # %%
 # Compute variance on parameter values and on function values
 plt.figure()
 samples_1D.plot_variance(color='r')
-samples_1D_funvals.plot_variance(color='b', linestyle='--')
+samples_1D.funvals.plot_variance(color='b', linestyle='--')
 
 # %% 
-# Note that in both cases the mean and the variance computed on parameter values are the same as the ones computed on function values. This is because computing the mean and variance on the parameter values then converting the results to function values is equivalent to computing the mean and variance on the function values directly in this case.
+# Note that in both cases the mean and the variance computed on parameter values are the same as the ones computed on function values. This is because the map from parameter values to function values is identity in this case. 
 
 # %%
 # Computing statistics on parameter and on function values (funvec) for samples (the 2D geometry case)
@@ -115,8 +144,13 @@ samples_1D_funvals.plot_variance(color='b', linestyle='--')
 plt.figure()
 samples_2D.plot_mean()
 plt.colorbar()
+
 plt.figure()
-samples_2D_funvals.plot_mean()
+samples_2D.funvals.plot_mean()
+plt.colorbar()
+
+plt.figure()
+samples_2D.funvals.vector.plot_mean()
 plt.colorbar()
 
 # %%
@@ -124,8 +158,13 @@ plt.colorbar()
 plt.figure()
 samples_2D.plot_variance()
 plt.colorbar()
+
 plt.figure()
-samples_2D_funvals.plot_variance()
+samples_2D.funvals.plot_variance()
+plt.colorbar()
+
+plt.figure()
+samples_2D.funvals.vector.plot_variance()
 plt.colorbar()
 
 # %%
