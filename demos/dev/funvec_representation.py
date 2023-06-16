8e8faa86fa90622b0944f4f9a00dee7af225996d
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-06-16 14:40:31 +0200

    Update demo

--- demos/dev/funvec_representation.py
+++ demos/dev/funvec_representation.py
@@ -57,115 +57,199 @@ samples_1D.funvals
 samples_2D.funvals
 
 # %%
-# Note that the `funvals` property of the `Samples` class returns a `Samples` object
+# Note that the `funvals` property of the `Samples` class returns a `Samples`
+# object
 
 # %%
 # query the `shape` of the `.funvals` property for both samples objects
+
+print("samples_1D.funvals.shape")
 print(samples_1D.funvals.shape)
+print("samples_2D.funvals.shape")
 print(samples_2D.funvals.shape)
 
 # %% 
-# Note that for the 2D geometry, the `shape` of the `funvals` is (3, 3, 5). This is because the function value representation is a 2D array.
+# Note that for the 2D geometry, the `shape` of the `funvals` is (3, 3, 5). This
+# is because the function value representation is a 2D array.
 
 # %%
 # query the `shape` of the `.funvals.vector` property for both samples objects
+
+print("samples_1D.funvals.vector.shape")
 print(samples_1D.funvals.vector.shape)
+print("samples_2D.funvals.vector.shape")
 print(samples_2D.funvals.vector.shape)
 
 # %%
-# Now for both geometries, the converted Samples object is a two
+# Now for both geometries, the samples in the converted Samples object is a two
 # dimensional arrays. In this case, executing the `funvals` property leads to 
 # calling the `par2fun` method of the geometry to convert the parameter values
 # to function values. Then executing the `vector` property leads to calling the
-# `fun2vec` method of the geometry to convert the function values to a vector
-# representation of these values.
+# `fun2vec` method of the geometry (if implemented) to convert the function 
+# values to a vector representation of these values.
 
 # %%
 # query the `shape` of the `.vector` property for both geometries
+
+print("samples_1D.vector.shape")
 print(samples_1D.vector.shape)
+print("samples_2D.vector.shape")
 print(samples_2D.vector.shape)
 
 # %% 
-# For samples_1D, we query the is par and is vec properties for all 
+# For samples_1D, we query the `is_par` and `is_vec` properties for all 
 # representations of the samples
 
+print("samples_1D.is_par")
 print(samples_1D.is_par)
+print("samples_1D.is_vec")
 print(samples_1D.is_vec)
 
+print("samples_1D.vector.is_par")
 print(samples_1D.vector.is_par)
+print("samples_1D.vector.is_vec")
 print(samples_1D.vector.is_vec)
 
+print("samples_1D.funvals.is_par")
 print(samples_1D.funvals.is_par)
+print("samples_1D.funvals.is_vec")
 print(samples_1D.funvals.is_vec)
 
+print("samples_1D.funvals.vector.is_par")
 print(samples_1D.funvals.vector.is_par)
+print("samples_1D.funvals.vector.is_vec")
 print(samples_1D.funvals.vector.is_vec)
+
+# %%
+# Note that for this 1D geometry, the `is_vec` property is always `True`. This
+# is because the function value representation is a 1D array.
 
 # %% 
 # We do the same for samples_2D
 
+print("samples_2D.is_par")
 print(samples_2D.is_par)
+print("samples_2D.is_vec")
 print(samples_2D.is_vec)
 
+print("samples_2D.vector.is_par")
 print(samples_2D.vector.is_par)
+print("samples_2D.vector.is_vec")
 print(samples_2D.vector.is_vec)
 
+print("samples_2D.funvals.is_par")
 print(samples_2D.funvals.is_par)
+print("samples_2D.funvals.is_vec")
 print(samples_2D.funvals.is_vec)
 
+print("samples_2D.funvals.vector.is_par")
 print(samples_2D.funvals.vector.is_par)
+print("samples_2D.funvals.vector.is_vec")
 print(samples_2D.funvals.vector.is_vec)
 
 # %%
-# Computing statistics on parameter and on function values for samples (the 1D geometry case)
+# Note that the property `samples_2D.funvals.is_vec` is false, but after 
+# converting the function values to a vector representation using the `vector` 
+# property, the property `samples_2D.vector.funvals.vector.is_vec` is true. 
+
+# %%
+# Computing statistics on parameter and on function values for samples (the 1D
+# geometry case)
 # --------------------------------------------------------------------------------------------
 
 # %%
-# Compute mean on parameter values and on function values
+# Compute and plot mean on parameter values and on function values
+print("samples_1D.plot_mean")
 plt.figure()
 samples_1D.plot_mean(color='r')
+print("samples_1D.funvals.plot_mean")
+plt.figure()
 samples_1D.funvals.plot_mean(color='b', linestyle='--')
 
 # %%
 # Compute variance on parameter values and on function values
+print("samples_1D.plot_variance")
 plt.figure()
 samples_1D.plot_variance(color='r')
+print("samples_1D.funvals.plot_variance")
+plt.figure()
 samples_1D.funvals.plot_variance(color='b', linestyle='--')
 
 # %% 
-# Note that in both cases the mean and the variance computed on parameter values are the same as the ones computed on function values. This is because the map from parameter values to function values is identity in this case. 
+# Note that in both cases the mean and the variance computed on parameter values
+# are the same as the ones computed on function values. This is because the map
+# from parameter values to function values is identity in this case. 
 
 # %%
-# Computing statistics on parameter and on function values (funvec) for samples (the 2D geometry case)
+# Computing statistics on parameter and on function values for samples
+# (the 2D geometry case)
 # ----------------------------------------------------------------------------------------------------
 
 # %%
-# Compute mean on parameter and function values
+# Compute mean on parameter values, function values, and vector representation 
+# of function values
+print("samples_2D.plot_mean")
 plt.figure()
 samples_2D.plot_mean()
 plt.colorbar()
 
+print("samples_2D.funvals.plot_mean")
 plt.figure()
 samples_2D.funvals.plot_mean()
 plt.colorbar()
 
+print("samples_2D.funvals.vector.plot_mean")
 plt.figure()
 samples_2D.funvals.vector.plot_mean()
 plt.colorbar()
 
 # %%
-# Compute variance on parameter and function values
+# Compute variance on parameter values, function values, and vector representation 
+# of function values
+print("samples_2D.plot_variance")
 plt.figure()
 samples_2D.plot_variance()
 plt.colorbar()
 
+print("samples_2D.funvals.plot_variance")
 plt.figure()
 samples_2D.funvals.plot_variance()
 plt.colorbar()
 
+print("samples_2D.funvals.vector.plot_variance")
 plt.figure()
 samples_2D.funvals.vector.plot_variance()
 plt.colorbar()
 
 # %%
-# Note that in both cases the mean and variance computed on parameter values are not the same as the ones computed on function values. This is because computing the mean and variance on the parameter values then converting the results to a function value is not equivalent to computing the mean and variance directly on the function values in this case, due to the nonlinear mapping `lambda x: x**2`. Also note that, internally, the mean and variance computed on the function values are computed on the vector representation of the function values, then plotted as functions (2D image in this case).
+# Note that in both cases the mean and variance computed on parameter values are
+# not the same as the ones computed on function values. This is because 
+# computing the mean and variance on the parameter values then converting the 
+# results to a function value is not equivalent to computing the mean and 
+# variance directly on the function values in this case, due to the nonlinear 
+# mapping `lambda x: x**2`. Also note that, in this case, we are able to compute
+# the statistics directly on the function values, but in some geometries, this
+# may not be possible and we may need to use the vector representation of the
+# function values to compute the statistics.
+
+# Plotting samples
+# ------------------------------------
+# Plot samples from `samples_2D`:
+print("samples_2D.plot")
+plt.figure()
+samples_2D.plot([1, 2, 3, 4])
+
+# %%
+# Plot samples from `samples_2D.funvals`:
+print("samples_2D.funvals.plot")
+plt.figure()
+samples_2D.funvals.plot([1, 2, 3, 4])
+
+# %%
+# Plot samples from `samples_2D.funvals.vector`:
+print("samples_2D.funvals.vector.plot")
+plt.figure()
+samples_2D.funvals.vector.plot([1, 2, 3, 4])
+
+# %%
+# The results are the same in all the three cases. Note that for the first case, the default behavior of the `plot` method is to convert the samples to the function values and then plot them.
