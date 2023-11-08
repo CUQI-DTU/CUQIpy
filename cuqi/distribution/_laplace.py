eb8e8295b4f2b8e6d04c62844499261ad475d35d
Chao Zhang <chao.zhang@math.tu-chemnitz.de>
2023-11-08 17:05:33 +0100

    rename precision from Laplace distribution to rate

--- cuqi/distribution/_laplace.py
+++ cuqi/distribution/_laplace.py
@@ -4,7 +4,7 @@ from cuqi.distribution import Distribution
 class Laplace(Distribution):
     """ Laplace distribution. 
 
-    Defines a Laplace distribution given a location and a precision. The Laplace distribution is defined as
+    Defines a Laplace distribution given a location and a rate parameter. The Laplace distribution is defined as
 
     .. math::
 
@@ -12,7 +12,7 @@ class Laplace(Distribution):
 
     where :math:`\mu` is the location and :math:`b` is the scale parameter.
 
-    The precision is defined as :math:`\lambda = \\frac{1}{b}`.
+    The rate parameter is defined as :math:`\lambda = \\frac{1}{b}`.
 
     The variables of this Laplace distribution are independent identically distributed (i.i.d.).
 
@@ -21,27 +21,27 @@ class Laplace(Distribution):
     location : scalar or ndarray
         The location parameter of the distribution.
 
-    prec : scalar
-        The precision parameter of the distribution.
+    rate : scalar
+        The rate parameter of the distribution.
 
     """
 
-    def __init__(self, location, prec, **kwargs):
+    def __init__(self, location, rate, **kwargs):
 
         # Init from abstract distribution class
         super().__init__(**kwargs)
 
         self.location = location
-        self.prec = prec
+        self.rate = rate
   
     def logpdf(self, x):
         if isinstance(x, (float,int)):
             x = np.array([x])
-        return self.dim*(np.log(self.prec/2)) - self.prec*np.linalg.norm(x-self.location,1)
+        return self.dim*(np.log(self.rate/2)) - self.rate*np.linalg.norm(x-self.location,1)
 
     def _sample(self,N=1,rng=None):
         if rng is not None:
-            s =  rng.laplace(self.location, 1.0/self.prec, (N,self.dim)).T
+            s =  rng.laplace(self.location, 1.0/self.rate, (N,self.dim)).T
         else:
-            s = np.random.laplace(self.location, 1.0/self.prec, (N,self.dim)).T
+            s = np.random.laplace(self.location, 1.0/self.rate, (N,self.dim)).T
         return s
