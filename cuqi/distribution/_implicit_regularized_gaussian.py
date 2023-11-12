33213421b41d4524245e3d6201d67333fddc6dcb
nabr <nabr@dtu.dk>
2023-11-12 23:39:31 +0100

    Simplify ImplicitRegularizedGMRF

--- cuqi/distribution/_implicit_regularized_gaussian.py
+++ cuqi/distribution/_implicit_regularized_gaussian.py
@@ -51,7 +51,12 @@ class ImplicitRegularizedGaussian(Distribution):
         # Init from abstract distribution class
         super().__init__(**kwargs)
 
-        # Check only one of proximal, projector, constraint or regularization is provided        
+        self._parse_regularization_input_arguments(proximal, projector, constraint, regularization)
+
+    def _parse_regularization_input_arguments(self, proximal, projector, constraint, regularization):
+        """ Parse regularization input arguments with guarding statements and store internal states """
+
+                # Check only one of proximal, projector, constraint or regularization is provided        
         if (proximal is not None) + (projector is not None) + (constraint is not None) + (regularization is not None) > 1:
             raise ValueError("Only one of proximal, projector, constraint or regularization can be provided.")
         
@@ -89,7 +94,6 @@ class ImplicitRegularizedGaussian(Distribution):
             self._preset = "l1"
         else:
             raise ValueError("Regularization not supported")
-            
 
     # This is a getter only attribute for the underlying Gaussian
     # It also ensures that the name of the underlying Gaussian
@@ -197,9 +201,7 @@ class ImplicitRegularizedGaussian(Distribution):
         return new_density
     
 
-
-    
-class ImplicitRegularizedGMRF(Distribution):
+class ImplicitRegularizedGMRF(ImplicitRegularizedGaussian):
     """
     Implicit Regularized GMRF (Gaussian Markov Random Field) distribution, to be used for the RegularizedLinearRTO sampler. 
 
@@ -233,151 +235,13 @@ class ImplicitRegularizedGMRF(Distribution):
         Preset regularization, including "l1". Required for use in Gibbs in future update.
 
     """
-        
+    # TODO: Once GMRF is updated, add default None to mean and prec here.
     def __init__(self, mean, prec, physical_dim=1, bc_type='zero', order=1, proximal = None, projector = None, constraint = None, regularization = None, **kwargs):
-        # Underlying explicity Gaussian
-        self._gaussian = GMRF(mean, prec, physical_dim=physical_dim, bc_type=bc_type, order=order)
-        
-        super().__init__(geometry = self._gaussian.geometry, **kwargs) 
-        
-        if (proximal is not None) + (projector is not None) + (constraint is not None) + (regularization is not None) != 1:
-            raise ValueError("Incorrect parameters")
-        # Init from abstract distribution class
-
-        if proximal is not None:
-            if not callable(proximal):
-                raise ValueError("Proximal needs to be callable.")
-            if len(get_non_default_args(proximal)) != 2:
-                raise ValueError("Proximal should take 2 arguments.")
             
-        if projector is not None:
-            if not callable(projector):
-                raise ValueError("Projector needs to be callable.")
-            if len(get_non_default_args(projector)) != 1:
-                raise ValueError("Projector should take 1 argument.")
+            # Underlying explicit Gaussian
+            self._gaussian = GMRF(mean, prec, physical_dim=physical_dim, bc_type=bc_type, order=order, **kwargs)
             
-        # Preset information, for use in Gibbs
-        self._preset = None
-        
-        if proximal is not None:
-            self._proximal = proximal
-        elif projector is not None:
-            self._proximal = lambda z, gamma: projector(z)
-        elif (isinstance(constraint, str) and constraint.lower() in ["nonnegativity", "nonnegative", "nn"]):
-            self._proximal = lambda z, gamma: ProjectNonnegative(z)
-            self._preset = "nonnegativity"
-        elif (isinstance(constraint, str) and constraint.lower() in ["box"]):
-            self._proximal = lambda z, gamma: ProjectBox(z)
-            self._preset = "box" # Not supported in Gibbs
-        elif (isinstance(regularization, str) and regularization.lower() in ["l1"]):
-            self._proximal = ProximalL1
-            self._preset = "l1"
-        else:
-            raise ValueError("Regularization not supported")
-        
-        
+            # Init from abstract distribution class
+            super(Distribution, self).__init__(**kwargs)
 
-    # This is a getter only attribute for the underlying Gaussian
-    # It also ensures that the name of the underlying Gaussian
-    # matches the name of the implicit regularized Gaussian
-    @property
-    def gaussian(self):
-        if self._name is not None:
-            self._gaussian._name = self._name
-        return self._gaussian
-    
-    @property
-    def proximal(self):
-        return self._proximal
-    
-    @property
-    def preset(self):
-        return self._preset
-
-    def logpdf(self, x):
-        return np.nan
-        #raise ValueError(
-        #    f"The logpdf of a implicit regularized Gaussian distribution need not be defined.")
-        
-    def _sample(self, N, rng=None):
-        raise ValueError(
-            "There is no known way of efficiently sampling from a implicit regularized Gaussian distribution.")
-  
-
-    # --- Defer behavior of the underlying Gaussian --- #
-    @property
-    def geometry(self):
-        return self.gaussian.geometry
-    
-    @geometry.setter
-    def geometry(self, value):
-        self._gaussian.geometry = value
-    
-    @property
-    def mean(self):
-        return self.gaussian.mean
-    
-    @mean.setter
-    def mean(self, value):
-        self.gaussian.mean = value
-    
-    @property
-    def cov(self):
-        return self.gaussian.cov
-    
-    @cov.setter
-    def cov(self, value):
-        self.gaussian.cov = value
-    
-    @property
-    def prec(self):
-        return self.gaussian.prec
-    
-    @prec.setter
-    def prec(self, value):
-        self.gaussian.prec = value
-    
-    @property
-    def sqrtprec(self):
-        return self.gaussian.sqrtprec
-    
-    @sqrtprec.setter
-    def sqrtprec(self, value):
-        self.gaussian.sqrtprec = value
-    
-    @property
-    def sqrtcov(self):
-        return self.gaussian.sqrtcov
-    
-    @sqrtcov.setter
-    def sqrtcov(self, value):
-        self.gaussian.sqrtcov = value     
-    
-    def get_conditioning_variables(self):
-        return self.gaussian.get_conditioning_variables()
-    
-    def get_mutable_variables(self):
-        return self.gaussian.get_mutable_variables()
-    
-    # Overwrite the condition method such that the underlying Gaussian is conditioned in general, except when conditioning on self.name
-    # which means we convert Distribution to Likelihood or EvaluatedDensity.
-    def _condition(self, *args, **kwargs):
-
-        # Handle positional arguments (similar code as in Distribution._condition)
-        cond_vars = self.get_conditioning_variables()
-        kwargs = self._parse_args_add_to_kwargs(cond_vars, *args, **kwargs)
-
-        # When conditioning, we always do it on a copy to avoid unintentional side effects
-        new_density = self._make_copy()
-
-        # Check if self.name is in the provided keyword arguments.
-        # If so, pop it and store its value.
-        value = kwargs.pop(self.name, None)
-
-        new_density._gaussian = self.gaussian._condition(**kwargs)
-
-        # If self.name was provided, we convert to a likelihood or evaluated density
-        if value is not None:
-            new_density = new_density.to_likelihood(value)
-
-        return new_density
+            self._parse_regularization_input_arguments(proximal, projector, constraint, regularization)
