f561086ce8aa61e51f6d64e2ab6b766a231baacb
Chao Zhang <chao.zhang@math.tu-chemnitz.de>
2023-11-14 15:12:40 +0100

    keep coding style consistent

--- demos/try12_Deconvolution_BayesianProblem.py
+++ demos/try12_Deconvolution_BayesianProblem.py
@@ -33,10 +33,10 @@ TP.prior = CMRF(location=0, scale=0.01, bc_type="zero", geometry=TP.model.domain
 #TP.prior = LMRF(location=0, scale=0.01, bc_type="neumann", geometry=TP.model.domain_geometry)
 
 # Bad choices (ignore) both 1D and 2D
-# TP.prior = Beta(2*np.ones(n), 5*np.ones(n)) #Might need tuning
-# TP.prior = Laplace(np.zeros(n), 1/10) #Might need tuning
-# TP.prior = InverseGamma(3*np.ones(n), np.zeros(n), 1*np.ones(n)) #Bad choice in general.. #Might need tuning
-# TP.prior = Lognormal(mean=np.zeros(n), cov=0.05) #NUTS ACTS out!
+#TP.prior = Beta(2*np.ones(n), 5*np.ones(n)) #Might need tuning
+#TP.prior = Laplace(np.zeros(n), 1/10) #Might need tuning
+#TP.prior = InverseGamma(3*np.ones(n), np.zeros(n), 1*np.ones(n)) #Bad choice in general.. #Might need tuning
+#TP.prior = Lognormal(mean=np.zeros(n), cov=0.05) #NUTS ACTS out!
 
 # %% Samples
 samples = TP.sample_posterior(Ns)
