f9a7f40afe8737a01cd75f3c8eaf9c9ff93a3b74
nabr <nabr@dtu.dk>
2024-01-16 21:36:41 +0100

    Fix unit tests for regularizedGaussian

--- tests/test_implicit_priors.py
+++ tests/test_implicit_priors.py
@@ -3,16 +3,16 @@ import numpy as np
 import pytest
 
 def test_RegularizedGaussian_default_init():
-    """ Test that the implicit regularized Gaussian is initialized by default with nonnegativity preset """
+    """ Test that the implicit regularized Gaussian requires at least 1 regularization argument """
 
-    x = cuqi.implicitprior.RegularizedGaussian(np.zeros(5), 1)
-    assert x._preset == "nonnegativity"
+    with pytest.raises(ValueError, match="Precisely one of "):
+        x = cuqi.implicitprior.RegularizedGaussian(np.zeros(5), 1)
 
 def test_RegularizedGaussian_guarding_statements():
     """ Test that we catch incorrect initialization of RegularizedGaussian """
 
     # More than 1 argument
-    with pytest.raises(ValueError, match="Only one of "):
+    with pytest.raises(ValueError, match="Precisely one of "):
         cuqi.implicitprior.RegularizedGaussian(np.zeros(5), 1, proximal=lambda s,z: s, constraint="nonnegativity")
 
     # Proximal
