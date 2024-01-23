bc4038b173fe824dfa703a76f0a250f187fddd34
Chao <chaz@dtu.dk>
2024-01-23 21:23:36 +0100

    add experimental code with Gibbs

--- sampler_test.py
+++ sampler_test.py
@@ -26,9 +26,47 @@ sampler.save_checkpoint('checkpoint.pickle')
 np.random.seed(0)
 
 sampler.reset()
-sampler.sample(1000)
 
+
+
+# user interface for doing sampling
+# 1) current way
+sam = sampler.sample(1000, warm_up=1000)
+# potentialy reset sampler after sampling call (don't have to)
+sam2 = sampler.sample(1000) # sample with the same state as the previous call
+# 2) object oriented way
+sampler.warmup(100)
+sampler.sample(1000)
 samples = sampler.get_samples()
+# 3)
+sam = sample("MH", x, x0=x0, scale=0.1, n_samples=1000, warm_up=1000)
+# sampler is created and deleted dynamically
+# 4)
+# use option 2 for sampling under BayesianProblem's interface
+
+# user interface for BayesianProblem
+# 1)
+BP.MAP()
+# 2)
+BP.MAP
+# what happen we call it multiple times?
+# 3)
+BP.compute_MAP()
+BP.get_MAP()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 f, axes = plt.subplots(1,2)
 
