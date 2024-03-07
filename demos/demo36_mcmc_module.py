f87ab8c7df1c0da2e1b3bb735e884237923be1a8
nabr <nabr@dtu.dk>
2024-03-07 08:37:14 +0100

    Add demo script for MCMC module

--- demos/demo36_mcmc_module.py
+++ demos/demo36_mcmc_module.py
@@ -37,7 +37,7 @@ print(sampler.scale) # Should be the same
 sampler.sample(500)
 sampler.warmup(100)
 sampler.warmup(100)
-sampler.sample(200)
+sampler.sample(500)
 
 # %%
 # Getting samples
@@ -45,8 +45,37 @@ sampler.sample(200)
 # To get samples one does as follows:
 
 samples = sampler.get_samples()
+print(samples)
+print(f"Shape: {samples.shape}")
 
+# Initial point, and all samples (both warmup and sample) are stored
+# It is currently up to the user to remove burn-in after sampling, e.g.
+samples.burnthin(1401).plot_pair()
 
+# %%
+# Checkpointing
+#
+# The new sampler supports checkpointing by saving and loading state (not samples) For example:
+
+# Save current state of sampler (e.g. current point, current scale etc.)
+sampler.save_checkpoint('demo36_sampler_checkpoint.pickle')
+
+# Then using a new sampler can load checkpoint
+sampler2 = MHNew(target)
+sampler2.load_checkpoint('demo36_sampler_checkpoint.pickle')
+
+print(sampler.scale) # Should be the same
+print(sampler2.scale) # Should be the same
+
+# %%
+# Batch sampling
+#
+# The new sampler supports batch sampling. This is useful for saving samples to disk in chunks
+# while sampling. For example:
+
+sampler.sample(500, batch_size=100, sample_path='demo36_sampler_samples/')
+
+# This stored samples in chunks of 100 in the folder demo36_sampler_samples
 
 
 # %%
