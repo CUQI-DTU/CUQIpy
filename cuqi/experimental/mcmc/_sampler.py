62a9d43f89cfdcf2d1693920e4f9b6489aced620
amal-ghamdi <amal.m.alghamdi@gmail.com>
2024-03-09 09:03:25 +0100

    add old scale

--- cuqi/experimental/mcmc/_sampler.py
+++ cuqi/experimental/mcmc/_sampler.py
@@ -238,6 +238,7 @@ class ProposalBasedSamplerNew(SamplerNew, ABC):
         self.current_target = self.target.logd(self.current_point)
         self.proposal = proposal
         self.scale = scale
+        self._scale_old = scale
 
         self._acc = [ 1 ] # TODO. Check
 
