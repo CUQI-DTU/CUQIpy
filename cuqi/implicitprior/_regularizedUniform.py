830eb12258fff43dd67facb3a2dea506ffe7a21d
Jasper Everink <jmev@dtu.dk>
2024-04-24 14:16:13 +0200

    Added "Regularized Uniform" distribution
    The Regularized Uniform distribution is currently modelled as a Regularized Gaussian with mean zero and sqrtprec zero.

---
+++ cuqi/implicitprior/_regularizedUniform.py
@@ -0,0 +1,21 @@
+from cuqi.implicitprior import RegularizedGaussian
+from cuqi.distribution import Distribution, Gaussian
+
+import numpy as np
+
+
+class RegularizedUniform(RegularizedGaussian):
+        
+    def __init__(self, proximal = None, projector = None, constraint = None, regularization = None, geometry = None, **kwargs):
+        
+            args = {"lower_bound" : kwargs.pop("lower_bound", None),
+                    "upper_bound" : kwargs.pop("upper_bound", None),
+                    "strength" : kwargs.pop("strength", None)}
+            
+            # Underlying explicit GMRF
+            self._gaussian = Gaussian(mean = np.zeros(geometry.par_dim), sqrtprec = np.zeros((geometry.par_dim,geometry.par_dim)), **kwargs)
+            
+            # Init from abstract distribution class
+            super(Distribution, self).__init__(**kwargs)
+
+            self._parse_regularization_input_arguments(proximal, projector, constraint, regularization, args)
