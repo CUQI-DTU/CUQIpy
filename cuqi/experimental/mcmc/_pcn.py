68e5e8ae0c5284cd04ac6d64921494ffcbb36d1c
Chao <chaz@dtu.dk>
2024-05-12 22:13:45 +0200

    add lambd to state keys

--- cuqi/experimental/mcmc/_pcn.py
+++ cuqi/experimental/mcmc/_pcn.py
@@ -5,7 +5,7 @@ from cuqi.array import CUQIarray
 
 class PCNNew(SamplerNew):  # Refactor to Proposal-based sampler?
 
-    _STATE_KEYS = SamplerNew._STATE_KEYS.union({'scale', 'current_likelihood_logd'})
+    _STATE_KEYS = SamplerNew._STATE_KEYS.union({'scale', 'current_likelihood_logd', 'lambd'})
 
     def __init__(self, target, scale=1.0, **kwargs):
 
