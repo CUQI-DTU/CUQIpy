93e712917f32a5b8a093edc15e2dfaa096d86020
Jasper Everink <jmev@dtu.dk>
2024-06-11 10:45:47 +0200

    Fix tests
    Updated some tests with new guarding statements

--- tests/test_implicit_priors.py
+++ tests/test_implicit_priors.py
@@ -5,26 +5,30 @@ import pytest
 def test_RegularizedGaussian_default_init():
     """ Test that the implicit regularized Gaussian requires at least 1 regularization argument """
 
-    with pytest.raises(ValueError, match="Precisely one of "):
+    with pytest.raises(ValueError, match="At least some "):
         x = cuqi.implicitprior.RegularizedGaussian(np.zeros(5), 1)
 
 def test_RegularizedGaussian_guarding_statements():
     """ Test that we catch incorrect initialization of RegularizedGaussian """
 
     # More than 1 argument
-    with pytest.raises(ValueError, match="Precisely one of "):
+    with pytest.raises(ValueError, match="User-defined proximals"):
         cuqi.implicitprior.RegularizedGaussian(np.zeros(5), 1, proximal=lambda s,z: s, constraint="nonnegativity")
 
     # Proximal
+    """ #TODO: This guarding statement no longer exists due to allowing for lists
     with pytest.raises(ValueError, match="Proximal needs to be callable"):
         cuqi.implicitprior.RegularizedGaussian(np.zeros(5), 1, proximal=1)
+    """
 
     with pytest.raises(ValueError, match="Proximal should take 2 arguments"):
         cuqi.implicitprior.RegularizedGaussian(np.zeros(5), 1, proximal=lambda s: s)
 
     # Projector
+    """ #TODO: This guarding statement no longer exists due to allowing for lists
     with pytest.raises(ValueError, match="Projector needs to be callable"):
         cuqi.implicitprior.RegularizedGaussian(np.zeros(5), 1, projector=1)
-
+    """
+    
     with pytest.raises(ValueError, match="Projector should take 1 argument"):
         cuqi.implicitprior.RegularizedGaussian(np.zeros(5), 1, projector=lambda s,z: s)
