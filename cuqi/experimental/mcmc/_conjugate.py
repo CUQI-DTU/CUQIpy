7ccf5bb2c4f29d0997be837da3d505e722b0f70b
nabr <nabr@dtu.dk>
2024-06-23 10:22:55 +0200

    Add new validation on conjugate sampler to check parameter types

--- cuqi/experimental/mcmc/_conjugate.py
+++ cuqi/experimental/mcmc/_conjugate.py
@@ -1,7 +1,10 @@
 import numpy as np
+from abc import ABC, abstractmethod
+import math
 from cuqi.experimental.mcmc import SamplerNew
 from cuqi.distribution import Posterior, Gaussian, Gamma, GMRF
 from cuqi.implicitprior import RegularizedGaussian, RegularizedGMRF
+from cuqi.utilities import get_non_default_args
 
 class ConjugateNew(SamplerNew):
     """ Conjugate sampler
@@ -25,53 +28,163 @@ class ConjugateNew(SamplerNew):
     [1] Everink, Jasper M., Yiqiu Dong, and Martin S. Andersen. "Bayesian inference with projected densities." SIAM/ASA Journal on Uncertainty Quantification 11.3 (2023): 1025-1043.
 
     """
+
     def _initialize(self):
         pass
 
-    def validate_target(self):
+    @SamplerNew.target.setter # Overwrite the target setter to set the conjugate pair
+    def target(self, value):
+        """ Set the target density. Runs validation of the target. """
+        self._target = value
+        if self._target is not None:
+            self._set_conjugatepair()
+            self.validate_target()
 
+    def validate_target(self):
         if not isinstance(self.target, Posterior):
             raise TypeError("Conjugate sampler requires a target of type Posterior")
-
-        if not isinstance(self.target.likelihood.distribution, (Gaussian, GMRF, RegularizedGaussian, RegularizedGMRF)):
-            raise ValueError("Conjugate sampler only works with a Gaussian-type likelihood function")
+        self._conjugatepair.validate_target()
         
-        if not isinstance(self.target.prior, Gamma):
-            raise ValueError("Conjugate sampler only works with Gamma prior")
-        
-        if not self.target.prior.dim == 1:
-            raise ValueError("Conjugate sampler only works with univariate Gamma prior")
-        
-        if isinstance(self.target.likelihood.distribution, (RegularizedGaussian, RegularizedGMRF)) and self.target.likelihood.distribution.preset not in ["nonnegativity"]:
-            raise ValueError("Conjugate sampler only works with implicit regularized Gaussian likelihood with nonnegativity constraints")
-
     def step(self):
-        # Extract variables
-        b = self.target.likelihood.data                                 #mu
-        m = self._calc_m_for_Gaussians(b)                               #n
-        Ax = self.target.likelihood.distribution.mean                   #x_i
-        L = self.target.likelihood.distribution(np.array([1])).sqrtprec #L
-        alpha = self.target.prior.shape                                 #alpha
-        beta = self.target.prior.rate                                   #beta
-
-        # Create Gamma distribution and sample
-        dist = Gamma(shape=m/2+alpha,rate=.5*np.linalg.norm(L@(Ax-b))**2+beta)
-
-        self.current_point = dist.sample()
+        self.current_point = self._conjugatepair.sample()
 
     def tune(self, skip_len, update_count):
+        pass # No tuning required for conjugate sampler
+
+    def _set_conjugatepair(self):
+        """ Set the conjugate pair based on the likelihood and prior. This requires target to be set. """
+        if isinstance(self.target.likelihood.distribution, (Gaussian, GMRF)) and isinstance(self.target.prior, Gamma):
+            self._conjugatepair = _GaussianGammaPair(self.target)
+        elif isinstance(self.target.likelihood.distribution, (RegularizedGaussian, RegularizedGMRF)) and isinstance(self.target.prior, Gamma):
+            self._conjugatepair = _RegularizedGaussianGammaPair(self.target)
+        else:
+            raise ValueError(f"No conjugate pair defined for likelihood {type(self.target.likelihood.distribution)} and prior {type(self.target.prior)}")
+
+    def __repr__(self):
+        msg = super().__repr__()
+        if hasattr(self, "_conjugatepair"):
+            msg += f"\n Conjugate pair:\n\t {type(self._conjugatepair).__name__.removeprefix('_')}"
+        return msg
+        
+class _ConjugatePair(ABC):
+    """ Abstract base class for conjugate pairs used in the Conjugate sampler. """
+
+    def __init__(self, target):
+        self.target = target
+
+    @abstractmethod
+    def validate_target(self):
+        """ Validate the target distribution for the conjugate pair. """
         pass
 
-    def _calc_m_for_Gaussians(self, b):
-        """ Helper method to calculate m parameter for Gaussian-Gamma conjugate pair.
-         
-        Classically m defines the number of observations in the Gaussian likelihood function.
+    @abstractmethod
+    def sample(self):
+        """ Sample from the conjugate distribution. """
+        pass
 
-        However, for implicit regularized Gaussians, m is the number of non-zero elements in the data vector b see [1].
-           
-        """
 
-        if isinstance(self.target.likelihood.distribution, (Gaussian, GMRF)):
-            return len(b)
-        elif isinstance(self.target.likelihood.distribution, (RegularizedGaussian, RegularizedGMRF)):
-            return np.count_nonzero(b)
+class _GaussianGammaPair(_ConjugatePair):
+    """ Implementation for the Gaussian-Gamma conjugate pair."""
+
+    def validate_target(self):
+        if not isinstance(self.target.likelihood.distribution, Gaussian):
+            raise ValueError("Conjugate sampler only works with a Gaussian likelihood function")
+
+        if not isinstance(self.target.prior, Gamma):
+            raise ValueError("Conjugate sampler only works with Gamma prior")
+
+        if self.target.prior.dim != 1:
+            raise ValueError("Conjugate sampler only works with univariate Gamma prior")
+
+        key, value = _get_conjugate_parameter(self.target)
+        if key == "cov":
+            assert _check_conjugate_parameter_is_scalar_reciprocal(value), \
+                "Gaussian-Gamma conjugate pair defined via covariance requires cov: lambda x : 1.0/x for the conjugate parameter"
+        elif key == "prec":
+            assert _check_conjugate_paramter_is_scalar_identity(value), \
+                "Gaussian-Gamma conjugate pair defined via precision requires prec: lambda x : x for the conjugate parameter"
+        else:
+            raise ValueError("Conjugate sampler only works with Gaussian likelihood functions where conjugate parameter is defined via covariance or precision")
+
+    def sample(self):
+        # Extract variables
+        b = self.target.likelihood.data                                 # mu
+        m = len(b)                                                      # n
+        Ax = self.target.likelihood.distribution.mean                   # x_i
+        L = self.target.likelihood.distribution(np.array([1])).sqrtprec # L
+        alpha = self.target.prior.shape                                 # alpha
+        beta = self.target.prior.rate                                   # beta
+
+        # Create Gamma distribution and sample
+        dist = Gamma(shape=m/2 + alpha, rate=.5 * np.linalg.norm(L @ (Ax - b))**2 + beta)
+
+        return dist.sample()
+
+
+class _RegularizedGaussianGammaPair(_ConjugatePair):
+    """Implementation for the Regularized Gaussian-Gamma conjugate pair."""
+
+    def validate_target(self):
+        if not isinstance(self.target.likelihood.distribution, RegularizedGaussian):
+            raise ValueError("Conjugate sampler only works with a Regularized Gaussian likelihood function")
+
+        if not isinstance(self.target.prior, Gamma):
+            raise ValueError("Conjugate sampler only works with Gamma prior")
+
+        if self.target.prior.dim != 1:
+            raise ValueError("Conjugate sampler only works with univariate Gamma prior")
+
+        if self.target.likelihood.distribution.preset not in ["nonnegativity"]:
+            raise ValueError("Conjugate sampler only works with implicit regularized Gaussian likelihood with nonnegativity constraints")
+
+        key, value = _get_conjugate_parameter(self.target)
+        if key == "cov":
+            assert _check_conjugate_parameter_is_scalar_reciprocal(value), \
+                "Regularized Gaussian-Gamma conjugate pair defined via covariance requires cov: lambda x : 1.0/x for the conjugate parameter"
+        elif key == "prec":
+            assert _check_conjugate_paramter_is_scalar_identity(value), \
+                "Regularized Gaussian-Gamma conjugate pair defined via precision requires prec: lambda x : x for the conjugate parameter"
+        else:
+            raise ValueError("Conjugate sampler only works with Regularized Gaussian likelihood functions where conjugate parameter is defined via covariance or precision")
+
+    def sample(self):
+        # Extract variables
+        b = self.target.likelihood.data                                 # mu
+        m = np.count_nonzero(b)                                         # n
+        Ax = self.target.likelihood.distribution.mean                   # x_i
+        L = self.target.likelihood.distribution(np.array([1])).sqrtprec # L
+        alpha = self.target.prior.shape                                 # alpha
+        beta = self.target.prior.rate                                   # beta
+
+        # Create Gamma distribution and sample
+        dist = Gamma(shape=m/2 + alpha, rate=.5 * np.linalg.norm(L @ (Ax - b))**2 + beta)
+
+        return dist.sample()
+
+def _get_conjugate_parameter(target):
+    """Extract the parameter name, attribute and variable location of the conjugate parameter."""
+    par_name = target.prior.name
+    mutable_likelihood_vars  = target.likelihood.distribution.get_mutable_variables()
+
+    for var_key in mutable_likelihood_vars:
+        attr = getattr(target.likelihood.distribution, var_key)
+        if callable(attr) and par_name in get_non_default_args(attr):
+            return var_key, attr
+    raise ValueError(f"Unable to find conjugate parameter {par_name} in likelihood function for conjugate sampler with target {target}")
+
+def _check_conjugate_paramter_is_scalar_identity(f):
+    """Tests whether a function (scalar to scalar) is the identity (lambda x : x)."""
+    return all(f(x) == x for x in [1.0, 10.0, 100.0])
+
+def _regression_test_linear(f):
+    """Tests whether a function (scalar to scalar or array to array) is linear."""
+    reference = f(1.0)
+    if isinstance(reference, np.ndarray):
+        reference = reference.flatten()[0]
+        return all(math.isclose(f(x).flatten()[0], x * reference) for x in [10.0, 100.0])
+    else:
+        return all(math.isclose(f(x), x * reference) for x in [10.0, 100.0])
+
+def _check_conjugate_parameter_is_scalar_reciprocal(f):
+    """Tests whether a function (scalar to scalar) is the reciprocal (lambda x : 1.0/x)."""
+    return all(math.isclose(f(x), 1.0 / x) for x in [1.0, 10.0, 100.0])
