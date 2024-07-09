77eac5fbad03b28db23a72ca9d51bd8dda3c84f3
remilaumont <remi@pop-os.localdomain>
2024-07-09 16:08:08 +0200

    Update of the init with MoreauYoshidaPrior and RestorationPrior

--- cuqi/implicitprior/__init__.py
+++ cuqi/implicitprior/__init__.py
@@ -1,3 +1,3 @@
 from ._regularizedGaussian import RegularizedGaussian
 from ._regularizedGMRF import RegularizedGMRF
-from ._denoiser import DenoiseRegularizer
+from ._restorator import DenoiseRegularizer, RestorationPrior, MoreauYoshidaPrior
