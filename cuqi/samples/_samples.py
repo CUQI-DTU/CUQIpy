2e5b7c56d04250a71d0b3c8bdc9312b2d18107b3
Chao <chaz@dtu.dk>
2024-08-12 18:11:22 +0200

    Capticalize S in Samples

--- cuqi/samples/_samples.py
+++ cuqi/samples/_samples.py
@@ -875,4 +875,4 @@ class Samples(object):
         return ax
 
     def __repr__(self) -> str:
-        return f"A CUQIpy samples object containging {self.Ns} samples, each of length {self.geometry.par_dim}."
+        return f"A CUQIpy Samples object containging {self.Ns} samples, each of length {self.geometry.par_dim}."
