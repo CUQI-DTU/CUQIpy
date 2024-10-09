86f7ad91d2b37101ce568a223c0f299974511ee0
Chao <chaz@dtu.dk>
2024-10-09 23:44:53 +0200

    relocate unit tests on truncated normal

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -16,6 +16,26 @@ def test_Normal_pdf_mean():
     pX = cuqi.distribution.Normal(0.1,1)
     assert pX.pdf(0.1) == approx(1.0/np.sqrt(2.0*np.pi))
 
+@pytest.mark.parametrize("mean,var,expected",[
+                            (2,3.5,[[8.17418321], [3.40055023]]),
+                            (3.141592653589793,2.6457513110645907,[[7.80883646],[4.20030911]]),
+                            (-1e-09, 1000000.0,[[1764052.34596766],[400157.20836722]]),
+                            (1.7724538509055159, 0, [[1.77245385],[1.77245385]])
+                        ])
+def test_Normal_sample_regression(mean,var,expected):
+    rng = np.random.RandomState(0) #Replaces legacy method: np.random.seed(0)
+    samples = cuqi.distribution.Normal(mean,var).sample(2,rng=rng)
+    target = np.array(expected).T
+    assert np.allclose( samples.samples, target)
+
+@pytest.mark.parametrize("mean,std,points,expected",[
+                            (0,1,[-1,0,1],[1,0,-1]),
+   (np.array([0,0]),np.array([1,1]),[[-1,0],[0,0],[0,-1]], [[1,0],[0,0],[0,1]])])
+def test_Normal_gradient(mean,std,points,expected):
+    p = cuqi.distribution.Normal(mean,std)
+    for point, grad in zip(points, expected):
+        assert np.allclose(p.gradient(point), grad)
+
 @pytest.mark.parametrize("mean,std,low,high,points",[(0.0, 
                                                1.0,
                                                -1.0,
@@ -74,26 +94,6 @@ def test_TruncatedNormal_sampling(mean,std,low,high):
     for i in range(samples.shape[1]):
         sample = samples[:,i]
         assert np.all(sample >= low) and np.all(sample <= high)
-
-@pytest.mark.parametrize("mean,var,expected",[
-                            (2,3.5,[[8.17418321], [3.40055023]]),
-                            (3.141592653589793,2.6457513110645907,[[7.80883646],[4.20030911]]),
-                            (-1e-09, 1000000.0,[[1764052.34596766],[400157.20836722]]),
-                            (1.7724538509055159, 0, [[1.77245385],[1.77245385]])
-                        ])
-def test_Normal_sample_regression(mean,var,expected):
-    rng = np.random.RandomState(0) #Replaces legacy method: np.random.seed(0)
-    samples = cuqi.distribution.Normal(mean,var).sample(2,rng=rng)
-    target = np.array(expected).T
-    assert np.allclose( samples.samples, target)
-
-@pytest.mark.parametrize("mean,std,points,expected",[
-                            (0,1,[-1,0,1],[1,0,-1]),
-   (np.array([0,0]),np.array([1,1]),[[-1,0],[0,0],[0,-1]], [[1,0],[0,0],[0,1]])])
-def test_Normal_gradient(mean,std,points,expected):
-    p = cuqi.distribution.Normal(mean,std)
-    for point, grad in zip(points, expected):
-        assert np.allclose(p.gradient(point), grad)
 
 def test_Gaussian_mean():
     mean = np.array([0, 0])
