8a106f83b9506d5d467d52e416a0e2c6a0c69d5f
nabr <nabr@dtu.dk>
2024-10-10 23:25:56 +0200

    update try script on affine model

--- demos/try17_AffineModel.py
+++ demos/try17_AffineModel.py
@@ -9,106 +9,117 @@ from cuqi.distribution import Gaussian, LMRF, GMRF, JointDistribution
 from cuqi.geometry import Discrete, Continuous1D
 from cuqi.array import CUQIarray
 
-#%% Test problem 
+# %% Test problem
 ### y = Ax + b
 ### Infer x, given the model A and b, and noisy observations of y
 
 # Dimensions
 n = 2
 m = 5
+
 # Unknown parameter
-x_truevec = np.array([3,2])
-x_true = CUQIarray(x_truevec, geometry=Discrete(['x1', 'x2']))
+x_truevec = np.array([3, 2])
+x_true = CUQIarray(x_truevec, geometry=Discrete(["x1", "x2"]))
+
 # Observation times
-t = np.linspace(0,m,m, endpoint=False)
+t = np.linspace(0, m, m, endpoint=False)
+
 # Forward model
-Amat = np.ones((m,n))
-Amat[:,1] = t
-b = np.ones(m)*3
-# Geometries 
+Amat = np.ones((m, n))
+Amat[:, 1] = t
+
+# shift/bias
+b = np.ones(m) * 3
+
+# Geometries
 domain_geometry = Discrete(n)
 range_geometry = Continuous1D(t)
 
 # Prior
-x_mean = np.array([2., 1.])
+x_mean = np.array([2.0, 1.0])
 x_std = np.ones(n)
-x = Gaussian(x_mean, sqrtcov = x_std, geometry = domain_geometry)
+x = Gaussian(x_mean, sqrtcov=x_std, geometry=domain_geometry)
 
 # Noisy data
 np.random.seed(10)
 y_std = np.ones(m)
-y_obs = Gaussian(Amat@x_truevec + b, sqrtcov=y_std, geometry = range_geometry).sample()
+y_obs = Gaussian(Amat @ x_truevec + b, sqrtcov=y_std, geometry=range_geometry).sample()
 
-#%% Linear model, include b in data term such that data = y-b
+# %% Linear model, include bias (b) from model in data term such that data = y-b
+
 # CUQI Model
-Alinear = LinearModel(Amat, domain_geometry = domain_geometry, range_geometry = range_geometry)
+A = LinearModel(Amat, domain_geometry=domain_geometry, range_geometry=range_geometry)
 
 # Likelihood
-y = Gaussian(Alinear(x), sqrtcov=y_std, geometry = range_geometry)
+y = Gaussian(A @ x, sqrtcov=y_std, geometry=range_geometry)
 
 # Posterior
-posterior = JointDistribution(x, y)(y=y_obs-b)
+posterior = JointDistribution(x, y)(y=y_obs - b)
 
 # Sample posterior with linear RTO
 np.random.seed(1000000)
-Linearsampler = MH(posterior, initial_point = x_mean) # Init point must be float (NOT AFFINE MODEL SPECIFIC)
+Linearsampler = MH(posterior, initial_point=x_mean)  # Init point must be float (NOT AFFINE MODEL SPECIFIC)
 Linearsampler.warmup(200)
 Linearsampler.sample(10000)
 samplesLinear = Linearsampler.get_samples()
 samplesLinear_burnin = samplesLinear.burnthin(200)
 
+# Compute posterior mean and variance and plot
 meanLinear = samplesLinear_burnin.mean()
 varLinear = samplesLinear_burnin.variance()
 
-print('Posterior mean and variance from Linear model: ')
+print("Posterior mean and variance using Linear model: ")
 print("mean: {}".format(meanLinear))
 print("cov: {}".format(varLinear))
 
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5,4))
-ax.plot(t, Alinear(samplesLinear_burnin).mean(), linestyle = '--', label = "Linear model")
-ax.plot(t, y_obs-b, 'o', label="Noisy data")
-ax.set_xlabel('t')
-ax.set_ylabel('y')
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5, 4))
+ax.plot(t, A(samplesLinear_burnin).mean(), linestyle="--", label="Sample mean using Linear model")
+ax.plot(t, y_obs - b, "o", label="Noisy data")
+ax.set_xlabel("t")
+ax.set_ylabel("y")
 plt.legend()
 
-fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(5,4), squeeze=False)
-cs = samplesLinear_burnin.plot_pair(marginals = True)
+cs = samplesLinear_burnin.plot_pair(marginals=True)
+plt.suptitle("Posterior samples using Linear model")
 
 
-#%% Affine model, include b in affine model such that data = y
+# %% Affine model, include bias (b) in affine model such that data = y
+
 # CUQI Model
-Aaffine = AffineModel(Amat, b, domain_geometry = domain_geometry, range_geometry = range_geometry)
+affine_model = AffineModel(Amat, b, domain_geometry=domain_geometry, range_geometry=range_geometry)
 
 # Likelihood
-y = Gaussian(Aaffine(x), sqrtcov=y_std, geometry = range_geometry)
+y = Gaussian(affine_model(x), sqrtcov=y_std, geometry=range_geometry)
 
 # Posterior
 posterior = JointDistribution(x, y)(y=y_obs)
 
-print(posterior.likelihood.model.forward(np.array([1,1])))
-print(posterior.likelihood.model._forward_func_no_shift(np.array([1,1])))
+print(posterior.likelihood.model.forward(np.array([1, 1])))
+print(posterior.likelihood.model._forward_func_no_shift(np.array([1, 1])))
 
 # Sample posterior with MH
 np.random.seed(1000000)
-Affinesampler = MH(posterior, initial_point = x_mean) # Init point must be float (NOT AFFINE MODEL SPECIFIC)
+Affinesampler = MH(posterior, initial_point=x_mean)  # Init point must be float (NOT AFFINE MODEL SPECIFIC)
 Affinesampler.warmup(200)
 Affinesampler.sample(10000)
 samplesAffine = Affinesampler.get_samples()
 samplesAffine_burnin = samplesAffine.burnthin(200)
 
+# Compute posterior mean and variance and plot
 meanAffine = samplesAffine_burnin.mean()
 varAffine = samplesAffine_burnin.variance()
 
-print('Posterior mean and variance from Affine model: ')
+print("Posterior mean and variance from Affine model: ")
 print("mean: {}".format(meanAffine))
 print("cov: {}".format(varAffine))
 
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5,4))
-ax.plot(t, Aaffine(samplesAffine_burnin).mean(), linestyle = '--', label = "Mean-predicted data")
-ax.plot(t, y_obs, 'o', label="Noisy data")
-ax.set_xlabel('t')
-ax.set_ylabel('y')
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5, 4))
+ax.plot(t, affine_model(samplesAffine_burnin).mean(), linestyle="--", label="Sample mean using Affine model")
+ax.plot(t, y_obs, "o", label="Noisy data")
+ax.set_xlabel("t")
+ax.set_ylabel("y")
 plt.legend()
 
-fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(5,4), squeeze=False)
-cs = samplesAffine_burnin.plot_pair(marginals = True)
+
+cs = samplesAffine_burnin.plot_pair(marginals=True)
+plt.suptitle("Posterior samples using Affine model")
