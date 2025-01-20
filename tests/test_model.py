b41f229bb5edd9b4d1ed35ebbad01ca10c5df223
amal-ghamdi <amal.m.alghamdi@gmail.com>
2025-01-20 07:04:10 +0100

    update test

--- tests/test_model.py
+++ tests/test_model.py
@@ -78,26 +78,43 @@ def test_forward(x, expected_type):
     fwd = model.forward(x)
     assert(isinstance(fwd, expected_type))
 
-@pytest.mark.parametrize("x, y, z, expected_type",
-                         [(np.array([1, 3, 4]),
-                            np.array([1, 3]),
-                            np.array([1, 3, 4]),
-                           np.ndarray)])
-def test_forward_with_multiple_inputs(x, y, z, expected_type):
-    """For different types of input to the model forward method, assert we are obtaining the correct output type"""
 
-    def forward(x, y, z):
-        return x*y[0] + z*y[1]
-    model = cuqi.model.Model(forward=forward,
-                            domain_geometry=
-                            cuqi.experimental.geometry._ProductGeometry(
-                            cuqi.geometry.Continuous1D(3),
-                            cuqi.geometry.Continuous1D(2),
-                            cuqi.geometry.Continuous1D(3)),
-                            range_geometry=cuqi.geometry.Continuous1D(3))
+@pytest.mark.parametrize(
+    "x, y, z, forward, domain_geometry, expected_value, expected_type",
+    [
+        (
+            np.array([1, 3, 4]),
+            np.array([1, 3]),
+            3,
+            lambda x, y, z: x * y[0] + z * y[1],
+            cuqi.experimental.geometry._ProductGeometry(
+                cuqi.geometry.Continuous1D(3),
+                cuqi.geometry.Continuous1D(2),
+                cuqi.geometry.Continuous1D(3),
+            ),
+            np.array([10, 12, 13]),
+            np.ndarray,
+        )
+    ],
+)
+def test_forward_with_multiple_inputs(
+    x, y, z, forward, domain_geometry, expected_value, expected_type
+):
+    """Test that the forward method can handle multiple inputs and
+    return the correct output type"""
+
+    model = cuqi.model.Model(
+        forward=forward,
+        domain_geometry=domain_geometry,
+        range_geometry=cuqi.geometry.Continuous1D(3),
+    )
 
     fwd = model.forward(x, y, z)
-    assert(isinstance(fwd, expected_type))
+    fwd2 = model(y=y, x=x, z=z)
+    assert isinstance(fwd, expected_type)
+    assert np.allclose(fwd, expected_value)
+    assert np.allclose(fwd, fwd2)
+
 
 @pytest.mark.parametrize("x, expected_type",
                          [(np.array([1, 3]),
@@ -533,5 +550,3 @@ def test_AffineModel_update_shift():
     # check model output of copied model with updated shift
     model_copy.shift = new_shift
     assert np.all(model_copy(x) == np.array([3,0]))
-
-
