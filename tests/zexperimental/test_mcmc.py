517db7beb352bdca3ef277493732a851c520bdba
Chao <chaz@dtu.dk>
2025-01-22 19:31:35 +0100

    Add HybridGibbs

--- tests/zexperimental/test_mcmc.py
+++ tests/zexperimental/test_mcmc.py
@@ -1321,6 +1321,67 @@ def test_RandomVariable_prior_against_Distribution_prior_regularized_RTO():
 
     assert np.allclose(samples_rv.samples, samples_dist.samples)
 
+def test_RandomVariable_prior_against_Distribution_prior_regularized_RTO_HybridGibbs():
+
+    # Forward problem
+    A, y_data, info = cuqi.testproblem.Deconvolution1D(dim=28, phantom='square', noise_std=0.001).get_components()
+
+    # Random seed
+    np.random.seed(0)
+
+    # Bayesian Inverse Problem
+    d = cuqi.distribution.Gamma(1, 1e-4)
+    s = cuqi.distribution.Gamma(1, 1e-4)
+    x = cuqi.distribution.LMRF(0, lambda d: 1/d, geometry=A.domain_geometry)
+    y = cuqi.distribution.Gaussian(A@x, lambda s: 1/s)
+
+    # Posterior
+    target = cuqi.distribution.JointDistribution(y, x, s, d)(y=y_data)
+
+    # Sampling strategy
+    sampling_strategy = {
+        "x" : cuqi.experimental.mcmc.UGLA(),
+        "s" : cuqi.experimental.mcmc.Conjugate(),
+        "d" : cuqi.experimental.mcmc.ConjugateApprox()
+    }
+
+    # Gibbs sampler
+    sampler = cuqi.experimental.mcmc.HybridGibbs(target, sampling_strategy)
+
+    # Run sampler
+    sampler.warmup(50)
+    sampler.sample(200)
+    samples = sampler.get_samples()
+
+    # Random seed
+    np.random.seed(0)
+
+    # Bayesian Inverse Problem
+    d_rv = cuqi.distribution.Gamma(1, 1e-4).rv
+    s_rv = cuqi.distribution.Gamma(1, 1e-4).rv
+    x_rv = cuqi.distribution.LMRF(0, lambda d_rv: 1/d_rv, geometry=A.domain_geometry).rv
+    y_rv = cuqi.distribution.Gaussian(A@x_rv, lambda s_rv: 1/s_rv).rv
+
+    # Posterior
+    target_rv = cuqi.distribution.JointDistribution(y_rv, x_rv, s_rv, d_rv)(y_rv=y_data)
+
+    # Sampling strategy
+    sampling_strategy_rv = {
+        "x_rv" : cuqi.experimental.mcmc.UGLA(),
+        "s_rv" : cuqi.experimental.mcmc.Conjugate(),
+        "d_rv" : cuqi.experimental.mcmc.ConjugateApprox()
+    }
+
+    # Gibbs sampler
+    sampler_rv = cuqi.experimental.mcmc.HybridGibbs(target_rv, sampling_strategy_rv)
+
+    # Run sampler
+    sampler_rv.warmup(50)
+    sampler_rv.sample(200)
+    samples_rv = sampler_rv.get_samples()
+
+    assert np.allclose(samples['x'].samples, samples_rv['x_rv'].samples)
+
 def Conjugate_GaussianGammaPair():
     """ Unit test whether Conjugacy Pair (Gaussian, Gamma) constructs the right distribution """
     x = cuqi.distribution.Gamma(1.0, 2.0)
