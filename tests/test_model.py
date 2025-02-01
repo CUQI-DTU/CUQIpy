1fb2a003bac0283c233c8dd17e32d05338a950df
amal-ghamdi <amal.m.alghamdi@gmail.com>
2025-02-01 11:45:49 +0100

    undo commit

--- tests/test_model.py
+++ tests/test_model.py
@@ -1607,7 +1607,7 @@ def test_model_updates_parameters_names_if_distributions_are_passed_with_new_par
         lambda par: model.forward(par[:2], par[-1]),
         np.hstack([input1, np.array([input2])]),
         np.array([direction]))
-    assert np.allclose(model_grad_v1[0]+0.1, grad_FD[:2])
+    assert np.allclose(model_grad_v1[0], grad_FD[:2])
     assert np.allclose(model_grad_v1[1], grad_FD[-1])
 
 
