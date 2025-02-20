106cc991ad6a2d9ff9989cbe9177c6deb00bf4df
Jasper Everink <jmev@dtu.dk>
2025-02-20 10:09:17 +0100

    Find valid sampling strategies for HybridGibbs

--- cuqi/experimental/mcmc/_utilities.py
+++ cuqi/experimental/mcmc/_utilities.py
@@ -1,5 +1,6 @@
 import cuqi
 import inspect
+import numpy as np
 
 def find_valid_samplers(target):
     """ Finds all samplers in the cuqi.experimental.mcmc module that accept the provided target. """
@@ -13,5 +14,34 @@ def find_valid_samplers(target):
             valid_samplers += [name]
         except:
             pass
+
+    # Need a separate case for HybridGibbs
+    if find_valid_sampling_strategy(target) is not None:
+        valid_samplers += [cuqi.experimental.mcmc.HybridGibbs.__name__]
+
+    return valid_samplers
+
+def find_valid_sampling_strategy(target):
+    """
+        Find valid samplers to be used for creating a sampling strategy for the HybridGibbs sampler
+    """
+
+    if not isinstance(target, cuqi.distribution.JointDistribution):
+        return None
+
+    par_names = target.get_parameter_names()
+    print(par_names)
+
+    valid_samplers = dict()
+    for par_name in par_names:
+        # TODO: Compute conditional distribution
+        conditional_params = {par_name_: np.ones(target.dim[i]) for i, par_name_ in enumerate(par_names) if par_name_ != par_name}
+        conditional = target(**conditional_params)
+
+        samplers = find_valid_samplers(conditional)
+        if len(samplers) == 0:
+            return None
+        
+        valid_samplers[par_name] = samplers
 
     return valid_samplers
