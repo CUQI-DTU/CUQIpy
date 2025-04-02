f6c920f4ac23fe0cb023cc4c6cd7bee5c5f1f476
Chao <chaz@dtu.dk>
2025-04-02 10:22:42 +0200

    accept tol and maxiter in minimizer

--- cuqi/experimental/mcmc/_rto.py
+++ cuqi/experimental/mcmc/_rto.py
@@ -292,7 +292,7 @@ class RegularizedLinearRTO(LinearRTO):
                                      lsmr_tol = self.inner_abstol)
         elif self.solver == "ScipyMinimizer":
                 bounds = [(self.target.prior._box_bounds[0][i], self.target.prior._box_bounds[1][i]) for i in range(self.target.prior.dim)]
-                sim = ScipyMinimizer(lambda x: 0.5*sum((self.M(x, 1)-y)**2), self.current_point, gradfunc=lambda x: self.M(self.M(x, 1) - y, 2), bounds=bounds)
+                sim = ScipyMinimizer(lambda x: 0.5*sum((self.M(x, 1)-y)**2), self.current_point, gradfunc=lambda x: self.M(self.M(x, 1) - y, 2), bounds=bounds, tol=self.abstol, options={"maxiter": self.maxit})
         else:
             raise ValueError("Choice of solver not supported.")
 
