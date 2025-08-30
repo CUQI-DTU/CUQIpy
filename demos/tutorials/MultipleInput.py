update demo and correct the gradient

demos/tutorials/MultipleInput.py
@@ -1,10 +1,27 @@
-"""Bathtub demo"""
+# %%
+"""
+Bathtub demo
+==============
 
+This is a demo for a bathtub temperature and volume model using CUQIpy. 
+We have measurements of the temperature and volume of the water in the bathtub
+and want to infer the temperature and volume of hot water and cold water that
+were used to fill in the bathtub"""
+
+# %%
 # Import libraries
+# ----------------
+
 import cuqi
 import numpy as np
 
-# %% Define the forward map
+# %%
+# Define the forward map
+# --------------------------
+#
+# `h_v` is the volume of hot water, `h_t` is the temperature of hot water,
+# `c_v` is the volume of cold water, `c_t` is the temperature of cold water.
+
 def forward_map(h_v, h_t, c_v, c_t): 
     # volume
     volume = h_v + c_v
@@ -13,24 +30,40 @@ def forward_map(h_v, h_t, c_v, c_t):
 
     return np.array([volume, temp]).reshape(2,)
 
-# %% Define the gradients
-# TODO: fix the gradients and try MALA or NUTS
+# %%
 # Define the gradients
-def gradient_h_v(direction, h_v, h_t, c_v, c_t):
-    return direction[0] * 1 + h_t * direction[1]
-#
-#
-def gradient_h_t(direction, h_v, h_t, c_v, c_t):
-    return direction[0] * 0 + h_v * direction[1]
-#
-def gradient_c_v(direction, h_v, h_t, c_v, c_t):
-    return direction[0] * 1 + c_t * direction[1]
-#
-def gradient_c_t(direction, h_v, h_t, c_v, c_t):
-    return direction[0] * 0 + c_v * direction[1]
+# --------------------------
 
-# %% Domain geometry and range geometry
-# Define the forward model with gradients
+
+# Define the gradient with respect to h_v
+def gradient_h_v(direction, h_v, h_t, c_v, c_t):
+    return (
+        direction[0]
+        + (h_t / (h_v + c_v) - (h_v * h_t + c_v * c_t) / (h_v + c_v) ** 2)
+        * direction[1]
+    )
+
+# Define the gradient with respect to h_t
+def gradient_h_t(direction, h_v, h_t, c_v, c_t):
+    return (h_v / (h_v + c_v)) * direction[1]
+
+# Define the gradient with respect to c_v
+def gradient_c_v(direction, h_v, h_t, c_v, c_t):
+    return (
+        direction[0]
+        + (c_t / (h_v + c_v) - (h_v * h_t + c_v * c_t) / (h_v + c_v) ** 2)
+        * direction[1]
+    )
+
+# Define the gradient with respect to c_t
+def gradient_c_t(direction, h_v, h_t, c_v, c_t):
+    return (c_v / (h_v + c_v)) * direction[1]
+
+
+# %%
+# Define domain geometry and range geometry
+# ---------------------------------------------
+
 domain_geometry = (
     cuqi.geometry.Discrete(['h_v']),
     cuqi.geometry.Discrete(['h_t']),
@@ -40,7 +73,10 @@ domain_geometry = (
 
 range_geometry = cuqi.geometry.Discrete(['temperature','volume'])
 
-# %% Model
+# %%
+# Define the forward model object
+# --------------------------------------
+
 model = cuqi.model.Model(
     forward=forward_map,
     gradient=(gradient_h_v, gradient_h_t, gradient_c_v, gradient_c_t),
@@ -48,75 +84,117 @@ model = cuqi.model.Model(
     range_geometry=range_geometry
 )
 
-# %% Partial evaluation
+# %%
+# Experiment with partial evaluation of the model
+# ---------------------------------------------------
+
 print("\nmodel()\n", model())
-print("\nmodel(h_v = 50)\n", model(h_v = 50))
-print("\nmodel(h_v = 50, h_t = 60)\n", model(h_v = 50, h_t = 60))
-print("\nmodel(h_v = 50, h_t = 60, c_v = 30)\n", model(h_v = 50, h_t = 60, c_v = 30))
-print("\nmodel(h_v = 50, h_t = 60, c_v = 30, c_t = 10)\n", model(h_v = 50, h_t = 60, c_v = 30, c_t = 10))
+print("\nmodel(h_v = 50)\n", model(h_v=50))
+print("\nmodel(h_v = 50, h_t = 60)\n", model(h_v=50, h_t=60))
+print("\nmodel(h_v = 50, h_t = 60, c_v = 30)\n", model(h_v=50, h_t=60, c_v=30))
+print(
+    "\nmodel(h_v = 50, h_t = 60, c_v = 30, c_t = 10)\n",
+    model(h_v=50, h_t=60, c_v=30, c_t=10),
+)
 
 # %%
-h_v = cuqi.distribution.Uniform(0, 60, geometry=domain_geometry[0])
-h_t = cuqi.distribution.Uniform(40, 70, geometry=domain_geometry[1])
-c_v = cuqi.distribution.Uniform(0, 60, geometry= domain_geometry[2])
-c_t = cuqi.distribution.TruncatedNormal(10, 2**2, 7, 15, geometry=domain_geometry[3])  # Truncated normal distribution between 7 and 15
+# Define prior distributions for the unknown parameters
+# ------------------------------------------------------------
 
-# %% Data distribution and likelihood
+h_v_dist = cuqi.distribution.Uniform(0, 60, geometry=domain_geometry[0])
+h_t_dist = cuqi.distribution.Uniform(40, 70, geometry=domain_geometry[1])
+c_v_dist = cuqi.distribution.Uniform(0, 60, geometry=domain_geometry[2])
+c_t_dist = cuqi.distribution.TruncatedNormal(
+    10, 2**2, 7, 15, geometry=domain_geometry[3]
+)
+
+# %%
+# Define a data distribution
+# ----------------------------
+
 data_dist = cuqi.distribution.Gaussian(
-    mean=model(h_v, h_t, c_v, c_t),
+    mean=model(h_v_dist, h_t_dist, c_v_dist, c_t_dist),
     cov=np.array([[1**2, 0], [0, 0.5**2]])
 )
 
+# %%
+# Define a joint distribution of prior and data distributions
+# ---------------------------------------------------------------
 
-# %% Define the joint distribution
 joint_dist = cuqi.distribution.JointDistribution(
     data_dist,
-    h_v,
-    h_t,
-    c_v,
-    c_t
+    h_v_dist,
+    h_t_dist,
+    c_v_dist,
+    c_t_dist
 )
 
-# %% Define the posterior distribution
+# %%
+# Define the posterior distribution by setting the observed data
+# ------------------------------------------------------------------
+
 posterior = joint_dist(data_dist=np.array([60, 38]))
 
-print("posterior", posterior)
-print("\nposterior(h_v = 50)\n", posterior(h_v=50))
-print("\nposterior(h_v = 50, h_t = 60)\n", posterior(h_v=50, h_t=60))
-print("\nposterior(h_v = 50, h_t = 60, c_v = 30)\n", posterior(h_v=50, h_t=60, c_v=30))
+# %%
+# Experiment with conditioning the posterior distribution
+# ------------------------------------------------------------
 
-# %% Sample from the joint distribution
+print("posterior", posterior)
+print("\nposterior(h_v = 50)\n", posterior(h_v_dist=50))
+print("\nposterior(h_v = 50, h_t = 60)\n", posterior(h_v_dist=50, h_t_dist=60))
+print(
+    "\nposterior(h_v = 50, h_t = 60, c_v = 30)\n",
+    posterior(h_v_dist=50, h_t_dist=60, c_v_dist=30),
+)
+
+# %%
+# Sample from the joint distribution
+# ------------------------------------------------------------
+#
+# First define sampling strategy for Gibbs sampling
+
 sampling_strategy = {
-    'h_v': cuqi.experimental.mcmc.MH(scale=0.05, initial_point= np.array([30])),
-    'h_t': cuqi.experimental.mcmc.MH(scale=0.02, initial_point= np.array([50])),
-    'c_v': cuqi.experimental.mcmc.MH(scale=0.05, initial_point= np.array([30])),
-    'c_t': cuqi.experimental.mcmc.MH(scale=0.02, initial_point= np.array([10])),
+    "h_v_dist": cuqi.experimental.mcmc.MALA(
+        scale=0.2, initial_point=np.array([30])),
+    "h_t_dist": cuqi.experimental.mcmc.MALA(
+        scale=0.2, initial_point=np.array([50])),
+    "c_v_dist": cuqi.experimental.mcmc.MALA(
+        scale=0.2, initial_point=np.array([30])),
+    "c_t_dist": cuqi.experimental.mcmc.MALA(
+        scale=0.2, initial_point=np.array([10])),
 }
 
-# %% Sampler
+# %%
+# Then create the sampler and sample
+
 hybridGibbs = cuqi.experimental.mcmc.HybridGibbs(
     posterior,
     sampling_strategy=sampling_strategy)
 
-hybridGibbs.warmup(1000)
+hybridGibbs.warmup(100)
 hybridGibbs.sample(2000)
 samples = hybridGibbs.get_samples()
 
-# %% Plot the results
-mean_h_v = samples['h_v'].mean()
-mean_h_t = samples['h_t'].mean()
-mean_c_v = samples['c_v'].mean()
-mean_c_t = samples['c_t'].mean()
+# %%
+# Plot some results
+# ------------------
 
+# Compute mean values
+mean_h_v = samples['h_v_dist'].mean()
+mean_h_t = samples['h_t_dist'].mean()
+mean_c_v = samples['c_v_dist'].mean()
+mean_c_t = samples['c_t_dist'].mean()
+
+# Print mean values
 print(f"Mean h_v: {mean_h_v}, Mean h_t: {mean_h_t}, Mean c_v: {mean_c_v}, Mean c_t: {mean_c_t}")
-
 print("Measured volume:", 60)
 print("Mean predicted volume:", mean_h_v + mean_c_v)
 print()
 print("Measured temperature:", 38)
 print("Mean predicted temperature:", (mean_h_v * mean_h_t + mean_c_v * mean_c_t) / (mean_h_v + mean_c_v))
 
-samples['h_v'].plot_trace()
-samples['h_t'].plot_trace()
-samples['c_v'].plot_trace()
-samples['c_t'].plot_trace()
+# Plot trace of samples
+samples['h_v_dist'].plot_trace()
+samples['h_t_dist'].plot_trace()
+samples['c_v_dist'].plot_trace()
+samples['c_t_dist'].plot_trace()