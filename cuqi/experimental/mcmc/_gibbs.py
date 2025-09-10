8c4bf93bbf7cc4e4ec5cee42ac72c1837325fe2a
amal-ghamdi <amal.m.alghamdi@gmail.com>
2025-09-10 20:07:05 +0300

    remove logic to save enable_FD flag

--- cuqi/experimental/mcmc/_gibbs.py
+++ cuqi/experimental/mcmc/_gibbs.py
@@ -260,7 +260,6 @@ class HybridGibbs:
             if isinstance(sampler, NUTS): # Special case for NUTS as it is not playing nice with get_state and get_history
                 sampler.initial_point = sampler.current_point
                 max_depth = sampler.max_depth
-                enable_FD = sampler._enable_FD
                 num_tree_node_list = sampler.num_tree_node_list
                 # if  step_size is not set, use the last epsilon value from the epsilon_list
                 # to set it.
@@ -280,7 +279,6 @@ class HybridGibbs:
 
             # Set state and history back to sampler
             if isinstance(sampler, NUTS): # Again, special case for NUTS.
-                sampler._enable_FD = enable_FD
                 sampler.max_depth = max_depth
                 sampler.num_tree_node_list = num_tree_node_list
                 sampler.epsilon_list = epsilon_list
