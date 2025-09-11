f4822857ef6a5936fbe86b9e2abdaa2e51a305fd
Chao <chaz@dtu.dk>
2025-09-11 08:36:24 +0200

    remove Score

--- cuqi/implicitprior/__init__.py
+++ cuqi/implicitprior/__init__.py
@@ -1,5 +1,5 @@
 from ._regularizedGaussian import RegularizedGaussian, ConstrainedGaussian, NonnegativeGaussian
 from ._regularizedGMRF import RegularizedGMRF, ConstrainedGMRF, NonnegativeGMRF
 from ._regularizedUnboundedUniform import RegularizedUnboundedUniform
-from ._restorator import RestorationPrior, MoreauYoshidaPrior, TweediePrior, ScorePrior
+from ._restorator import RestorationPrior, MoreauYoshidaPrior, TweediePrior
 
