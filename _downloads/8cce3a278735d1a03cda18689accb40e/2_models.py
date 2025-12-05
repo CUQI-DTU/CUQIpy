e24a57c1b559c6f5464f4ab5846e5d3d43829a18
amal-ghamdi <amal-ghamdi@users.noreply.github.com>
2025-12-05 08:34:20 +0000

    deploy: 8c8bbb650b8b914d99f96f7f6fa321899088a930

---
+++ _downloads/8cce3a278735d1a03cda18689accb40e/2_models.py
@@ -0,0 +1,14 @@
+"""
+Deterministic models
+====================
+
+This tutorial will cover the basic concepts of deterministic models in CUQIpy.
+
+"""
+# %%
+import cuqi
+
+# %%
+#
+# .. todo::
+#    Continue tutorial here.
